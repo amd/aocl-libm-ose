efd3a5781ce4aa4c05132a8731d58b460df410bc
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-02-08 08:50:44 +0530

    libm: test: exp2 make use of developer mode
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- tests/exp2/test_exp2.c
+++ tests/exp2/test_exp2.c
@@ -74,7 +74,8 @@ __m256d LIBM_FUNC_VEC(d, 4, exp2)(__m256d in)
 }
 #endif
 
-#if 1
+#if defined(DEVELOPER_MODE)
+#pragma message "Developer mode changing prototype to exp2_v2()"
 #undef LIBM_FUNC
 #define LIBM_FUNC(x) FN_PROTOTYPE( x ## _v2 )
 
@@ -382,7 +383,7 @@ static int test_exp2_default_setup(struct libm_test *test)
         LIBM_TEST_DPRINTF(PANIC, "Unable to populate test_data\n");
         goto out;
     }
-
+    
     return 0;
 
  out:

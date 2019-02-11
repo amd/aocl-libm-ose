e7d987a0cd7fca0b70a5f594e1b6ae15a2943e26
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-02-11 19:25:20 +0530

    libm: exp2: reference implementation with 64 and 1024 tables
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- src/ref/exp2.c
+++ src/ref/exp2.c
@@ -5,271 +5,257 @@
  */
 #include <stdint.h>
 
-#include "libm_macros.h"
+#include <libm_amd_paths.h>
+#include <libm_special.h>
 
-#define ALIGN(x) __attribute__((aligned(x)))
+#pragma GCC push_options
+#pragma GCC optimize ("O3")
 
-typedef union {
-	int32_t         i;
-	float           f;
-} flt32_t;
+#include <libm_macros.h>
+#include <libm_types.h>
 
-typedef union {
-	int64_t         i;
-	double          d;
-} flt64_t;
+/*
+ * N defines the precision with which we deal with 'x'
+ * I.O.W (1 << N) is the size  of the look up table
+ */
 
-enum {
-	LIBM_CONST_FLT_REAL_1,
-	LIBM_CONST_FLT_REAL_64,
+#define N 10
+#define TABLE_SIZE (1ULL << N)
 
-	/*
-	 * Known logarithms
-	 */
-	LIBM_CONST_FLT_REAL_LN2,
+#if N == 6
+extern double exp_v2_two_to_jby64_table[];
+#define TABLE_DATA exp_v2_two_to_jby64_table
 
-	/*
-	 * Reciprocals
-	 */
-	LIBM_CONST_FLT_1_BY_2,
-	LIBM_CONST_FLT_1_BY_6,
-	LIBM_CONST_FLT_1_BY_24,
-	LIBM_CONST_FLT_1_BY_120,
-	LIBM_CONST_FLT_1_BY_720,
+#define POLY_DEGREE 6
+
+#elif N == 10
+extern double exp_v2_two_to_jby1024_table[];
+#define TABLE_DATA exp_v2_two_to_jby1024_table
+#define POLY_DEGREE 4
+
+#endif
+
+#ifdef __GNUC__
+#define ALIGN(x)        __attribute__((aligned ((x))))
+
+#define RODATA          __attribute__((section (".rodata")))
+
+#define HIDDEN         __attribute__ ((__visibility__ ("hidden")))
+#define NOINLINE       __attribute__ ((noinline))
+#define likely(x)      __builtin_expect (!!(x), 1)
+#define unlikely(x)    __builtin_expect (x, 0)
+#define strong_alias(f, a)                             \
+    extern __typeof (f) a __attribute__ ((alias (#f)));
+#define hidden_alias(f, a)                                             \
+    extern __typeof (f) a __attribute__ ((alias (#f), visibility ("hidden")));
+
+
+/* Optimize related defines */
+#define OPTIMIZE_O1 OPTIMIZE(1)
+#define OPTIMIZE_O2 OPTIMIZE(2)
+#define OPTIMIZE_O3 OPTIMIZE(3)
+#define OPTIMIZE_Og __attribute__((optimize("Og")))
+#endif
+
+#define MAX_POLYDEGREE 8
+
+static struct {
+	double table_size;
+	double one_by_table_size;
+	double ln2;
+	double ALIGN(16) poly[MAX_POLYDEGREE];
+} exp2_data = {
+#if N == 10
+	.table_size          = 0x1.0p+10,
+	.one_by_table_size   = 0x1.0p-10,
+#elif N == 6
+	.table_size          = 0x1.0p+6,
+	.one_by_table_size   = 0x1.0p-6,
+#else
+#error "N not defined"
+#endif
+	.ln2  = 0x1.62e42fefa39efp-1, /* ln(2) */
+        /*
+         * Polynomial constants, 1/x! (reciprocal of factorial(x))
+         * To make better use of cache line, we dont store 0! and 1!
+         */
+	.poly = {	/* skip for 0! and 1! */
+		0x1.0000000000000p-1,	/* 1/2! = 1/2    */
+		0x1.5555555555555p-3,	/* 1/3! = 1/6    */
+		0x1.5555555555555p-5,	/* 1/4! = 1/24   */
+		0x1.1111111111111p-7 ,	/* 1/5! = 1/120  */
+		0x1.6c16c16c16c17p-10,	/* 1/6! = 1/720  */
+		0x1.a01a01a01a01ap-13,	/* 1/7! = 1/5040 */
+		0x1.a01a01a01a01ap-16,	/* 1/8! = 1/40320*/
+		0x1.71de3a556c734p-19,	/* 1/9! = 1/322880*/
+	},
 };
 
-enum {
-	LIBM_CONST_DBL_REAL_1,
-	LIBM_CONST_DBL_REAL_64,
-	LIBM_CONST_DBL_REAL_1024,
+/* C1 is 1 as 1! = 1 and 1/1! = 1 */
+#define C2	exp2_data.poly[0]
+#define C3	exp2_data.poly[1]
+#define C4	exp2_data.poly[2]
+#define C5	exp2_data.poly[3]
+#define C6	exp2_data.poly[4]
+#define C7	exp2_data.poly[5]
+#define C8	exp2_data.poly[6]
+#define REAL_TABLE_SIZE         exp2_data.table_size
+#define REAL_1_BY_TABLE_SIZE	exp2_data.one_by_table_size
+#define REAL_LN2		exp2_data.ln2
 
-	/*
-	 * Known logarithms
-	 */
-	LIBM_CONST_DBL_LN2,
-
-	/*
-	 * Reciprocals
-	 */
-	LIBM_CONST_DBL_1_BY_2,
-	LIBM_CONST_DBL_1_BY_6,
-	LIBM_CONST_DBL_1_BY_24,
-	LIBM_CONST_DBL_1_BY_64,
-	LIBM_CONST_DBL_1_BY_120,
-	LIBM_CONST_DBL_1_BY_720,
-	LIBM_CONST_DBL_1_BY_1024,
-};
-
-//#define RODATA __attribute__((section (".rodata")))
-
-// Polynomial constants, 1/x! (reciprocal of factorial(x))
-#define MAX_POLYORDER 8
-flt64_t poly[MAX_POLYORDER + 2] = {	/* for 0! and 1! */
-	{.i = 0x3ff0000000000000ULL}, 	/* 1/0! = 1/1    */
-	{.i = 0x3ff0000000000000ULL},	/* 1/1! = 1/1    */
-	{.i = 0x3fe0000000000000ULL},	/* 1/2! = 1/2    */
-	{.i = 0x3fc5555555555555ULL},	/* 1/3! = 1/6    */
-	{.i = 0x3fa5555555555555ULL},	/* 1/4! = 1/24   */
-	{.i = 0x3f81111111111111ULL},	/* 1/5! = 1/120  */
-	{.i = 0x3f56c16c16c16c17ULL},	/* 1/6! = 1/720  */
-	{.i = 0x3f2a01a01a01a01aULL},	/* 1/7! = 1/5040 */
-	{.i = 0x0},			/* 1/8! = 1/40320*/
-	{.i = 0x0},			/* 1/9!          */
-};
-
-#define C2	poly[2].d
-#define C3	poly[3].d
-#define C4	poly[4].d
-#define C5	poly[5].d
-#define C6	poly[6].d
-#define C7	poly[7].d
-
-flt64_t math_table_64[] = {
-	/*
-	 * Integers with real equivalant
-	 */
-	[LIBM_CONST_DBL_REAL_1]   = { .i = 0x3ff0000000000000ULL},
-	[LIBM_CONST_DBL_REAL_64]  = { .i = 0x4050000000000000ULL},
-	[LIBM_CONST_DBL_REAL_1024]= { .i = 0x4090000000000000ULL},
-	/*
-	 * Reciprocals
-	 */
-	[LIBM_CONST_DBL_1_BY_2]	  = { .i = 0x3fe0000000000000ULL},
-	[LIBM_CONST_DBL_1_BY_6]	  = { .i = 0x3fc5555555555555ULL},
-	[LIBM_CONST_DBL_1_BY_24]  = { .i = 0x3fa5555555555555ULL},
-	[LIBM_CONST_DBL_1_BY_64]  = { .i = 0x3F90000000000000ULL},
-	[LIBM_CONST_DBL_1_BY_120] = { .i = 0x3f81111111111111ULL},
-	[LIBM_CONST_DBL_1_BY_720] = { .i = 0x3f56c16c16c16c17ULL},
-	[LIBM_CONST_DBL_1_BY_1024]= { .i = 0x3F50000000000000ULL},
-	[LIBM_CONST_DBL_LN2]	  = { .i = 0x3FE62E42FEFA39EFULL},	/* ln(2) */
-};
 
 static inline   int32_t
-cast_double_to_int32( double x )
+cast_double_to_i32( double x )
 {
-	return ( int32_t ) x;
+    return (int32_t) x;
+}
+
+static inline   int64_t
+cast_double_to_i64( double x )
+{
+    return (int64_t) x;
 }
 
 static inline double
 cast_i32_to_double( int32_t x )
 {
-	return ( double )x;
+    return (double)x;
 }
 
 static inline double
 cast_i64_to_double( int64_t x )
 {
-	return ( double )x;
+    return (double)x;
 }
 
-extern double   __two_to_jby64_table[];
-extern double   __two_to_jby64_tail_table[];
-extern double   __two_to_jby64_head_table[];
+//static const double max_exp2_arg = 1024.0;
+//static const double min_exp2_arg = -1074.0;
 
-extern double  __two_to_jby1024_table[];
-extern double  __two_to_jby1024_tail_table[];
-extern double  __two_to_jby1024_head_table[];
+double _exp2_special(double x, double y, uint32_t code);
 
-#define OPT_O1 __attribute__((optimize("O1")))
-#define OPT_O2 __attribute__((optimize("O2")))
-#define OPT_O3 __attribute__((optimize("O3")))
-#define OPT_Og __attribute__((optimize("Og")))
-
-#define OPTIMIZE(x) __attribute__((optimize("O"#x)))
-
-double  OPTIMIZE(3)
-FN_PROTOTYPE( exp2_v1 ) ( double x )
+static inline uint32_t top12(double x)
 {
-	double          a, dn, r, q;
-	int64_t         n, j;
-
-	flt64_t         m, q1;
-
-	// a = x * 64.0
-	a = x * math_table_64[LIBM_CONST_DBL_REAL_64].d;
-
-	// n = (int) a
-	n = ( int64_t ) a;
-
-	// dn = (double) n
-	dn = ( double ) n;
-
-	// r = x - (dn / 64)
-	r = x - ( dn * math_table_64[LIBM_CONST_DBL_1_BY_64].d );
-
-	r *= math_table_64[LIBM_CONST_DBL_LN2].d;
-
-	j = n & 0x3f;                      /* last 6-digits, index to lookup */
-
-	m.i = ( n - j ) / 64;
-
-	// r + ((r*r)*(1/2 + (r*1/6))) +
-	// ((r*r) * (r*r)) * (1/24 + (r * (1/120 + (r*1/720))))
-
-	double r2 = r * r;
-	q = r + ( r2) *
-		( math_table_64[LIBM_CONST_DBL_1_BY_2].d +
-		  r * ( math_table_64[LIBM_CONST_DBL_1_BY_6].d ) );
-
-	q += ( ( r2 ) * ( r2 ) ) *   /* r^4 */
-		( math_table_64[LIBM_CONST_DBL_1_BY_24].d +
-		  r * ( math_table_64[LIBM_CONST_DBL_1_BY_120].d +
-			  ( r * math_table_64[LIBM_CONST_DBL_1_BY_720].d ) ) );
-
-	// f(j)*q + f1 + f2
-	q1.d = ( q * __two_to_jby64_table[j] ) +
-		__two_to_jby64_tail_table[j] + __two_to_jby64_head_table[j];
-
-	/*
-	 * Processing denormals
-	 */
-	if ( ( m.i <= -1022LL ) ) {        /* -1022 */
-		if ( q1.d > math_table_64[LIBM_CONST_DBL_REAL_1].d )
-			goto process_normals;
-		if ( m.i < -1022LL ) {
-			/*
-			 * Process true de-normals
-			 */
-			m.i += 1074;
-			flt64_t         tmp = {.i = ( 1 << ( uint8_t ) m.i ) };
-			q1.d *= tmp.d;
-		}
-	}
-
-process_normals:
-	q1.i += ( m.i << 52 );
-
-	return q1.d;
+    return asuint64(x) >> 52;
 }
 
-double OPTIMIZE(3)
-FN_PROTOTYPE( exp2_v2 )( double x )
+double
+FN_PROTOTYPE(exp2_v2)(double x)
 {
-	double          a, dn, r, q;
-	int64_t         n, j;
+    double_t    a, dn, r, q;
+    int64_t     n, j, m;
+    flt64_t     q1 = {.i = 0,}, q2 = q1;
 
-	flt64_t         m, q1;
+#define EXP_X_NAN       1
+#define EXP_Y_ZERO      2
+#define EXP_Y_INF       3
 
-	a = x * math_table_64[LIBM_CONST_DBL_REAL_1024].d;
+    /*
+     * Top 11 bits, ignoring sign bit
+     * this is with BIAS
+     */
+    uint32_t exponent = top12(x) & 0x7ff;
 
-	n = ( int64_t ) a;
+    /*
+     * 11-bit 'exponent' is compared with, 12-bit unsigned value
+     * one comparison for multiple decisions
+     */
+    if (unlikely(exponent >= top12(512.0))) {
+        if (exponent >= top12(1024.0))
+            return _exp2_special(x, x+x, EXP_Y_INF);
 
-	dn = ( double ) n;
+        if ((exponent - 1024) >= 0x80000000U)
+            return _exp2_special(x, x+x, EXP_Y_ZERO);
+    }
 
-	r = x - (dn * math_table_64[LIBM_CONST_DBL_1_BY_1024].d);
+    a = x * REAL_TABLE_SIZE;
 
-	r *= math_table_64[LIBM_CONST_DBL_LN2].d;
+    n = cast_double_to_i64(a);
 
-	j = n & 0x3ff;                      /* last 6-digits, index to lookup */
+    dn = cast_i64_to_double(n);
 
-	m.i = (n - j) / 1024;
+    r = x - (dn * REAL_1_BY_TABLE_SIZE);
 
-#define HORNORS_POLY 1
-#ifdef HORNORS_POLY
-	q = r * (1 +
-		 r * (C2 +
-		      r * (C3 +
-			   r * (C4 +
-				r * (C5 +
-				     r * C6)))));
-#else  /* Estrin's */
-	// r + ((r*r)*(1/2 + (r*1/6))) +
-	// ((r*r) * (r*r)) * (1/24 + (r * (1/120 + (r*1/720))))
+    r *= REAL_LN2;
 
-	double r2 = r * r;
-	q = r + (r2 * (C2  +
-		       r * C3 );
+    j = n & ((1ULL << N) - 1);          /* table index, for lookup */
 
-	q += (r2 * r2) *   /* r^4 */
-		 (C4 +
-		  r * (C5 +
-		       (r * C6)));
+    m = (n - j) >> N;			/* n-j/TABLE_SIZE, TABLE_SIZE = 1<<N */
+
+#define ESTRIN_SCHEME  0xee
+#define HORNER_SCHEME  0xef
+
+#define POLY_EVAL_METHOD ESTRIN_SCHEME
+
+#ifndef POLY_EVAL_METHOD
+#error "Polynomial evaluation method NOT defined"
 #endif
 
-	// f(j)*q + f1 + f2
-	q1.d = q * __two_to_jby1024_table[j] +
-	       __two_to_jby1024_head_table[j] +
-	       __two_to_jby1024_tail_table[j];
+#if POLY_EVAL_METHOD == HORNER_SCHEME
+#if !defined(POLY_DEGREE)
+#define POLY_DEGREE 6
+#endif
+#if POLY_DEGREE == 7
+    q = r * (1 + r * (C2 + r * (C3 + r * (C4 + r * (C5 + r * (C6 + r * C7))))));
+#elif   POLY_DEGREE == 6
+    q = r * (1 + r * (C2 + r * (C3 + r * (C4 + r * (C5 + r * C6)))));
+#elif POLY_DEGREE == 5
+    q = r * (1 + r * (C2 + r * (C3 + r * (C4 + r * C5))));
+#elif POLY_DEGREE == 4
+    q = r * (1 + r * (C2 + r * (C3 + r * C4)));
+#elif POLY_DEGREE == 3
+    q = r * (1 + r * (C2 + r * C3));
+#else  /* Poly order <=2 */
+    q = r * (1 + r * C2);
+#endif	/* Order <=2 && Order == 3 */
 
-	/*
-	 * Processing denormals
-	 */
-	if ( m.i <= -1022LL ) {        /* -1022 */
-		if ( q1.d > math_table_64[LIBM_CONST_DBL_REAL_1].d )
-			goto process_normals;
-		if ( m.i < -1022LL ) {
-			/*
-			 * Process true de-normals
-			 */
-			m.i += 1074;
-			flt64_t tmp = {.i = ( 1 << ( uint8_t ) m.i ) };
-			q1.d *= tmp.d;
-		}
-	}
+#elif POLY_EVAL_METHOD == ESTRIN_SCHEME
+    /* Estrin's */
+    // r + ((r*r)*(1/2 + (r*1/6))) +
+    // ((r*r) * (r*r)) * (1/24 + (r * (1/120 + (r*1/720))))
 
-process_normals:
-	q1.i += ( m.i << 52 );
+    double r2 = r * r;
+    q = r + (r2 * (C2  + r * C3));
 
-	return q1.d;
+#if POLY_DEGREE == 4
+    q += (r2 * r2) *  C4; /* r^4 * C4 */
+#elif POLY_DEGREE == 5
+    q += (r2 * r2) * (C4 + r*C5);
+#elif POLY_DEGREE == 6
+    q += (r2 * r2) * (C4 + r * (C5 + r*C6));
+#endif
 
+#endif  /* if HORNER_SCHEME || ESTRIN_SCHEME */
+
+    {
+	    struct exp_table {
+		    double_t main, head, tail;
+	    };
+	    struct exp_table *tbl = &((struct exp_table*)TABLE_DATA)[j];
+	    /* f(j)*q + f1 + f2 */
+	    q = q * tbl->main + tbl->head + tbl->tail;
+    }
+
+    /*
+     * Processing denormals
+     */
+    if (unlikely (exponent == 0)) {
+        if (m < -1023) {
+            /* Process true de-normals */
+            m += 1074;
+            flt64u_t tmp = {.i = (1 << (uint8_t)m) };
+            q2.d = q * tmp.d;
+            return q2.d;
+        }
+    }
+
+    m <<= 52;
+    q1.i = m + asuint64(q);
+
+    return q1.d;
+
+#if defined(__ENABLE_IEEE_EXCEPTIONS)
+
+#endif
 }
+
+#pragma GCC pop_options

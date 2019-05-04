e372f7876339568f4eaecd6656cc28e63d17a325
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-05-04 13:51:40 +0530

    libm: exp() fix compilation error
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- src/optmized/exp.c
+++ src/optmized/exp.c
@@ -2,29 +2,59 @@
  * Copyright (C) 2018, Advanced Micro Devices. All rights reserved.
  *
  * Author: Prem Mallappa <pmallapp@amd.com>
+ *      Literal translation of ASM routien + optimizations
  */
+
+
 /*
- * exp(x) -> e^(x)
+ * ISO-IEC-10967-2: Elementary Numerical Functions
+ * Signature:
+ *   double exp(double x)
  *
- * e^x = 2^(x/ln2)
- * 
+ * Spec:
+ *   exp(1) = e
+ *   exp(x) = 1           if x ∈ F and exp(x) ≠ eˣ
+ *   exp(x) = 1           if x = -0
+ *   exp(x) = +inf        if x = +inf
+ *   exp(x) = 0           if x = -inf
+ *   exp(x) = eˣ
  *
+ *   exp(x) overflows     if (approximately) x > ln(DBL_MAX). (709.782712893384)
+ */
+
+/*
+ * Implementation Notes
+ * ----------------------
+ * 1. Argument Reduction:
+ *      e^x = 2^(x/ln2) = 2^(x*(64/ln(2))/64)     --- (1)
  *
+ *      Choose 'n' and 'f', such that
+ *      x * 64/ln2 = n + f                        --- (2) | n is integer
+ *							  | |f| <= 0.5
+ *     Choose 'm' and 'j' such that,
+ *      n = (64 * m) + j                          --- (3)
  *
+ *     From (1), (2) and (3),
+ *      e^x = 2^((64*m + j + f)/64)
+ *          = (2^m) * (2^(j/64)) * 2^(f/64)
+ *          = (2^m) * (2^(j/64)) * e^(f*(ln(2)/64))
  *
+ * 2. Table Lookup
+ *      Values of (2^(j/64)) are precomputed, j = 0, 1, 2, 3 ... 63
  *
+ * 3. Polynomial Evaluation
+ *   From (2),
+ *     f = x*(64/ln(2)) - n
+ *   Let,
+ *     r  = f*(ln(2)/64) = x - n*(ln(2)/64)
  *
- *
- *
- *
- *
- *
- *
+ * 4. Reconstruction
+ *      Thus,
+ *        e^x = (2^m) * (2^(j/64)) * e^r
  *
  */
 #include <stdint.h>
 #include <libm_util_amd.h>
-#include <libm_amd_paths.h>
 #include <libm_special.h>
 
 #if !defined(__clang__) && !defined(ENABLE_DEBUG)
@@ -43,107 +73,9 @@
  * I.O.W (1 << N) is the size  of the look up table
  */
 
-#define N 10
-#define TABLE_SIZE (1ULL << N)
+#define EXP_N 6
 
-#if N == 6                              /* Table size 64 */
-extern double exp_v2_two_to_jby64_table[];
-#define TABLE_DATA exp_v2_two_to_jby64_table
-#define POLY_DEGREE 6
-
-#elif N == 7                            /* Table size 128 */
-extern double exp_v2_two_to_jby128_table[];
-#define TABLE_DATA exp_v2_two_to_jby128_table
-#define POLY_DEGREE 5
-
-#elif N == 8                            /* Table size 256 */
-extern double exp_v2_two_to_jby256_table[];
-#define TABLE_DATA exp_v2_two_to_jby256_table
-#define POLY_DEGREE 4
-
-#elif N == 9                            /* Table size 512 */
-extern double exp_v2_two_to_jby512_table[];
-#define TABLE_DATA exp_v2_two_to_jby512_table
-#define POLY_DEGREE 4
-
-#elif N == 10                           /* Table size 1024 */
-extern double exp_v2_two_to_jby1024_table[];
-#define TABLE_DATA exp_v2_two_to_jby1024_table
-#define POLY_DEGREE 4
-#endif
-
-#define MAX_POLYDEGREE 8
-
-struct exp_table {
-	double main, head, tail;
-};
-
-/*
- * tblsz_byln2 = (-1) x (log(2) / TABLE_SIZE);
- * head = asuint64(tblsz_byln2)
- * tail = tblsz_byln2 - head
- */
-
-static struct {
-	double Huge;
-	double tblsz_byln2;
-	double ln2by_tblsz_head, ln2by_tblsz_tail;
-	double ALIGN(16) poly[MAX_POLYDEGREE];
-	struct exp_table table[TABLE_SIZE];
-} exp_data = {
-#if N == 10
-	.tblsz_byln2	   =  0x1.71547652b82fep+10,
-	.ln2by_tblsz_head  = -0x1.62e42fefa0000p-11,
-	.ln2by_tblsz_tail  = -0x1.cf79abc9e3b3ap-50,
-#elif N == 9
-	.tblsz_byln2	   =  0x1.71547652b82fep+9,
-	.ln2by_tblsz_head  = -0x1.62e42fefa0000p-10,
-	.ln2by_tblsz_tail  = -0x1.cf79abc9e3b39p-49,
-#elif N	== 8
-	.tblsz_byln2	   =  0x1.71547652b82fep+8,
-	.ln2by_tblsz_head  = -0x1.62e42fefa0000p-9,
-	.ln2by_tblsz_tail  = -0x1.cf79abc9e3b39p-48,
-#elif N	== 7
-	.tblsz_byln2	   =  0x1.71547652b82fep+7,
-	.ln2by_tblsz_head  = -0x1.62e42fefa0000p-8,
-	.ln2by_tblsz_tail  = -0x1.cf79abc9e3b39p-47,
-#elif N	== 6
-	.tblsz_byln2	   =  0x1.71547652b82fep+6,
-	.ln2by_tblsz_head  = -0x1.62e42fefa0000p-7,
-	.ln2by_tblsz_tail  = -0x1.cf79abc9e3b39p-46,
-#else
-#error "N not defined"
-#endif
-	/*
-         * Polynomial constants, 1/x! (reciprocal of factorial(x))
-         * To make better use of cache line, we dont store 0! and 1!
-         */
-	.poly = {	/* skip for 0! and 1! */
-		0x1.0000000000000p-1,	/* 1/2! = 1/2    */
-		0x1.5555555555555p-3,	/* 1/3! = 1/6    */
-		0x1.5555555555555p-5,	/* 1/4! = 1/24   */
-		0x1.1111111111111p-7 ,	/* 1/5! = 1/120  */
-		0x1.6c16c16c16c17p-10,	/* 1/6! = 1/720  */
-		0x1.a01a01a01a01ap-13,	/* 1/7! = 1/5040 */
-		0x1.a01a01a01a01ap-16,	/* 1/8! = 1/40320*/
-		0x1.71de3a556c734p-19,	/* 1/9! = 1/322880*/
-	},
-	.Huge		   = 0x1.8p+52,
-};
-
-/* C1 is 1 as 1! = 1 and 1/1! = 1 */
-#define C2	exp_data.poly[0]
-#define C3	exp_data.poly[1]
-#define C4	exp_data.poly[2]
-#define C5	exp_data.poly[3]
-#define C6	exp_data.poly[4]
-#define C7	exp_data.poly[5]
-#define C8	exp_data.poly[6]
-#define HUGE	exp_data.Huge
-
-#define TBLSZ_BY_LN2		exp_data.tblsz_byln2
-#define LN2_BY_TBLSZ_HEAD	exp_data.ln2by_tblsz_head
-#define LN2_BY_TBLSZ_TAIL	exp_data.ln2by_tblsz_tail
+#include "exp_data.h"
 
 double _exp_special(double x, double y, uint32_t code);
 
@@ -156,8 +88,9 @@ double
 FN_PROTOTYPE(exp_v2)(double x)
 {
     double_t    r, q, dn;
-    int64_t     n, j, m;
-    flt64_t     q1 = {.i = 0,}, q2 = q1;
+    int64_t	n, m;
+    int32_t	j;
+    flt64_t     q1 = {.i = 0,};
 
 #define EXP_X_NAN       1
 #define EXP_Y_ZERO      2
@@ -180,24 +113,24 @@ FN_PROTOTYPE(exp_v2)(double x)
             return _exp_special(x, asdouble(PINFBITPATT_DP64), EXP_Y_INF);
     }
 
-    double_t a	= x * TBLSZ_BY_LN2;
+    double_t a = x * EXP_TBLSZ_BY_LN2;
 
 #define FAST_INTEGER_CONVERSION 1
 #if FAST_INTEGER_CONVERSION
-    q1.d   = a + HUGE;
+    q1.d   = a + EXP_HUGE;
     n      = q1.i;
-    dn     = q1.d - HUGE;
+    dn     = q1.d - EXP_HUGE;
 #else
     n	   = cast_double_to_i64(a);
     dn	   = cast_i64_to_double(n);
 #endif
 
-    double_t r1 = x + dn * LN2_BY_TBLSZ_HEAD;
-    double_t r2 = dn * LN2_BY_TBLSZ_TAIL;
+    double_t r1 = x + dn * EXP_LN2_BY_TBLSZ_HEAD;
+    double_t r2 = dn * EXP_LN2_BY_TBLSZ_TAIL;
     r = r1 + r2;
 
     /* table index, for lookup, truncated */
-    j = n % TABLE_SIZE;
+    j = n % EXP_TABLE_SIZE;
 
     /*
      * n-j/TABLE_SIZE, TABLE_SIZE = 1<<N
@@ -205,8 +138,15 @@ FN_PROTOTYPE(exp_v2)(double x)
      *    (n - j) >> N   and
      *    m <<= 52
      */
-    m = (n - j) << (52 - N);
+    m = (n - j) << (52 - EXP_N);
 
+    /* redefining for easy reading */
+#define C2 EXP_C2
+#define C3 EXP_C3
+#define C4 EXP_C4
+#define C5 EXP_C5
+#define C6 EXP_C6
+#define C7 EXP_C7
 
 #define ESTRIN_SCHEME  0xee
 #define HORNER_SCHEME  0xef
@@ -218,18 +158,18 @@ FN_PROTOTYPE(exp_v2)(double x)
 #endif
 
 #if POLY_EVAL_METHOD == HORNER_SCHEME
-#if !defined(POLY_DEGREE)
-#define POLY_DEGREE 6
+#if !defined(EXP_POLY_DEGREE)
+#define EXP_POLY_DEGREE 6
 #endif
-#if POLY_DEGREE == 7
+#if EXP_POLY_DEGREE == 7
     q = r * (1 + r * (C2 + r * (C3 + r * (C4 + r * (C5 + r * (C6 + r * C7))))));
-#elif   POLY_DEGREE == 6
+#elif   EXP_POLY_DEGREE == 6
     q = r * (1 + r * (C2 + r * (C3 + r * (C4 + r * (C5 + r * C6)))));
-#elif POLY_DEGREE == 5
+#elif EXP_POLY_DEGREE == 5
     q = r * (1 + r * (C2 + r * (C3 + r * (C4 + r * C5))));
-#elif POLY_DEGREE == 4
+#elif EXP_POLY_DEGREE == 4
     q = r * (1 + r * (C2 + r * (C3 + r * C4)));
-#elif POLY_DEGREE == 3
+#elif EXP_POLY_DEGREE == 3
     q = r * (1 + r * (C2 + r * C3));
 #else  /* Poly order <=2 */
     q = r * (1 + r * C2);
@@ -243,11 +183,11 @@ FN_PROTOTYPE(exp_v2)(double x)
     r2 = r * r; 			/* r^2 */
     q = r + (r2 * (C2  + r * C3));
 
-#if POLY_DEGREE == 4
+#if EXP_POLY_DEGREE == 4
     q += (r2 * r2) *  C4; /* r^4 * C4 */
-#elif POLY_DEGREE == 5
+#elif EXP_POLY_DEGREE == 5
     q += (r2 * r2) * (C4 + r*C5);
-#elif POLY_DEGREE == 6
+#elif EXP_POLY_DEGREE == 6
     q += (r2 * r2) * (C4 + r * (C5 + r*C6));
 #endif
 #else
@@ -255,29 +195,51 @@ FN_PROTOTYPE(exp_v2)(double x)
 #endif  /* if HORNER_SCHEME || ESTRIN_SCHEME */
 
     /* f(j)*q + f1 + f2 */
-    struct exp_table *tbl = &((struct exp_table*)TABLE_DATA)[j];
+    struct exp_table *tbl = &((struct exp_table*)EXP_TABLE_DATA)[j];
     q = q * tbl->main + tbl->head + tbl->tail;
 
     /*
      * Processing denormals
      */
-    if (unlikely(exponent == 0)) {
-        if (m < -1023) {
-            /* Process true de-normals */
-            m += 1074;
-            flt64u_t tmp = {.i = (1 << (uint8_t)m) };
-            q2.d = q * tmp.d;
-            return q2.d;
+    if (unlikely (exponent == 0)) {
+        /* re-calculate m */
+        int32_t m2 = (n - j) >> EXP_N;
+        if (m2 <= -1022) {
+            if (m2 < -1022 || q < 1.0) {
+                /* Process true de-normals */
+                m2 +=  1074;
+                flt64u_t tmp = {.i = (1ULL << m2) };
+                return q * tmp.d;
+            }
         }
     }
 
-    q1.i = m + asuint64(q);
-    return q1.d;
+    q1.d =  asdouble(m + asuint64(q));
 
+    return q1.d;
 
 #if defined(__ENABLE_IEEE_EXCEPTIONS)
 #endif
 }
+
+#if defined(ENABLE_GLIBC_API)
+strong_alias (__exp_finite, FN_PROTOTYPE(exp_v2))
+hidden_alias (__ieee754_exp, FN_PROTOTYPE(exp_v2))
+#endif
+
+#if defined(ENABLE_LIBM_API)
+strong_alias (exp, FN_PROTOTYPE(exp_v2))
+#endif
+
+#if defined(ENABLE_AMDLIBM_API)
+strong_alias (amd_exp, FN_PROTOTYPE(exp_v2))
+#endif
+
+/*
+ * No API call is defined, the functions to be called directly
+ * like FN_PROTOTYPE(exp_v2)(arg)
+ * useful for internal testing
+ */
 
 #if defined(__GCC__) && !defined(__clang__) && !defined(ENABLE_DEBUG)
 #pragma GCC pop_options

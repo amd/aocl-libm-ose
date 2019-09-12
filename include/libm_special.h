5c38545084cbe993faba3fef6745ea3cc76b0ebc
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-09-12 11:18:11 +0530

    libm: added proper libm_special.h
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- include/libm_special.h
+++ include/libm_special.h
@@ -7,8 +7,8 @@
 
 #include <fpieee.h>
 #define EXCEPTION_S _exception
-#define _amd_handle_error _handle_error
-#define _amd_handle_errorf _handle_errorf
+#define __amd_handle_error _handle_error
+#define __amd_handle_errorf _handle_errorf
 
 double _handle_error(
         char *fname,
@@ -37,7 +37,7 @@ float _handle_errorf(
 #else //Windows
 #define EXCEPTION_S exception
 
-double _amd_handle_error(
+double __amd_handle_error(
         char *fname,
         int opcode,
         unsigned long long value,
@@ -49,7 +49,7 @@ double _amd_handle_error(
         int nargs
         );
 
-float _amd_handle_errorf(
+float __amd_handle_errorf(
         char *fname,
         int opcode,
         unsigned long long value,
@@ -64,127 +64,127 @@ float _amd_handle_errorf(
 
 
 typedef enum {
-    _FpCodeUnspecified,
-    _FpCodeAdd,
-    _FpCodeSubtract,
-    _FpCodeMultiply,
-    _FpCodeDivide,
-    _FpCodeSquareRoot,
-    _FpCodeRemainder,
-    _FpCodeCompare,
-    _FpCodeConvert,
-    _FpCodeRound,
-    _FpCodeTruncate,
-    _FpCodeFloor,
-    _FpCodeCeil,
-    _FpCodeAcos,
-    _FpCodeAsin,
-    _FpCodeAtan,
-    _FpCodeAtan2,
-    _FpCodeCabs,
-    _FpCodeCos,
-    _FpCodeCosh,
-    _FpCodeExp,
-    _FpCodeFabs,
-    _FpCodeFmod,
-    _FpCodeFrexp,
-    _FpCodeHypot,
-    _FpCodeLdexp,
-    _FpCodeLog,
-    _FpCodeLog10,
-    _FpCodeModf,
-    _FpCodePow,
-    _FpCodeSin,
-    _FpCodeSinh,
-    _FpCodeTan,
-    _FpCodeTanh,
-    _FpCodeY0,
-    _FpCodeY1,
-    _FpCodeYn,
-    _FpCodeLogb,
-    _FpCodeNextafter,
-    _FpCodeNegate, 
-    _FpCodeFmin,         /* XMMI */
-    _FpCodeFmax,         /* XMMI */
-    _FpCodeConvertTrunc, /* XMMI */
-    _XMMIAddps,          /* XMMI */
-    _XMMIAddss,
-    _XMMISubps,
-    _XMMISubss,
-    _XMMIMulps,
-    _XMMIMulss,
-    _XMMIDivps,
-    _XMMIDivss,
-    _XMMISqrtps,
-    _XMMISqrtss,
-    _XMMIMaxps,
-    _XMMIMaxss,
-    _XMMIMinps,
-    _XMMIMinss,
-    _XMMICmpps,
-    _XMMICmpss,
-    _XMMIComiss,
-    _XMMIUComiss,
-    _XMMICvtpi2ps,
-    _XMMICvtsi2ss,
-    _XMMICvtps2pi,
-    _XMMICvtss2si,
-    _XMMICvttps2pi,
-    _XMMICvttss2si,
-    _XMMIAddsubps,       /* XMMI for PNI */
-    _XMMIHaddps,         /* XMMI for PNI */
-    _XMMIHsubps,         /* XMMI for PNI */
-    _XMMIRoundps,        /* 66 0F 3A 08  */ 
-    _XMMIRoundss,        /* 66 0F 3A 0A  */
-    _XMMIDpps,           /* 66 0F 3A 40  */
-    _XMMI2Addpd,         /* XMMI2 */ 
-    _XMMI2Addsd,
-    _XMMI2Subpd,
-    _XMMI2Subsd,
-    _XMMI2Mulpd,
-    _XMMI2Mulsd,
-    _XMMI2Divpd,
-    _XMMI2Divsd,
-    _XMMI2Sqrtpd,
-    _XMMI2Sqrtsd,
-    _XMMI2Maxpd,
-    _XMMI2Maxsd,
-    _XMMI2Minpd,
-    _XMMI2Minsd,
-    _XMMI2Cmppd,
-    _XMMI2Cmpsd,
-    _XMMI2Comisd,
-    _XMMI2UComisd,
-    _XMMI2Cvtpd2pi,   /* 66 2D */
-    _XMMI2Cvtsd2si,   /* F2 */
-    _XMMI2Cvttpd2pi,  /* 66 2C */
-    _XMMI2Cvttsd2si,  /* F2 */
-    _XMMI2Cvtps2pd,   /* 0F 5A */
-    _XMMI2Cvtss2sd,   /* F3 */
-    _XMMI2Cvtpd2ps,   /* 66 */
-    _XMMI2Cvtsd2ss,   /* F2 */
-    _XMMI2Cvtdq2ps,   /* 0F 5B */
-    _XMMI2Cvttps2dq,  /* F3 */
-    _XMMI2Cvtps2dq,   /* 66 */
-    _XMMI2Cvttpd2dq,  /* 66 0F E6 */
-    _XMMI2Cvtpd2dq,   /* F2 */
-    _XMMI2Addsubpd,   /* 66 0F D0 */
-    _XMMI2Haddpd,     /* 66 0F 7C */
-    _XMMI2Hsubpd,     /* 66 0F 7D */
-    _XMMI2Roundpd,    /* 66 0F 3A 09 */
-    _XMMI2Roundsd,    /* 66 0F 3A 0B */
-    _XMMI2Dppd,       /* 66 0F 3A 41 */
-    _FpCodeFmaSingle,
-    _FpCodeFmaDouble,
-    _FpCodeFms,
-    _FpCodeFmsSingle,
-    _FpCodeFmsDouble,
-    _FpCodeFnma,
-    _FpCodeFnmaSingle,
-    _FpCodeFnmaDouble,
-    _FpCodeFamin,
-    _FpCodeFamax,
-} _FP_OPERATION_CODE;
+    __amd_unspecified,
+    __amd_add,
+    __amd_subtract,
+    __amd_multiply,
+    __amd_divide,
+    __amd_squareroot,
+    __amd_remainder,
+    __amd_compare,
+    __amd_convert,
+    __amd_round,
+    __amd_truncate,
+    __amd_floor,
+    __amd_ceil,
+    __amd_acos,
+    __amd_asin,
+    __amd_atan,
+    __amd_atan2,
+    __amd_cabs,
+    __amd_cos,
+    __amd_cosh,
+    __amd_exp,
+    __amd_fabs,
+    __amd_fmod,
+    __amd_frexp,
+    __amd_hypot,
+    __amd_ldexp,
+    __amd_log,
+    __amd_log10,
+    __amd_modf,
+    __amd_pow,
+    __amd_sin,
+    __amd_sinh,
+    __amd_tan,
+    __amd_tanh,
+    __amd_y0,
+    __amd_y1,
+    __amd_yn,
+    __amd_logb,
+    __amd_nextafter,
+    __amd_negate, 
+    __amd_fmin,         
+    __amd_fmax,         
+    __amd_converttrunc, 
+    __amd_addps,        
+    __amd_addss,
+    __amd_subps,
+    __amd_subss,
+    __amd_mulps,
+    __amd_mulss,
+    __amd_divps,
+    __amd_divss,
+    __amd_sqrtps,
+    __amd_sqrtss,
+    __amd_maxps,
+    __amd_maxss,
+    __amd_minps,
+    __amd_minss,
+    __amd_cmpps,
+    __amd_cmpss,
+    __amd_comiss,
+    __amd_ucomiss,
+    __amd_cvtpi2ps,
+    __amd_cvtsi2ss,
+    __amd_cvtps2pi,
+    __amd_Cvtss2si,
+    __amd_cvttps2pi,
+    __amd_cvttss2si,
+    __amd_addsubps,       /* XMMI for PNI */
+    __amd_haddps,         /* XMMI for PNI */
+    __amd_hsubps,         /* XMMI for PNI */
+    __amd_roundps,        /* 66 0F 3A 08  */ 
+    __amd_roundss,        /* 66 0F 3A 0A  */
+    __amd_dpps,           /* 66 0F 3A 40  */
+    __amd_addpd,         /* XMMI2 */ 
+    __amd_addsd,
+    __amd_subpd,
+    __amd_subsd,
+    __amd_mulpd,
+    __amd_mulsd,
+    __amd_divpd,
+    __amd_divsd,
+    __amd_sqrtpd,
+    __amd_sqrtsd,
+    __amd_maxpd,
+    __amd_maxsd,
+    __amd_minpd,
+    __amd_minsd,
+    __amd_cmppd,
+    __amd_cmpsd,
+    __amd_comisd,
+    __amd_ucomisd,
+    __amd_cvtpd2pi,   /* 66 2D */
+    __amd_cvtsd2si,   /* F2 */
+    __amd_cvttpd2pi,  /* 66 2C */
+    __amd_cvttsd2si,  /* F2 */
+    __amd_cvtps2pd,   /* 0F 5A */
+    __amd_cvtss2sd,   /* F3 */
+    __amd_cvtpd2ps,   /* 66 */
+    __amd_cvtsd2ss,   /* F2 */
+    __amd_cvtdq2ps,   /* 0F 5B */
+    __amd_cvttps2dq,  /* F3 */
+    __amd_cvtps2dq,   /* 66 */
+    __amd_cvttpd2dq,  /* 66 0F E6 */
+    __amd_cvtpd2dq,   /* F2 */
+    __amd_addsubpd,   /* 66 0F D0 */
+    __amd_haddpd,     /* 66 0F 7C */
+    __amd_hsubpd,     /* 66 0F 7D */
+    __amd_roundpd,    /* 66 0F 3A 09 */
+    __amd_roundsd,    /* 66 0F 3A 0B */
+    __amd_dppd,       /* 66 0F 3A 41 */
+    __amd_fmaSingle,
+    __amd_fmaDouble,
+    __amd_fms,
+    __amd_fmsSingle,
+    __amd_fmsDouble,
+    __amd_fnma,
+    __amd_fnmaSingle,
+    __amd_fnmaDouble,
+    __amd_famin,
+    __amd_famax,
+} _AMDLIBM_CODE;
 
 #define _DOMAIN     1   /* argument domain error */
 #define _SING       2   /* argument singularity */
@@ -196,29 +196,29 @@ typedef enum {
 
 typedef enum 
 {
-	_FpCodeAsinh = (_XMMI2Dppd + 20),
-	_FpCodeAcosh,
-	_FpCodeAtanh,
-	_FpCodeCbrt,
-	_FpCodeExp10,
-	_FpCodeExp2,
-	_FpCodeExpm1,
-	_FpCodeLrint,
-	_FpCodeRint,
-	_FpCodeNexttoward,
-	_FpCodeNearbyint,
-	_FpCodeFdim,
-	_FpCodeFinite,
-	_FpCodeScalbn,
-	_FpCodeScalbln,
-	_FpCodeLog1p,
-	_FpCodeLog2,
-	_FpCodeLlrint,
-	_FpCodeLlround,
-	_FpCodeLround,
-	_FpCodeRemquo,
-    _FpCodeFma
-}_FP_OPERATION_CODE_EXT;
+	__amd_asinh = (__amd_dppd + 20),
+	__amd_acosh,
+	__amd_atanh,
+	__amd_cbrt,
+	__amd_exp10,
+	__amd_exp2,
+	__amd_expm1,
+	__amd_lrint,
+	__amd_rint,
+	__amd_nexttoward,
+	__amd_nearbyint,
+	__amd_fdim,
+	__amd_finite,
+	__amd_scalbn,
+	__amd_scalbln,
+	__amd_log1p,
+	__amd_log2,
+	__amd_llrint,
+	__amd_llround,
+	__amd_lround,
+	__amd_remquo,
+        __amd_fma
+}_AMDLIBM_EXTERNAL;
 
 #endif // __LIBM_SPECIAL_H__
 

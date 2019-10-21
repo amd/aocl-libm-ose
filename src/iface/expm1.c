c3cb033b6d0306f35084f6ce57b7e79792e7c007
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-10-21 13:30:43 +0530

    libm: tests: expm1 iface and vrd4_expm1 added to fix compilation
    Change-Id: Iffd5dceaa617f7ae6b58e5506b5b0285c53e9914
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

---
+++ src/iface/expm1.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2018-2019, AMD. All rights reserved.
+ *
+ * Author: Prem Mallappa <pmallapp@amd.com>
+ *
+ */
+
+#include <libm_macros.h>
+#include <libm_amd.h>
+#include <libm/amd_funcs_internal.h>
+#include <libm/iface.h>
+#include <libm/entry_pt.h>
+
+void
+LIBM_IFACE_PROTO(expm1)(void *arg)
+{
+    /*
+     * Should setup all variants,
+     * single, double, and vectors (also complex if available)
+     */
+    G_ENTRY_PT_PTR(expm1) = &FN_PROTOTYPE_BAS64(expm1);
+    G_ENTRY_PT_PTR(expm1f) = &FN_PROTOTYPE_BAS64(expm1f);
+
+    /*vector versions*/
+    G_ENTRY_PT_PTR(vrd4_expm1) = &FN_PROTOTYPE(vrd4_expm1);
+}
---
+++ src/ref/vrd4_expm1.c
@@ -0,0 +1,29 @@
+#include "libm_amd.h"
+#include "libm_util_amd.h"
+#include <libm/amd_funcs_internal.h>
+
+__m256d FN_PROTOTYPE(vrd4_expm1)(__m256d x)
+{
+
+	union XMMREGTYP_
+	{
+
+		__m256d d256;
+		double d[4];
+	};
+
+	typedef union XMMREGTYP_ XMMREGTYP;
+
+
+	XMMREGTYP inp256;
+	XMMREGTYP res256;
+
+	inp256.d256 = x;
+
+	FN_PROTOTYPE_FMA3(vrda_expm1)(4, &inp256.d[0], &res256.d[0]);
+
+	return res256.d256;
+
+}
+
+

18dd98057bf50ac6d3fcd407743678d2fa8d867d
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-10-23 19:51:22 +0530

    libm: iface: add ldexp
    Change-Id: I89aed867eebd49c39a48ac972ca0fe013b6cedd4
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

---
+++ src/iface/ldexp.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018, AMD. All rights reserved.
+ *
+ * Author: Prem Mallappa <pmallapp@amd.com>
+ *
+ */
+
+#include <libm_macros.h>
+#include <libm/cpu_features.h>
+#include <libm/entry_pt.h>
+#include <libm/iface.h>
+#include <libm/amd_funcs_internal.h>    /* Contains all implementations */
+
+
+void
+LIBM_IFACE_PROTO(ldexp)(void *arg)
+{
+	/* Double */
+	G_ENTRY_PT_PTR(ldexp) = &FN_PROTOTYPE_REF(ldexp);
+
+	/* Single */
+	G_ENTRY_PT_PTR(ldexpf) = &FN_PROTOTYPE_REF(ldexpf);
+
+	/* Vector Double */
+	/* Vector Single */
+}
+

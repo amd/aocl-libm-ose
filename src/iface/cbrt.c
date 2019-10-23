90794502cfabf8dad721fd297cf849744c2f8a4c
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-10-23 19:48:29 +0530

    libm: iface : adding cbrt and ceil
    Change-Id: Id2f0d97a5035964f6504040975d8edc4eec74ea6
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

---
+++ src/iface/cbrt.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018, AMD. All rights reserved.
+ *
+ * Author: Prem Mallappa <pmallapp@amd.com>
+ *
+ */
+
+#include <libm_macros.h>
+#include <libm/cpu_features.h>
+#include <libm/entry_pt.h>
+#include <libm/iface.h>
+#include <libm/amd_funcs_internal.h>    /* Contains all implementations */
+
+
+void
+LIBM_IFACE_PROTO(cbrt)(void *arg)
+{
+	/* Double */
+	G_ENTRY_PT_PTR(cbrt) = &FN_PROTOTYPE_FMA3(cbrt);
+
+	/* Single */
+	G_ENTRY_PT_PTR(cbrtf) = &FN_PROTOTYPE_FMA3(cbrtf);
+
+	/* Vector Double */
+	/* Vector Single */
+}
+

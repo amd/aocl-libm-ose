3e6e6c8810c1c68a032ddd29a6f019a9fc3c7874
Prem Mallappa <Premachandra.Mallappa@amd.com>
2019-12-26 20:50:17 +0530

    libm: iface: log: enable vector 4 logf()
    Change-Id: I27237620f1c33162b2d2183f4a3e93b60c88cf46
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- src/iface/log.c
+++ src/iface/log.c
@@ -16,7 +16,10 @@ typedef __m128d (*amd_log_v2d_t)(__m128d);
 typedef __m256d (*amd_log_v4d_t)(__m256d);
 typedef __m128  (*amd_log_v4s_t)(__m128);
 
-extern float FN_PROTOTYPE_OPT(logf_v3)(float);
+__m128 FN_PROTOTYPE_FMA3(vrs4_logf_dev)(__m128 x);
+float FN_PROTOTYPE_OPT(logf_v3)(float x);
+__m128 FN_PROTOTYPE_OPT(vrs4_logf)(__m128 x);
+
 
 void
 LIBM_IFACE_PROTO(log)(void *arg)
@@ -25,6 +28,7 @@ LIBM_IFACE_PROTO(log)(void *arg)
     amd_log_t  fn_d = NULL;
     amd_logf_t fn_s = NULL;
     amd_log_v4d_t fn_v4d = NULL;
+    amd_log_v4s_t fn_v4s = NULL;
 
     static struct cpu_features *features = NULL;
 
@@ -37,11 +41,13 @@ LIBM_IFACE_PROTO(log)(void *arg)
     fn_d = &FN_PROTOTYPE_FMA3(log);
     fn_s = &FN_PROTOTYPE_FMA3(logf);
     fn_v4d = &FN_PROTOTYPE_FMA3(vrd4_log);
+    fn_v4s = &FN_PROTOTYPE_FMA3(vrs4_logf);
 
     if (CPU_HAS_AVX2(features) &&
         CPU_FEATURE_AVX2_USABLE(features)) {
             //fn_d = &FN_PROTOTYPE_OPT(log);	/* we dont have an optimized log yet */
-	    fn_s = &FN_PROTOTYPE_OPT(logf);
+            fn_s   = &FN_PROTOTYPE_OPT(logf);
+            fn_v4s = &FN_PROTOTYPE_OPT(vrs4_logf);
     } else if (CPU_HAS_SSSE3(features) &&
                CPU_FEATURE_SSSE3_USABLE(features)) {
 	    fn_d = &FN_PROTOTYPE_BAS64(log);
@@ -69,6 +75,6 @@ LIBM_IFACE_PROTO(log)(void *arg)
     G_ENTRY_PT_PTR(log) = fn_d;
     G_ENTRY_PT_PTR(logf) = fn_s;
     G_ENTRY_PT_PTR(vrd4_log) = fn_v4d;
-
+    G_ENTRY_PT_PTR(vrs4_logf) = fn_v4s;
 }
 

539d068b5027102749b666a28debc509a7a22cf3
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-02-26 18:41:39 +0530

    libm: poly.h add 9th degree poly macro
    uses Estrin scheme and needed for fastlogf
    
    Change-Id: Ia482777a5e9468a3f7ad2123ab0e3b63c535f91c
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-633]

--- include/libm/poly.h
+++ include/libm/poly.h
@@ -34,4 +34,28 @@
             q;                                          \
             })
 
+/*
+ * poly = C1 + C2*r + C3*r^2 + C4*r^3 + C5 *r^4 + C6*r^5 \
+ *           + C7*r^6 + C8*r^7 + C9*r^8
+ *
+ *      = ((C6+C7*x)*x2 + (C4+C5*x))*x4 +
+ *                      (C8+C9*x)*x8) +
+ *                      ((C2+C3*x)*x2 + (C0+C1*x));
+ */
+#define POLY_EVAL_9(r, c0, c1, c2, c3, c4, c5, c6, c7, c8) ({   \
+	__typeof(r) a1, a2, a3, a4, b1, b2, q;			\
+	__typeof(r) r2, r4;					\
+	a1 = c2*r + c1;						\
+	a2 = c4*r + c3;						\
+	r2 = r * r;						\
+	a3 = c6*r + c5;						\
+	r4 = r2 * r2;						\
+	a4 = c8*r + c7;						\
+                                                                \
+	b1 = a4*r4 + a3*r2 + a2;				\
+	b2 = a1*r2 + c0*r;					\
+	q = b1*r4 + b2;						\
+	q;							\
+	})
+
 #endif	/* LIBM_POLY_H */

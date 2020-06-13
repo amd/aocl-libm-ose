f8cca05ac591e9e9bddee7d1829030c8e8b960ae
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-06-13 19:17:24 +0530

    libm: arch: zen2: add exp.c
    Change-Id: I4b983b17633e1c0a96a7fc31657ce425a813d576
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-571]

---
+++ src/arch/zen2/exp.c
@@ -0,0 +1,14 @@
+#define  ALM_OVERRIDE 1
+
+#include <libm_macros.h>
+#include <libm/types.h>
+
+#define  ALM_ARCH_FN_ZEN2(x) 		x##_zn2
+#define  ALM_ARCH_FN(x)  		ALM_ARCH_FN_ZEN2(x)
+
+#define  ALM_PROTO_ZEN2(x)		ALM_ARCH_FN(amd_##x)
+#define  FN_PROTOTYPE (x) 		ALM_PROTO_ZEN2(x)
+#define  FN_PROTOTYPE_OPT(x) 		ALM_PROTO_ZEN2(x)
+
+#include "../../optmized/exp.c"
+

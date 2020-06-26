b51dc77229263ecb27c0edb66295448631bc966e
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-06-26 12:23:12 +0530

    libm: add duplicate ALM_PROTO to header
    Change-Id: Ia711682a7f189ad9483cc32226841093c5055b76
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1015]

--- include/libm_macros.h
+++ include/libm_macros.h
@@ -5,11 +5,6 @@
 #ifndef __LIBM_MACROS_H__
 #define __LIBM_MACROS_H__
 
-#define INC_REF(x)    <c/x>
-#define INC_AVX(x)    <avx/x>
-#define INC_AVX2(x)   <avx2/x>
-#define INC_AVX512(x) <avx512/x>
-
 /*
  * Spare the ugliness to non-coders
  */
@@ -20,16 +15,48 @@
 #endif
 
 #ifndef ALM_OVERRIDE
-#define FN_PROTOTYPE(fn_name) amd_##fn_name
 
-#define FN_PROTOTYPE_OPT(fn_name) amd_opt_##fn_name
+#define ALM_PREFIX      		amd
+
+#define FN_PROTOTYPE(fn_name)           amd_##fn_name
+
+/*
+ * ALM_PROTO_OPT to be removed, once all the FN_PROTOTYPE_OPT is converted to ALM_PROTO
+ */
+#define ALM_PROTO_OPT(fn_name)	amd_opt_##fn_name
+
 
 #endif /* ALM_OVERRIDE */
 
-#define FN_PROTOTYPE_REF(fn_name) amd_ref_##fn_name
+#if defined(ALM_SUFFIX)
+#define ALM_PROTO(x)      ALM_MAKE_PROTO_SFX(ALM_PREFIX, x, ALM_SUFFIX)
+#else
+#define ALM_PROTO(x)      ALM_MAKE_PROTO(ALM_PREFIX, x)
+#endif
 
-#define FN_PROTOTYPE_FAST(f) amd_fast##f
+#define ALM_MAKE_PROTO_SFX(pfx, fn, sfx)        __ALM_MAKE_PROTO_SFX(pfx, fn, sfx)
+#define ALM_MAKE_PROTO(pfx, fn)                 __ALM_MAKE_PROTO(pfx, fn)
 
+#define __ALM_MAKE_PROTO_SFX(pfx, fn, sfx)        pfx##_##fn##_##sfx
+#define __ALM_MAKE_PROTO(pfx, fn)                 pfx##_##fn
+
+#define FN_PROTOTYPE_OPT(fn)            amd_opt_##fn
+#define FN_PROTOTYPE_REF(fn)            amd_ref_##fn
+#define FN_PROTOTYPE_FAST(fn)           amd_fast##fn
+
+#define ALM_PROTO_REF(fn)    		amd_ref_##fn
+#define ALM_PROTO_FAST(fn)   		amd_fast##fn
+
+/* 64-bit variants */
+#define ALM_PROTO_BAS64(fn)	__amd_bas64_##fn
+/* Buldozer specific, FMA3 and FMA4 */
+#define ALM_PROTO_BDOZR(fn)	__amd_bdozr_##fn
+/* Generic FMA3 */
+#define ALM_PROTO_FMA3(fn)	__amd_fma3_##fn
+/* AVX2 versions */
+#define ALM_PROTO_AVX2(fn)	__amd_avx2_##fn
+/* AVX512 versions */
+#define ALM_PROTO_AVX512(fn)	__amd_avx512_##fn
 
 
 #define FN_PROTOTYPE_BAS64(fn_name) __amd_bas64_##fn_name
--- src/arch/zen2/exp.c
+++ src/arch/zen2/exp.c
@@ -8,7 +8,12 @@
 
 #define  ALM_PROTO_ZEN2(x)		ALM_ARCH_FN(amd_##x)
 #define  FN_PROTOTYPE (x) 		ALM_PROTO_ZEN2(x)
+
+#ifdef FN_PROTOTYPE_OPT
+#undef FN_PROTOTYPE_OPT
 #define  FN_PROTOTYPE_OPT(x) 		ALM_PROTO_ZEN2(x)
+#endif
+
 
 #include "../../optmized/exp.c"
 

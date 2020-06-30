11b5261ac527d931c4a365dba71c852eaada3042
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-06-30 12:19:24 +0530

    libm: add new arch specific zen2.h
    Change-Id: I24a573141f2b26bfc994a6fa54b04315899e2d22
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1015]

---
+++ include/libm/arch/zen2.h
@@ -0,0 +1,17 @@
+#ifndef __ALM_ARCH_ZEN2_H__
+#define __ALM_ARCH_ZEN2_H__
+
+#define  ALM_OVERRIDE 1
+
+#define ALM_PREFIX			amd
+#define ALM_ARCH			zn2
+
+#define  ALM_PROTO_OPT(x) 		ALM_PROTO_ARCH(ALM_PREFIX, x, ALM_ARCH)
+
+#define  ALM_PROTO_ARCH(a, x, y)	ALM_MAKE_PROTO_SFX(a, x, y)
+
+#include <libm_macros.h>
+#include <libm/types.h>
+
+#endif
+

5c4534737b350b600b0f209e354366671d186d74
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:45 +0530

    libm: tests: convert mparith to mpfr for rint.c
    Change-Id: I94bccbfb64ec48500c24d8e70ab63c9cac0727d0
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/rint.c
+++ tests/libs/mparith/rint.c
@@ -11,31 +11,7 @@
 #error
 #endif
 
-REAL FUNC_RINT(REAL x)
-{
-    REAL y;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp;
-    int ifail;
-
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    /* Sets YMP to be the RINT function of XMP. */
-    MPRINT(xmp, ymp, params, &ifail);
-
-    MPTOD(ymp, params, &y, &ifail);
-
-    free(xmp);
-    free(ymp);
-
-    return y;
-}
-
-REAL FUNC_RINT_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
+REAL FUNC_RINT_ULP123(REAL x,REAL z, double   *sulps, double   *sreldiff)
 {
     REAL y;
     fp_params params;
@@ -63,6 +39,37 @@ REAL FUNC_RINT_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
     free(xmp);
     free(ymp);
 
+    return y;
+}
+
+
+
+#include <mpfr.h>
+
+REAL FUNC_RINT(REAL x)
+{
+    REAL y;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mp_rop;
+
+    mpfr_inits2(256, mpx, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+#endif
+
+    mpfr_rint(mp_rop, mpx, rnd);
+
+#if defined(FLOAT)
+    y = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    y = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mp_rop, (mpfr_ptr) 0);
     return y;
 }
 

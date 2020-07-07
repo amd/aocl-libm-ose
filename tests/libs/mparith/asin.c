fa3d9deec4efcd85fe0debb18e5e159fd1e1928c
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:08 +0530

    libm: tests: convert mparith to mpfr for asin.c
    Change-Id: I7b2ebd1276be85ef9dd37ff334403a1c8873f302
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/asin.c
+++ tests/libs/mparith/asin.c
@@ -11,31 +11,7 @@
 #error
 #endif
 
-REAL FUNC_ASIN(REAL x)
-{
-    REAL y;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp;
-    int ifail;
-
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    MPASIN(xmp, params, ymp, &ifail);
-
-    MPTOD(ymp, params, &y, &ifail);
-
-    free(xmp);
-    free(ymp);
-
-    return y;
-}
-
-
-REAL FUNC_ASIN_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
+REAL FUNC_ASIN_ULP123(REAL x,REAL z, double   *sulps, double   *sreldiff)
 {
     REAL y;
     fp_params params;
@@ -63,3 +39,34 @@ REAL FUNC_ASIN_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
 
     return y;
 }
+
+
+#include <mpfr.h>
+
+REAL FUNC_ASIN(REAL x)
+{
+    REAL y;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mp_rop;
+
+    mpfr_inits2(256, mpx, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+#endif
+
+    mpfr_asin(mp_rop, mpx, rnd);
+
+#if defined(FLOAT)
+    y = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    y = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mp_rop, (mpfr_ptr) 0);
+    return y;
+}
+

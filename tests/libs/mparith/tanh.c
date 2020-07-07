5ac8abd7e7534c9c07ee12c5702fb9054a629683
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:52 +0530

    libm: tests: convert mparith to mpfr for tanh.c
    Change-Id: I2c153f67ab51e36b0fad827918f94ed25233840c
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/tanh.c
+++ tests/libs/mparith/tanh.c
@@ -11,32 +11,7 @@
 #error
 #endif
 
-REAL FUNC_TANH(REAL x)
-{
-    REAL y;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp;
-    int ifail;
-
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    MPTANH(xmp, params, ymp, &ifail);
-
-
-
-    MPTOD(ymp, params, &y, &ifail);
-
-    free(xmp);
-    free(ymp);
-
-    return y;
-}
-
-REAL FUNC_TANH_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
+REAL FUNC_TANH_ULP123(REAL x,REAL z, double   *sulps, double   *sreldiff)
 {
     REAL y;
     fp_params params;
@@ -66,3 +41,34 @@ REAL FUNC_TANH_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
 
     return y;
 }
+
+
+#include <mpfr.h>
+
+REAL FUNC_TANH(REAL x)
+{
+    REAL y;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mp_rop;
+
+    mpfr_inits2(256, mpx, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+#endif
+
+    mpfr_tanh(mp_rop, mpx, rnd);
+
+#if defined(FLOAT)
+    y = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    y = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mp_rop, (mpfr_ptr) 0);
+    return y;
+}
+

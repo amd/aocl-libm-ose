f06e45a567e798e53a3b0e7499360786e4308d2a
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:28 +0530

    libm: tests: convert mparith to mpfr for fma.c
    Change-Id: I9e5897679131443a52e094063b3d3c869e774dab
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/fma.c
+++ tests/libs/mparith/fma.c
@@ -11,36 +11,7 @@
 #error
 #endif
 
-REAL FUNC_FMA(REAL x, REAL y, REAL z)
-{
-    REAL ret;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp, *zmp, *result;
-    int ifail;
-
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-    zmp = new_mp(params);
-    result = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    DTOMP(y, ymp, params, &ifail);
-    DTOMP(z, zmp, params, &ifail);
-    MPFMA(xmp, ymp, zmp, result, params, &ifail);
-
-    MPTOD(result, params, &ret, &ifail);
-
-    free(xmp);
-    free(ymp);
-    free(zmp);
-    free(result);
-
-    return ret;
-}
-
-REAL FUNC_FMA_ULP(REAL x, REAL y, REAL z,REAL re, double   *sulps, double   *sreldiff)
+REAL FUNC_FMA_ULP123(REAL x, REAL y, REAL z,REAL re, double   *sulps, double   *sreldiff)
 {
     REAL ret;
     fp_params params;
@@ -75,3 +46,39 @@ REAL FUNC_FMA_ULP(REAL x, REAL y, REAL z,REAL re, double   *sulps, double   *sre
 
     return ret;
 }
+
+
+#include <mpfr.h>
+
+REAL FUNC_FMA(REAL x, REAL y, REAL z)
+{
+    REAL r;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mpy, mpz, mp_rop;
+
+    mpfr_inits2(256, mpx, mpy, mpz, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+    mpfr_set_flt(mpy, y, rnd);
+    mpfr_set_flt(mpz, z, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+    mpfr_set_d(mpy, y, rnd);
+    mpfr_set_d(mpz, z, rnd);
+#endif
+
+    mpfr_fma(mp_rop, mpx, mpy, mpz, rnd);
+
+#if defined(FLOAT)
+    r = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    r = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mpy, mpz, mp_rop, (mpfr_ptr) 0);
+    return r;
+}
+
+

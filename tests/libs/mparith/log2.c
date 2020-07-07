88d629777fb770707073ce9c22ffd9f38964f497
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:38 +0530

    libm: tests: convert mparith to mpfr for log2.c
    Change-Id: I1345989b81943907c81805f8f7bb107887ec82cb
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/log2.c
+++ tests/libs/mparith/log2.c
@@ -11,30 +11,7 @@
 #error
 #endif
 
-REAL FUNC_LOG2(REAL x)
-{
-    REAL y;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp;
-    int ifail;
-
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    MPLOG2(xmp, params, ymp, &ifail);
-
-    MPTOD(ymp, params, &y, &ifail);
-
-    free(xmp);
-    free(ymp);
-
-    return y;
-}
-
-REAL FUNC_LOG2_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
+REAL FUNC_LOG2_ULP123(REAL x,REAL z, double   *sulps, double   *sreldiff)
 {
     REAL y;
     fp_params params;
@@ -62,3 +39,34 @@ REAL FUNC_LOG2_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
 
     return y;
 }
+
+
+#include <mpfr.h>
+
+REAL FUNC_LOG2(REAL x)
+{
+    REAL y;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mp_rop;
+
+    mpfr_inits2(256, mpx, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+#endif
+
+    mpfr_log2(mp_rop, mpx, rnd);
+
+#if defined(FLOAT)
+    y = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    y = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mp_rop, (mpfr_ptr) 0);
+    return y;
+}
+

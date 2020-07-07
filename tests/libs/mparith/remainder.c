eafd7e3684c16372d569c002b3a3905c6d173fa4
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:42 +0530

    libm: tests: convert mparith to mpfr for remainder.c
    Change-Id: I67cfc43cba609aa869792ef74e7cceb9cc5d818b
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/remainder.c
+++ tests/libs/mparith/remainder.c
@@ -11,34 +11,8 @@
 #error
 #endif
 
-REAL FUNC_REMAINDER(REAL x, REAL y)
-{
-    REAL ret;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp, *result;
-    int ifail;
 
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-    result = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    DTOMP(y, ymp, params, &ifail);
-    /* Computes remainder(XMP, YMP), result in RESULT */
-    MPREMAINDER(xmp, ymp, params, result, &ifail);
-
-    MPTOD(result, params, &ret, &ifail);
-
-    free(xmp);
-    free(ymp);
-    free(result);
-
-    return ret;
-}
-
-REAL FUNC_REMAINDER_ULP(REAL x, REAL y,REAL z, double   *sulps, double   *sreldiff)
+REAL FUNC_REMAINDER_ULP123(REAL x, REAL y,REAL z, double   *sulps, double   *sreldiff)
 {
     REAL ret;
     fp_params params;
@@ -71,3 +45,35 @@ REAL FUNC_REMAINDER_ULP(REAL x, REAL y,REAL z, double   *sulps, double   *sreldi
 
     return ret;
 }
+
+#include <mpfr.h>
+
+REAL FUNC_REMAINDER(REAL x, REAL y)
+{
+    REAL y1;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mpy, mp_rop;
+
+    mpfr_inits2(256, mpx, mpy, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+    mpfr_set_flt(mpy, y, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+    mpfr_set_d(mpy, y, rnd);
+#endif
+
+    mpfr_remainder(mp_rop, mpx, mpy, rnd);
+
+#if defined(FLOAT)
+    y1 = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    y1 = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mpy, mp_rop, (mpfr_ptr) 0);
+    return y1;
+}
+

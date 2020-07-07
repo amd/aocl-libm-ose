3fa1cc6a914db3ae17b46e862286ed87b81af400
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-07 12:39:23 +0530

    libm: tests: convert mparith to mpfr for exp10.c
    Change-Id: Id3f53edb7f6192ebfa464e427776814c307deaf9
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-1022]

--- tests/libs/mparith/exp10.c
+++ tests/libs/mparith/exp10.c
@@ -11,30 +11,8 @@
 #error
 #endif
 
-REAL FUNC_EXP10(REAL x)
-{
-    REAL y;
-    fp_params params;
-    int base, mantis, emin, emax;
-    int *xmp, *ymp;
-    int ifail;
 
-    initMultiPrecision(ISDOUBLE, 0, &base, &mantis, &emin, &emax, &params);
-    xmp = new_mp(params);
-    ymp = new_mp(params);
-
-    DTOMP(x, xmp, params, &ifail);
-    MPEXP10(xmp, params, ymp, &ifail);
-
-    MPTOD(ymp, params, &y, &ifail);
-
-    free(xmp);
-    free(ymp);
-
-    return y;
-}
-
-REAL FUNC_EXP10_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
+REAL FUNC_EXP10_ULP123(REAL x,REAL z, double   *sulps, double   *sreldiff)
 {
     REAL y;
     fp_params params;
@@ -63,3 +41,33 @@ REAL FUNC_EXP10_ULP(REAL x,REAL z, double   *sulps, double   *sreldiff)
 
     return y;
 }
+
+#include <mpfr.h>
+
+REAL FUNC_EXP10(REAL x)
+{
+    REAL y;
+
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_t mpx, mp_rop;
+
+    mpfr_inits2(256, mpx, mp_rop, (mpfr_ptr) 0);
+
+#if defined(FLOAT)
+    mpfr_set_flt(mpx, x, rnd);
+#elif defined(DOUBLE)
+    mpfr_set_d(mpx, x, rnd);
+#endif
+
+    mpfr_exp10(mp_rop, mpx, rnd);
+
+#if defined(FLOAT)
+    y = mpfr_get_flt(mp_rop, rnd);
+#elif defined(DOUBLE)
+    y = mpfr_get_d(mp_rop, rnd);
+#endif
+
+    mpfr_clears (mpx, mp_rop, (mpfr_ptr) 0);
+    return y;
+}
+

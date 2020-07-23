8895c83a192b2450aef68e6e48763694629a981f
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-23 15:31:03 +0530

    libm: poly.h fix indentation
    Change-Id: Ib696660006153b47444b08512feeaf232afb9578
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-630]

--- include/libm/poly.h
+++ include/libm/poly.h
@@ -43,19 +43,44 @@
  *                      ((C2+C3*x)*x2 + (C0+C1*x));
  */
 #define POLY_EVAL_9(r, c0, c1, c2, c3, c4, c5, c6, c7, c8) ({   \
-	__typeof(r) a1, a2, a3, a4, b1, b2, q;			\
-	__typeof(r) r2, r4;					\
-	a1 = c2*r + c1;						\
-	a2 = c4*r + c3;						\
-	r2 = r * r;						\
-	a3 = c6*r + c5;						\
-	r4 = r2 * r2;						\
-	a4 = c8*r + c7;						\
+                        __typeof(r) a1, a2, a3, a4, b1, b2, q;  \
+                        __typeof(r) r2, r4;                     \
+                        a1 = c2*r + c1;                         \
+                        a2 = c4*r + c3;                         \
+                        r2 = r * r;                             \
+                        a3 = c6*r + c5;                         \
+                        r4 = r2 * r2;                           \
+                        a4 = c8*r + c7;                         \
                                                                 \
-	b1 = a4*r4 + a3*r2 + a2;				\
-	b2 = a1*r2 + c0*r;					\
-	q = b1*r4 + b2;						\
-	q;							\
-	})
+                        b1 = a4*r4 + a3*r2 + a2;                \
+                        b2 = a1*r2 + c0*r;                      \
+                        q = b1*r4 + b2;                         \
+                        q;                                      \
+                })
 
-#endif	/* LIBM_POLY_H */
+
+/*
+ * poly = x * (C1 + C2*x^2 + C3*x^4 + C4*x^6 + C5*x^8 + \
+ *              C6*x^10 + C7*x^12)
+ */
+#define POLY_EVAL_EVEN_8(r, c1, c2, c3, c4, c5, c6, c7, c8) ({          \
+                        __typeof(r) a1, a2, a3, a4, b1, b2 ,q;          \
+                        __typeof(r) r2, r4, r8, r12;                    \
+                        r2 = r * r;                                     \
+                        r4 = r2 * r2;                                   \
+                        r8 = r4 * r4;                                   \
+                        r12 = r8 * r4;                                  \
+                        a1 = c1 + c2*r2;                                \
+                        a2 = c3 + c4*r2;                                \
+                        a3 = c5 + c6*r2;                                \
+                        a4 = c7 + c8*r2;                                \
+                                                                        \
+                        b1 = a1 + a2*r4;                                \
+                        b2 = r8*a3 + r12*a4;                            \
+                                                                        \
+                        q = r*(b1 + b2);                                \
+                        q;                                              \
+                })
+
+
+#endif  /* LIBM_POLY_H */

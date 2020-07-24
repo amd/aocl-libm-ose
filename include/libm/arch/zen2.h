0514ad05509370933e29f4b5cca92e5e202d8e2a
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-07-24 15:32:30 +0530

    libm: fix up zen2.h
    Change-Id: Icc7554ed040baddf1b61e8047dc4b99eade525bd
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-630]

--- include/libm/arch/zen2.h
+++ include/libm/arch/zen2.h
@@ -4,7 +4,16 @@
 /*
  * This architecture is for "Zen2", same as --march=znver2
  */
-#define ALM_ARCH			zn2
+#define ALM_ARCH_ZN2			zn2
+#define ALM_PREFIX_ZN2			amd
+
+#ifndef ALM_PREFIX
+#define ALM_PREFIX		ALM_PREFIX_ZN2
+#endif
+
+#ifndef ALM_ARCH
+#define ALM_ARCH		ALM_ARCH_ZN2
+#endif
 
 /*
  * Override all previously definded prototypes
@@ -14,29 +23,45 @@
 #include <libm_macros.h>
 #include <libm/types.h>
 
-#ifndef  ALM_PREFIX
-#define  ALM_PREFIX			amd
+#ifndef  ALM_PREFIX_ZN2
+#undef   ALM_PREFIX_ZN2
+#define  ALM_PREFIX_ZN2			amd
 #endif
 
 #ifdef FN_PROTOTYPE_OPT
 #undef FN_PROTOTYPE_OPT
-#define FN_PROTOTYPE_OPT(x)		ALM_PROTO_OPT(x)
 #endif
+#define FN_PROTOTYPE_OPT(x)		ALM_PROTO_OPT(x)
 
 #ifdef FN_PROTOTYPE_REF
 #undef FN_PROTOTYPE_REF
+#endif
 #define FN_PROTOTYPE_REF(x)		ALM_PROTO_ARCH_ZN2(x)
 
 #define  ALM_PROTO_OPT(x) 		ALM_PROTO_ARCH_ZN2(x)
 
-#endif
 
 #endif /* ALM_OVERRIDE */
 
 
+#ifndef ALM_PROTO_ARCH
 #define __ALM_PROTO_ARCH(a, x, y)	ALM_MAKE_PROTO_SFX(a, x, y)
 #define  ALM_PROTO_ARCH(a, x, y) 	__ALM_PROTO_ARCH(a, x, y)		
-#define  ALM_PROTO_ARCH_ZN2(x)		ALM_PROTO_ARCH(ALM_PREFIX, x, ALM_ARCH)
-
 #endif
+
+#define  ALM_PROTO_ARCH_ZN2(x)		ALM_PROTO_ARCH(ALM_PREFIX_ZN2, x, ALM_ARCH_ZN2)
+
+#ifdef  ALM_PROTO
+#undef  ALM_PROTO
+#define ALM_PROTO		ALM_PROTO_ARCH_ZN2
+
+#define ALM_PROTO_CHANGE_INTERNAL  1
+#include <libm/arch/alm_funcs.h>
+
+#undef  ALM_PROTO
+#undef  ALM_PROTO_CHANGE_INTENRAL
+
+#endif /* ALM_PROTO */
+
+#endif  /* _ALM_ARCH_ZEN2_H_ */
 

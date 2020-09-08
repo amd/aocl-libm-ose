c71f9300d7b0e6b11bcc8d57c4e9b6ddd6af3eb2
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-09-08 13:47:55 +0530

    libm: poly.h: adding 15 degree odd-term polynomial
    Change-Id: I4759dcfc1f9e7a5267a28c3c2728cbdf8141828c
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- include/libm/poly.h
+++ include/libm/poly.h
@@ -64,7 +64,7 @@
  *              C6*x^10 + C7*x^12 + C8*x^14)
  * 15 degree polynomial with only even terms
  */
-#define POLY_EVAL_EVEN_15(r, c1, c2, c3, c4, c5, c6, c7, c8) ({          \
+#define POLY_EVAL_EVEN_15(r, c1, c2, c3, c4, c5, c6, c7, c8) ({         \
                         __typeof(r) a1, a2, a3, a4, b1, b2 ,q;          \
                         __typeof(r) r2, r4, r8, r12;                    \
                         r2 = r * r;                                     \
@@ -85,16 +85,42 @@
 
 
 /*
+ * poly = x + (C1*x^3 + C2*x^5 + C3*x^7 + C4*x^9 + C5*x^11 + \
+ *              C6*x^13 + C7*x^15)
+ *      = x + x3*(C1 + C2*x^2 + C3*x^4 + C4*x^6 + C5*x^8 + \
+ *              C6*x^10 + C7*x^12)
+ * 15 degree polynomial with only odd terms
+ */
+#define POLY_EVAL_ODD_15(r, c1, c2, c3, c4, c5, c6, c7) ({              \
+            __typeof(r) a1, a2, a3, b1, b2 ,q;                          \
+            __typeof(r) r2, r4, r8, r12;                                \
+            r2 = r * r;                                                 \
+            r4 = r2 * r2;                                               \
+            r8 = r4 * r4;                                               \
+            r12 = r8 * r4;                                              \
+            a1 = c1 + c2*r2;                                            \
+            a2 = c3 + c4*r2;                                            \
+            a3 = c5 + c6*r2;                                            \
+                                                                        \
+            b1 = a1 + a2*r4;                                            \
+            b2 = r8*a3 + r12*c7;                                        \
+                                                                        \
+            q = r + r * r2 * (b1 + b2);                                 \
+            q;                                                          \
+        })
+
+
+/*
  * poly = C0 + C1*r^3 + C2*r^5 + C3*r^7+ C4 *r^9 \
  *
  *      = C0 + r^2*(C1 + C2*r^2) + r^4*(C3*r^2+C4*r^4)
- *    
+ *
  */
 #define POLY_EVAL_ODD_9(r, c0, c1, c2, c3, c4) ({               \
         __typeof(r) a0, a1, a2, q;                              \
         __typeof(r) r2, r4;                                     \
-        r2 = r * r;	                                        \
-        r4 = r2 * r2;   	                                \
+        r2 = r * r;                                             \
+        r4 = r2 * r2;                                           \
         a0 = c2*r2 + c1;                                        \
         a1 = a0*r2 + c0;                                        \
         a2 = (c3*r2 + c4*r4)*r4;                                \

ff6284e946db2676b83426b8a7829d1989af5b82
Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
2020-09-11 14:33:15 +0530

    libm:vrs8_sinf: Added C implementation of vrs8 sinf()
    1. Added C implementation of vrs8 sinf using only single
       precision variables for computation.
    2. Maximum ULP is 3.0
    
    Change-Id: Icdc4625b57dd84b7e2902b14e2effa4282b847b9
    Signed-off-by: Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
    AMD-Internal:[CPUPL-1172]

--- src/optmized/vec/vrs8_sinf.c
+++ src/optmized/vec/vrs8_sinf.c
@@ -36,48 +36,46 @@
 */
 
 static struct {
-    v_f64x4_t invpi, pi, pi1, pi2;
-    v_f64x4_t shift;
-    v_u32x8_t infinity, mask32;
-    v_u64x4_t sign_mask;
-    v_f64x4_t poly_sinf[5];
- } sinf_data = {
-     .sign_mask = _MM_SET1_I64(0x7FFFFFFFFFFFFFFF),
+    v_f32x8_t invpi, pi1, pi2, pi3;
+    v_f32x8_t shift;
+    v_u32x8_t mask32;
+    v_u32x8_t max_arg;
+    v_f32x8_t poly_sinf[5];
+ } v8_sinf_data = {
+     .max_arg = _MM256_SET1_I32(0x4A989680),
      .mask32    = _MM256_SET1_I32(0x7fffffff),
-     .infinity  = _MM256_SET1_I32(0x7f800000),
-     .pi    = _MM_SET1_PD4(0x1.921fb54442d18p1),
-     .pi1   = _MM_SET1_PD4(0x1.921fb50000000p1),
-     .pi2   = _MM_SET1_PD4(0x1.110b4611a6263p-25),
-     .invpi = _MM_SET1_PD4(0x1.45f306dc9c883p-2),
-     .shift = _MM_SET1_PD4(0x1.8p+52),
+     .pi1    = _MM256_SET1_PS8(-0x1.921fb6p1),
+     .pi2   = _MM256_SET1_PS8(0x1.777a5cp-24),
+     .pi3   = _MM256_SET1_PS8(0x1.ee59dap-49),
+     .invpi = _MM256_SET1_PS8(0x1.45f306p-2),
+     .shift = _MM256_SET1_PS8(0x1.8p23),
      /*
       * Polynomial coefficients obtained using Remez algorithm from Sollya
       */
      .poly_sinf = {
-         _MM_SET1_PD4(1.0),
-         _MM_SET1_PD4(-0x1.55554d018df8bp-3),
-         _MM_SET1_PD4(0x1.110f0293a5dcbp-7),
-         _MM_SET1_PD4(-0x1.9f781a0aebdb9p-13),
-         _MM_SET1_PD4(0x1.5e2a3e7550c85p-19)
+         _MM256_SET1_PS8(0x1.p0),
+         _MM256_SET1_PS8(-0x1.555548p-3),
+         _MM256_SET1_PS8(0x1.110e7cp-7),
+         _MM256_SET1_PS8(-0x1.9f6446p-13),
+         _MM256_SET1_PS8(0x1.5d38b6p-19)
      },
 };
 
 
-#define pi      sinf_data.pi
-#define pi1     sinf_data.pi1
-#define pi2     sinf_data.pi2
-#define invpi   sinf_data.invpi
-#define ALM_SHIFT   sinf_data.shift
-#define ARG_MAX sinf_data.infinity
+#define V8_SINF_PI1       v8_sinf_data.pi1
+#define V8_SINF_PI2       v8_sinf_data.pi2
+#define V8_SINF_PI3       v8_sinf_data.pi3
+#define V8_SINF_INVPI     v8_sinf_data.invpi
+#define ALM_SHIFT_SP      v8_sinf_data.shift
+#define V8_SINF_ARG_MAX   v8_sinf_data.max_arg
+#define ALM_SIGN_MASK32   v8_sinf_data.mask32
 
-#define C1  sinf_data.poly_sinf[0]
-#define C3  sinf_data.poly_sinf[1]
-#define C5  sinf_data.poly_sinf[2]
-#define C7  sinf_data.poly_sinf[3]
-#define C9  sinf_data.poly_sinf[4]
+#define C1  v8_sinf_data.poly_sinf[0]
+#define C3  v8_sinf_data.poly_sinf[1]
+#define C5  v8_sinf_data.poly_sinf[2]
+#define C7  v8_sinf_data.poly_sinf[3]
+#define C9  v8_sinf_data.poly_sinf[4]
 
-#define SIGN_MASK sinf_data.sign_mask
-#define SIGN_MASK32 sinf_data.mask32
 
 float ALM_PROTO(sinf)(float);
 
@@ -94,65 +92,48 @@ v_f32x8_t
 ALM_PROTO_OPT(vrs8_sinf)(v_f32x8_t x)
 {
 
-    v_f64x4_t xd, F, poly, result;
+    v_f32x8_t r, F, poly, result;
 
-    v_u64x4_t n;
+    v_u32x8_t n;
 
-    v_u64x4_t sign, uxd;
+    v_u32x8_t sign;
 
     v_u32x8_t ux = as_v8_u32_f32(x);
 
-    v_u32x8_t cmp = (ux & SIGN_MASK32) > (ARG_MAX);
+    sign = ux & ~ALM_SIGN_MASK32;
 
-    v_f32x4_t _x[2];
+    v_u32x8_t cmp = (ux & ~ALM_SIGN_MASK32) > (V8_SINF_ARG_MAX);
 
-    v_f32x4_t ret_array[2];
+    r  = as_v8_f32_u32(ux & ~ALM_SIGN_MASK32);
 
-    _x[0] = _mm256_extractf128_ps(x, 0);
+    v_f32x8_t dn =  r * V8_SINF_INVPI + ALM_SHIFT_SP;
 
-    _x[1] = _mm256_extractf128_ps(x, 1);
+    n = as_v8_u32_f32(dn);
 
-    for(int lane = 0; lane < 2; lane++) {
+    dn -= ALM_SHIFT_SP;
 
-        xd = cast_v4_f32_to_f64(_x[lane]);
+    F = r + dn * V8_SINF_PI1;
 
-        uxd = as_v4_u64_f64(xd);
+    F = F + dn * V8_SINF_PI2;
 
-        sign = uxd & (~SIGN_MASK);
+    F = F + dn * V8_SINF_PI3;
 
-        xd  = as_v4_f64_u64(uxd & (SIGN_MASK));
+    v_u32x8_t odd =  n << 31;
 
-        v_f64x4_t dn =  xd * invpi + ALM_SHIFT;
+    /*
+     *  Calculate the polynomial approximation x*(1+C1*x^2+C2*x^4+C3*x^6+C4*x^8)
+     *
+     */
 
-        n = cast_v4_f64_to_i64(dn);
+    poly = POLY_EVAL_ODD_9(F, C1, C3, C5, C7, C9);
 
-        dn -= ALM_SHIFT;
-
-        F = xd - dn*pi1;
-
-        F = F - dn*pi2;
-
-        v_u64x4_t odd =  n << 63;
-
-        /*
-        *  Calculate the polynomial approximation x*(1+C1*x^2+C2*x^4+C3*x^6+C4*x^8)
-        *
-        *
-        */
-
-        poly = POLY_EVAL_ODD_9(F, C1, C3, C5, C7, C9);
-
-        result = as_v4_f64_u64(as_v4_u64_f64(poly) ^ sign ^ odd);
-
-        ret_array[lane] = cvt_v4_f64_to_f32(result);
-
-    }
-
-    v_f32x8_t ret =  _mm256_setr_m128(ret_array[0], ret_array[1]);
+    result = as_v8_f32_u32(as_v8_u32_f32(poly) ^ sign ^ odd);
 
     if(unlikely(any_v8_u32_loop(cmp))) {
-        return sinf_specialcase(x, ret, cmp);
+
+        return sinf_specialcase(x, result, cmp);
+
     }
 
-    return ret;
+    return result;
 }

57b0653548e324bb2cdf2f2fc3aa077900b1972b
Prem Mallappa <Premachandra.Mallappa@amd.com>
2020-09-14 14:15:42 +0530

    libm: adding alm_special.c to handle vector special cases
    Change-Id: If40a177863e8bfba63021aaaa8897b459c993a7a
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [CPUPL-764]

--- src/optmized/vec/alm_special.c
+++ src/optmized/vec/alm_special.c
@@ -7,27 +7,14 @@
 #include <libm/typehelper.h>
 #include <libm/amd_funcs_internal.h>
 
-#define  ALM_TANF_SIGN_MASK   ~(1UL<<63)
-#define  ALM_TANF_SIGN_MASK32 ~(1U<<31)
-
-float tanf_specialcase(float x)
+float
+tanf_oddcase(float result)
 {
-    uint32_t   ux = asuint32(x);
+    return -1.0/result;
+}
 
-    if(unlikely((ux - asuint32(0x1p-126)) >
-                (asuint32(0x1p+127) - asuint32(0x1p-126)))) {
-
-        if((ux  & ALM_TANF_SIGN_MASK32) >= 0x7f800000) {
-            /*  infinity or NaN */
-            return _tanf_special(x);
-        }
-    }
-
-    if (ux == 0)
-        return 0;
-
-    /* Result is already calculated */
-    return x;
-
-    /* return ALM_PROTO(tanf)(x); */
+float
+tanf_specialcase(float x)
+{
+    return ALM_PROTO(tanf)(x);
 }

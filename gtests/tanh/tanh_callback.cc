5bbfed4fe029b64aa530ccf8c93463f4ae03db94
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2020-10-05 11:41:25 +0530

    libm: Merged hyperbolic functions (gtest) into aocl-libm
    Change-Id: Ib4598da69539d1b864b9f37a9175c553f12f057a
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1178]

---
+++ gtests/tanh/tanh_callback.cc
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2019-2020 Advanced Micro Devices, Inc. All rights reserved
+ */
+ 
+#include <cmath>
+#include "libm_tests.h"
+#include "../../include/libm_macros.h"
+
+#define AMD_LIBM_VEC_EXPERIMENTAL
+
+#include "../../include/external/amdlibm.h"
+#include "../../include/external/amdlibm_vec.h"
+#include "../../include/libm_amd.h"
+#include "../../include/libm/amd_funcs_internal.h"
+#include <fmaintrin.h>
+#include <immintrin.h>
+#include "callback.h"
+#include "test_tanh_data.h"
+#include "../libs/mparith/alm_mp_funcs.h"
+
+float LIBM_FUNC(tanhf)(float);
+double LIBM_FUNC(tanh)(double);
+
+#if (LIBM_PROTOTYPE == PROTOTYPE_AOCL)
+/*vector routines*/
+__m128d LIBM_FUNC_VEC(d, 2, tanh)(__m128d);
+__m256d LIBM_FUNC_VEC(d, 4, tanh)(__m256d);
+
+__m128 LIBM_FUNC_VEC(s, 4, tanhf)(__m128);
+__m256 LIBM_FUNC_VEC(s, 8, tanhf)(__m256);
+#endif
+
+static uint32_t ipargs = 1;
+
+uint32_t GetnIpArgs( void )
+{
+	return ipargs;
+}
+
+void SpecSetupf32(SpecParams *specp) {
+  specp->data32 = test_tanhf_special_data;
+  specp->countf = ARRAY_SIZE(test_tanhf_special_data); 
+}
+
+void SpecSetupf64(SpecParams *specp) {
+  specp->data64 = test_tanh_special_data;
+  specp->countd = ARRAY_SIZE(test_tanh_special_data); 
+}
+
+void ConfSetupf32(SpecParams *specp) {
+  specp->data32 = test_tanhf_conformance_data;
+  specp->countf = ARRAY_SIZE(test_tanhf_conformance_data); 
+}
+
+void ConfSetupf64(SpecParams *specp) {
+  specp->data64 = test_tanh_conformance_data;
+  specp->countd = ARRAY_SIZE(test_tanh_conformance_data); 
+}
+
+float getFuncOp(float *data) {
+  return LIBM_FUNC(tanhf)(data[0]);
+}
+
+double getFuncOp(double *data) {
+  return LIBM_FUNC(tanh)(data[0]);
+}
+
+float getExpected(float *data) {
+  auto val = alm_mp_tanhf(data[0]);
+  return val;
+}
+
+double getExpected(double *data) {
+  auto val = alm_mp_tanh(data[0]);
+  return val;
+}
+
+float getGlibcOp(float *data) {
+  return tanhf(data[0]);
+}
+
+double getGlibcOp(double *data) {
+  return tanh(data[0]);
+}
+
+/**********************
+*FUNCTIONS*
+**********************/
+int test_s1s(test_data *data, int idx)  {
+  float *ip  = (float*)data->ip;
+  float *op  = (float*)data->op;
+  op[0] = LIBM_FUNC(tanhf)(ip[idx]);
+  return 0;
+}
+
+int test_s1d(test_data *data, int idx)  {
+  double *ip  = (double*)data->ip;
+  double *op  = (double*)data->op; 
+  op[0] = LIBM_FUNC(tanh)(ip[idx]);
+  return 0;
+}
+
+int test_v2d(test_data *data, int idx)  {
+#if 0 
+  double *ip  = (double*)data->ip;
+  double *op  = (double*)data->op; 
+  __m128d ip2 = _mm_set_pd(ip[idx+1], ip[idx]);
+  __m128d op2 = LIBM_FUNC_VEC(d, 2, tanh)(ip2);
+  _mm_store_pd(&op[0], op2);
+#endif  
+  return 0;
+}
+
+int test_v4s(test_data *data, int idx)  {
+#if 0 
+  float *ip  = (float*)data->ip;
+  float *op  = (float*)data->op; 
+  __m128 ip4 = _mm_set_ps(ip[idx+3], ip[idx+2], ip[idx+1], ip[idx]);
+  __m128 op4 = LIBM_FUNC_VEC(s, 4, tanhf)(ip4);
+  _mm_store_ps(&op[0], op4);
+#endif  
+  return 0;
+}
+
+int test_v4d(test_data *data, int idx)  {
+#if 0 
+  double *ip  = (double*)data->ip;
+  double *op  = (double*)data->op; 
+  __m256d ip4 = _mm256_set_pd(ip[idx+3], ip[idx+2], ip[idx+1], ip[idx]);
+  __m256d op4 = LIBM_FUNC_VEC(d, 4, tanh)(ip4);
+  _mm256_store_pd(&op[0], op4);
+#endif
+  return 0;
+}
+
+int test_v8s(test_data *data, int idx)  {
+#if 0 
+  float *ip  = (float*)data->ip;
+  float *op  = (float*)data->op; 
+  __m256 ip8 = _mm256_set_ps(ip[idx+7], ip[idx+6], ip[idx+5], ip[idx+4],
+                             ip[idx+3], ip[idx+2], ip[idx+1], ip[idx]);
+  __m256 op8 = LIBM_FUNC_VEC(s, 8, tanhf)(ip8);
+  _mm256_store_ps(&op[0], op8);
+#endif  
+  return 0;
+}
+

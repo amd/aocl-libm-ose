8c71231fa35dad698cf991ce30908735b880d80a
Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
2020-10-28 15:35:13 +0530

    libm:coshf: Added optimized C implementation of coshf
    Maximum ULP is 1
    
    Change-Id: I1e26ef6885b74264f7d9a67a9daf780240518681
    Signed-off-by: Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
    AMD-Internal:[CPUPL-1250]

---
+++ src/optmized/coshf.c
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2008-2020 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ */
+
+/*
+ * ISO-IEC-10967-2: Elementary Numerical Functions
+ * Signature:
+ *   float coshf(float x)
+ *
+ * Spec:
+ *   coshf(|x| > 89.415985107421875) = Infinity
+ *   coshf(Infinity)  = infinity
+ *   coshf(-Infinity) = infinity
+ *
+ ******************************************
+ * Implementation Notes
+ * ---------------------
+ *
+ * cosh(x) = (exp(x) + exp(-x))/2
+ * cosh(-x) = +cosh(x)
+ *
+ * checks for special cases
+ * if ( asint(x) > infinity) return x with overflow exception and
+ * return x.
+ * if x is NaN then raise invalid FP operation exception and return x.
+ *
+ * if x < 0x1p-11
+ *  coshf(x) = 1+1/2*x*x
+ * if 0x1p-11 < x < 0x1.62e43p-2
+ *
+ *  coshf = C0 + Y*Y*(C1 + Y*Y*(C2 + Y*Y*C3))
+ * if 0x1.62e43p-2 < x < 8.5
+ *  coshf = 0.5 * (exp(x) + 1/exp(x))
+ *
+ * if 8.5 < x < 0x1.62e42ep6
+ *  coshf = 0.5 * exp(x)
+ *
+ * if 0x1.62e42ep6 < x
+ *  coshf = v/2 * exp(x - log(v)) where v = 0x1.0000e8p-1
+ *
+ */
+
+#include "libm_util_amd.h"
+#include "libm_inlines_amd.h"
+#include "libm_special.h"
+
+#include <stdint.h>
+#include <libm_macros.h>
+#include <libm/types.h>
+#include <libm/typehelper.h>
+#include <libm/compiler.h>
+#include <libm/poly.h>
+//#include <libm/poly-vec.h>
+
+static struct {
+    uint32_t arg_max, infinity;
+    float logV, invV2, halfV, halfVm1;
+    float theeps, xc, ybar, wmax;
+    float poly_coshf[4];
+    float half, t1;
+ } coshf_data = {
+        .arg_max = 0x42B2D4FC,
+        .infinity = 0x7F800000,
+        .logV = 0x1.62e6p-1,
+        .invV2 =  0x1.fffc6p-3,
+        .halfVm1 = 0x1.d0112ep-17,
+        .halfV = 0x1.0000e8p0,
+        .theeps = 0x1p-11,
+        .xc = 0x1.62e43p-2,
+        .ybar =  0x1.62e42ep6,
+        .wmax =  0x1.62e0f2p6,
+        .half = 0x1p-1,
+        .t1 = 0x1p-1,
+        .poly_coshf = {
+            0x1p+0,
+            0x1p-1,
+            0x1.555466p-5,
+            0x1.6da5e2p-10,
+        },
+};
+
+#define C0 coshf_data.poly_coshf[0]
+#define C1 coshf_data.poly_coshf[1]
+#define C2 coshf_data.poly_coshf[2]
+#define C3 coshf_data.poly_coshf[3]
+
+#define LOGV    coshf_data.logV
+#define INVV2   coshf_data.invV2
+#define HALFVM1 coshf_data.halfVm1
+#define HALFV   coshf_data.halfV
+#define THEEPS  coshf_data.theeps
+#define XC      coshf_data.xc
+#define YBAR    coshf_data.ybar
+#define WMAX    coshf_data.wmax
+#define HALF    coshf_data.half
+#define T1      coshf_data.t1
+#define INF     coshf_date.infinity
+#define ARG_MAX coshf_data.arg_max
+
+float ALM_PROTO(expf)(float);
+
+float ALM_PROTO_OPT(coshf)(float x)
+{
+
+    float y, w, z, r, result;
+
+    uint32_t ux = asuint32(x) & 0x7FFFFFFF;
+
+    y = asfloat(ux);
+
+    if(unlikely(ux > ARG_MAX)) {
+
+        if (ux > PINFBITPATT_SP32) /* |x| is a NaN? */{
+
+            return x + x;
+
+        }
+        else {
+            /* x is infinity */
+            return x / 0.0;
+
+        }
+
+    }
+
+    if(ux <= asuint32(THEEPS)) {
+
+        return (1.0 + T1 * y * y);
+
+    }
+
+    if(ux > asuint32(8.5)) {
+
+        if(y > YBAR) {
+
+            w = y - LOGV;
+
+            z = ALM_PROTO(expf)(w);
+
+            return HALFV * z;
+        }
+
+        z = ALM_PROTO(expf)(y);
+
+        return (HALF * z);
+    }
+    else {
+        /*if(y > THEEPS)*/
+        if(y > XC) {
+
+            z = ALM_PROTO(expf)(y);
+
+            return HALF * (z + (1.0 / z));
+        }
+
+        /* coshf(x) = C0 + y*y*(C1 + y*y*(C2 + y*y*C3))  */
+        r = y * y;
+
+        result = C0 + r * (C1 + r * (C2 + r * C3));
+
+        return (result);
+
+    }
+
+}
+
+

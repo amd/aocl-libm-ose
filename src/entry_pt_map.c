e2be0a9e4cd28d6346457d8e6cbb07d722436417
Nimmy Krishnan <nimmy.krishnan@amd.com>
2020-11-11 16:17:59 +0530

    libm:vrs8_tanhf: Adding signature of C implementation of vrs8 tanhf to internal files
    Adding signature of vrs8 tanhf C implementation to
    1. include/libm/__alm_func_internal.h
    2. Entry point files
    
    Change-Id: Iab444b0c84e3dd44e870ee0c24d0b97425795700
    Signed-off-by: Krishnan, Nimmy <nimmy.krishnan@amd.com>
    AMD-Internal:[CPUPL-1246]

--- src/entry_pt_map.c
+++ src/entry_pt_map.c
@@ -235,6 +235,7 @@ LIBM_DECL_FN_MAP(vrs8_sinf);
 LIBM_DECL_FN_MAP(vrs8_tanf);
 LIBM_DECL_FN_MAP(vrs8_cosf);
 LIBM_DECL_FN_MAP(vrs8_coshf);
+LIBM_DECL_FN_MAP(vrs8_tanhf);
 
 LIBM_DECL_FN_MAP(vrda_cbrt);
 LIBM_DECL_FN_MAP(vrda_cos);

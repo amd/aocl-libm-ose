33c5fc232b9482f323cefa09b1385a17fb7e5a47
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2020-12-23 18:18:01 +0530

    libm:dynamic load: expp vector array variants
    Change-Id: I1525b1cb828bf6a6da144ae6c7292fc59f0db4d8
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1387]<F12>

--- gtests/dynamic/src/test_exp.c
+++ gtests/dynamic/src/test_exp.c
@@ -1,8 +1,13 @@
 #include "libm_dynamic_load.h"
 
+#define RANGEF GenerateRangeFloat(-100.0, 100.0)
+#define RANGED GenerateRangeDouble(-100.0, 100.0)
+
 int test_exp(void* handle) {
     char* error;
     int i;
+    int dim=5, loopCount=10;
+    int array_size = dim * loopCount;
 
     float (*lamd_expf)(float);
     double (*lamd_exp)(double);
@@ -10,6 +15,10 @@ int test_exp(void* handle) {
     __m128  (*lamd_vrs4_expf) (__m128);
     __m256d (*lamd_vrd4_exp)  (__m256d);
     __m256  (*lamd_vrs8_expf) (__m256);
+
+    //array vector
+    void (*lamd_vrda_exp) (int, double*, double*);
+    void (*lamd_vrsa_expf) (int, float* , float*);
 
     /*scalar inputs*/
     float inputf = 3.145, outputf;
@@ -19,6 +28,22 @@ int test_exp(void* handle) {
     __m128  ip_vrs4, op_vrs4;
     __m256d ip_vrd4, op_vrd4;
     __m256  ip_vrs8, op_vrs8;
+
+    //array vector inputs
+    float *input_arrayf   = (float *)  malloc(sizeof(float) * array_size);
+    float *output_arrayf  = (float *)  malloc(sizeof(float) * array_size);
+    double *input_arrayd  = (double *) malloc(sizeof(double) * array_size);
+    double *output_arrayd = (double *) malloc(sizeof(double) * array_size);
+
+    for (unsigned int i = 0; i < array_size; i++) {
+         input_arrayf[i] = RANGEF;
+	 input_arrayd[i] = RANGED;
+    }
+
+    for (unsigned int i = 0; i < array_size; i++) {
+        output_arrayf[0] += output_arrayf[i];
+	output_arrayd[0] += output_arrayd[i];
+    }
 
     double input_array_vrd2[2] = {1.2, 3.5};
     double output_array_vrd2[2];
@@ -47,6 +72,9 @@ int test_exp(void* handle) {
     lamd_vrs4_expf = dlsym(handle, "amd_vrs4_expf");
     lamd_vrd4_exp  = dlsym(handle, "amd_vrd4_exp");
     lamd_vrs8_expf = dlsym(handle, "amd_vrs8_expf");
+    //vector array variants
+    lamd_vrsa_expf = dlsym(handle, "amd_vrsa_expf");
+    lamd_vrda_exp  = dlsym(handle, "amd_vrda_exp");
 
     error = dlerror();
     if (error != NULL) {
@@ -54,7 +82,7 @@ int test_exp(void* handle) {
         return 1;
     }
 
-    printf("Exercising exp routines\n");
+    printf("Exerciexpg exp routines\n");
     /*scalar*/
     outputf = (*lamd_expf)(inputf);
     printf("amd_expf(%f) = %f\n", inputf, outputf);
@@ -87,12 +115,40 @@ int test_exp(void* handle) {
     /*vrs8*/
     op_vrs8 = (*lamd_vrs8_expf)(ip_vrs8);
     _mm256_storeu_ps(output_array_vrs8, op_vrs8);
-    printf("amd_vrs8_expf\ninput:");
+    printf("amd_vrs8_expf\ninput:\n");
     for(i=0; i<8; i++)
         printf("%f\t",input_array_vrs8[i]);
     printf("\nOutput:\n");
     for(i=0; i<8; i++)
         printf("%f\t",output_array_vrs8[i]);
+
+    /*vector array*/
+    printf("amd_vrsa_expf\nInput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%f\t", input_arrayf[i]);
+    }
+    for (unsigned int i = 0; i < loopCount; i++) {
+        (*lamd_vrsa_expf)(dim, input_arrayf + i*dim, output_arrayf + i*dim);
+    }
+    printf("\nOutput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%f\t", output_arrayf[i]);
+    }
+
+    printf("amd_vrda_exp\nInput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%lf\t", input_arrayd[i]);
+    }
+
+    for (unsigned int i = 0; i < loopCount; i++) {
+        (*lamd_vrda_exp)(dim, input_arrayd + i*dim, output_arrayd + i*dim);
+    }
+
+    printf("\nOutput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%lf\t", output_arrayd[i]);
+    }
+
     printf("\n");
 
     return 0;

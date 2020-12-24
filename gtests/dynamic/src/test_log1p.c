07435746b9aecb0ca56e6abc8d88f53a452b1ffc
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2020-12-24 15:10:00 +0530

    libm:dynamic load: update log1p array vector
    Change-Id: I5bcc3ef2ecdd294b71f83c9c7de5113af5d1d4eb
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1387]

--- gtests/dynamic/src/test_log1p.c
+++ gtests/dynamic/src/test_log1p.c
@@ -1,8 +1,13 @@
 #include "libm_dynamic_load.h"
 
+#define RANGEF GenerateRangeFloat(-100.0, 100.0)
+#define RANGED GenerateRangeDouble(-100.0, 100.0)
+
 int test_log1p(void* handle) {
     char* error;
     int i;
+    int dim=5, loopCount=10;
+    int array_size = dim * loopCount;
 
     float (*lamd_log1pf)(float);
     double (*lamd_log1p)(double);
@@ -10,6 +15,10 @@ int test_log1p(void* handle) {
     __m128  (*lamd_vrs4_log1pf) (__m128);
     //__m256d (*lamd_vrd4_log1p)  (__m256d);
     //__m256  (*lamd_vrs8_log1pf) (__m256);
+
+    //array vector
+    void (*lamd_vrda_log1p) (int, double*, double*);
+    void (*lamd_vrsa_log1pf) (int, float* , float*);
 
     /*scalar inputs*/
     float inputf = 3.145, outputf;
@@ -20,19 +29,35 @@ int test_log1p(void* handle) {
     //__m256d ip_vrd4, op_vrd4;
     //__m256  ip_vrs8, op_vrs8;
 
+    //array vector inputs
+    float *input_arrayf   = (float *)  malloc(sizeof(float) * array_size);
+    float *output_arrayf  = (float *)  malloc(sizeof(float) * array_size);
+    double *input_arrayd  = (double *) malloc(sizeof(double) * array_size);
+    double *output_arrayd = (double *) malloc(sizeof(double) * array_size);
+
+    for (unsigned int i = 0; i < array_size; i++) {
+         input_arrayf[i] = RANGEF;
+	 input_arrayd[i] = RANGED;
+    }
+
+    for (unsigned int i = 0; i < array_size; i++) {
+        output_arrayf[0] += output_arrayf[i];
+	output_arrayd[0] += output_arrayd[i];
+    }
+
     double input_array_vrd2[2] = {1.2, 3.5};
     double output_array_vrd2[2];
-/*
-    double input_array_vrd4[4] = {0.0, 1.1, 3.6, 2.8};
-    double output_array_vrd4[4];
-*/
+
+    //double input_array_vrd4[4] = {0.0, 1.1, 3.6, 2.8};
+    //double output_array_vrd4[4];
+
     float input_array_vrs4[4] = {3.5, 1.2, 3.4, 0.5};
     float output_array_vrs4[4];
-/*
-    float input_array_vrs8[8] = {1.2, 2.3, 5.6, 50.3,
-                                -50.45, 45.3, 23.4, 4.5};
-    float output_array_vrs8[8];
-*/
+
+    //float input_array_vrs8[8] = {1.2, 2.3, 5.6, 50.3,
+    //                            -50.45, 45.3, 23.4, 4.5};
+    //float output_array_vrs8[8];
+
     /*packed inputs*/
     ip_vrd2 = _mm_loadu_pd(input_array_vrd2);
     ip_vrs4 = _mm_loadu_ps(input_array_vrs4);
@@ -47,6 +72,9 @@ int test_log1p(void* handle) {
     lamd_vrs4_log1pf = dlsym(handle, "amd_vrs4_log1pf");
     //lamd_vrd4_log1p  = dlsym(handle, "amd_vrd4_log1p");
     //lamd_vrs8_log1pf = dlsym(handle, "amd_vrs8_log1pf");
+    //vector array variants
+    lamd_vrsa_log1pf = dlsym(handle, "amd_vrsa_log1pf");
+    lamd_vrda_log1p  = dlsym(handle, "amd_vrda_log1p");
 
     error = dlerror();
     if (error != NULL) {
@@ -54,7 +82,7 @@ int test_log1p(void* handle) {
         return 1;
     }
 
-    printf("Exercising log1p routines\n");
+    printf("Exercilog1pg log1p routines\n");
     /*scalar*/
     outputf = (*lamd_log1pf)(inputf);
     printf("amd_log1pf(%f) = %f\n", inputf, outputf);
@@ -89,13 +117,39 @@ int test_log1p(void* handle) {
     /*
     op_vrs8 = (*lamd_vrs8_log1pf)(ip_vrs8);
     _mm256_storeu_ps(output_array_vrs8, op_vrs8);
-    printf("amd_vrs8_log1pf\ninput:");
+    printf("amd_vrs8_log1pf\ninput:\n");
     for(i=0; i<8; i++)
         printf("%f\t",input_array_vrs8[i]);
     printf("\nOutput:\n");
     for(i=0; i<8; i++)
         printf("%f\t",output_array_vrs8[i]);
     */
+    /*vector array*/
+    printf("amd_vrsa_log1pf\nInput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%f\t", input_arrayf[i]);
+    }
+    for (unsigned int i = 0; i < loopCount; i++) {
+        (*lamd_vrsa_log1pf)(dim, input_arrayf + i*dim, output_arrayf + i*dim);
+    }
+    printf("\nOutput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%f\t", output_arrayf[i]);
+    }
+
+    printf("amd_vrda_log1p\nInput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%lf\t", input_arrayd[i]);
+    }
+
+    for (unsigned int i = 0; i < loopCount; i++) {
+        (*lamd_vrda_log1p)(dim, input_arrayd + i*dim, output_arrayd + i*dim);
+    }
+
+    printf("\nOutput:\n");
+    for (unsigned int i = 0; i < array_size; i++) {
+        printf("%lf\t", output_arrayd[i]);
+    }
 
     printf("\n");
 

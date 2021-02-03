c94612fa256c479f559cd2e7c1cc1770e34dbaed
Prem Mallappa <Premachandra.Mallappa@amd.com>
2021-02-03 15:52:18 +0530

    libm: iface: call new exp2() implementation
    Change-Id: I70b6b5dad226e7679ba1bb9c478e21b4575b4327
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    AMD-Internal: [SWLCSG-519]

--- src/iface/exp2.c
+++ src/iface/exp2.c
@@ -51,7 +51,7 @@ struct alm_arch_funcs __arch_funcs_exp2 = {
 
         [ALM_UARCH_VER_ZEN2] = {
             NULL, 
-            NULL, //&ALM_PROTO_ARCH_ZN2(exp2),
+            &ALM_PROTO_ARCH_ZN2(exp2),
             NULL,                           /* vrs4 ? */
             NULL,                           /* vrs8 ? */
             NULL,//&ALM_PROTO_ARCH_ZN2(vrd2_exp2),

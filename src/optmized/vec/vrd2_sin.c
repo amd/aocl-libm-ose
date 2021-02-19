bd8ba19b5bd2975af223eb9c50bbee97476204cd
Dhiraj Kumar <dhiraj.kumar@amd.com>
2021-02-19 15:40:26 +0530

    libm: vrd2_sin: warning message cleanup
    Chages to cleanup warning messages.
    
    Change-Id: Ie4ceb0a24f3ccfed23bb909de8ca77bf9ececbbc
    Signed-off-by: Dhiraj Kumar <dhiraj.kumar@amd.com>
    AMD-Internal: [SWLCSG-545]

--- src/optmized/vec/vrd2_sin.c
+++ src/optmized/vec/vrd2_sin.c
@@ -31,6 +31,7 @@
 #include <libm/types.h>
 #include <libm/typehelper.h>
 #include <libm/typehelper-vec.h>
+#include <libm/amd_funcs_internal.h>
 #include <libm/compiler.h>
 #include <libm/poly.h>
 
@@ -101,9 +102,6 @@ static struct {
 #define C10 v2_sin_data.poly_sin[5]
 #define C12 v2_sin_data.poly_sin[6]
 #define C14 v2_sin_data.poly_sin[7]
-
-
-double ALM_PROTO(sin)(double);
 
 
 static inline v_f64x2_t

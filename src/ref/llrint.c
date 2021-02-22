32e459517dd5017e3d4098c904c4566bf5974b97
Dhiraj Kumar <dhiraj.kumar@amd.com>
2021-02-23 03:18:51 +0530

    libm: ref: llrint: warning message cleanup
    Chages to cleanup warning messages.
    
    Change-Id: I5939416378bbd42c79d48577d4644fc05bf0a37c
    Signed-off-by: Dhiraj Kumar <dhiraj.kumar@amd.com>
    AMD-Internal: [SWLCSG-545]

--- src/ref/llrint.c
+++ src/ref/llrint.c
@@ -33,10 +33,9 @@
 
 long long int FN_PROTOTYPE_REF(llrint)(double x)
 {
-
-
     UT64 checkbits,val_2p52;
     checkbits.f64=x;
+
 
     /* Clear the sign bit and check if the value can be rounded */
 
@@ -45,8 +44,9 @@ long long int FN_PROTOTYPE_REF(llrint)(double x)
         /* number cant be rounded raise an exception */
         /* Number exceeds the representable range could be nan or inf also*/
        // __amd_handle_error(DOMAIN, EDOM, "llrint", x,0.0 ,(double)x);
-		__amd_handle_error("llrint", __amd_lrint, (long long int)x, _DOMAIN, 0, EDOM, x, 0.0, 1);
-		return (long long int) x;
+	__amd_handle_error("llrint", __amd_lrint, (unsigned long long int)x,
+                                                             _DOMAIN, 0, EDOM, x, 0.0, 1);
+	return (long long int) x;
     }
 
     val_2p52.u32[1] = (checkbits.u32[1] & 0x80000000) | 0x43300000;

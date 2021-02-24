48b79a5d02530a19584cee0321b4ba04e38cb250
Prem Mallappa <Premachandra.Mallappa@amd.com>
2021-02-24 11:43:27 +0530

    libm: poly.h : fix compiler warnings
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>
    Change-Id: I59c3af449efe2fd171c2e95acc1099a7b43b7595

--- include/libm/poly.h
+++ include/libm/poly.h
@@ -73,14 +73,14 @@
  *      = (C1 + C2*r) + r^2(C3 + C4*r) + r^4(C5 + C6*r)
  */
 #define POLY_EVAL_6(r, c0, c1, c2, c3, c4, c5) ({       \
-            __typeof(r) t1, t2, t3, r2, q;              \
+            __typeof(r) t1, t2, t3, r2, _q;             \
             t1 = c0 + c1*r;                             \
             t2 = c2 + c3*r;                             \
             r2 = r * r;                                 \
             t3 = c4 + c5*r;                             \
-            q = t1 + r2 * t2;                           \
-            q = q + r2 * r2 * t3;                       \
-            q;                                          \
+            _q = t1 + r2 * t2;                          \
+            _q = _q + r2 * r2 * t3;                     \
+            _q;                                         \
             })
 
 /*

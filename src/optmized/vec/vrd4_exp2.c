c743a844145d2e87debcce6d3df8184dfc5eaf8a
Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
2021-04-22 11:04:19 +0530

    libm:vrd4_exp2: Fixed accuracy errors
    Error was due to incorrect argument reduction of the input
    
    Signed-off-by: Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
    AMD-Internal: [CPUPL-1502]
    Change-Id: I32c8defc5609364e3d838b65bba280db853765f2

--- src/optmized/vec/vrd4_exp2.c
+++ src/optmized/vec/vrd4_exp2.c
@@ -25,7 +25,6 @@
  *
  */
 
-
 #include <libm_util_amd.h>
 #include <libm_special.h>
 #include <libm_macros.h>
@@ -37,12 +36,11 @@
 #include <libm/poly-vec.h>
 
 
-//#define EXPERIMENTAL 1
-
 static const struct {
     v_i64x4_t   mask;
     v_i64x4_t   arg_max;
-
+    v_f64x4_t   ln2by_64_head;
+    v_f64x4_t   ln2by_64_tail;
 #ifdef EXPERIMENTAL
     v_f64x4_t   sixtyfour_byln2;
     v_f64x4_t   ln2by_64_head, ln2by_64_tail;
@@ -56,45 +54,38 @@ static const struct {
     v_f64x4_t   poly[12];
     } exp2_data = {
     .mask           = _MM_SET1_I64(0x7FFFFFFFFFFFFFFF),
-    .arg_max        = _MM_SET1_I64(0x4086000000000000),
+    .arg_max       = _MM_SET1_I64(0x408FF80000000000),
     .sixtyfour      = _MM_SET1_PD4(0x1.0p+6),
-#ifdef EXPERIMENTAL
     .ln2by_64_head  = _MM_SET1_PD4(0x1.63p-1),
     .ln2by_64_tail  = _MM_SET1_PD4(-0x1.bd0105c610ca8p-13),
-#else
     .oneby_64       = _MM_SET1_PD4(0x1.0p-6),
-#endif
     .huge           = _MM_SET1_PD4(0x1.8p+52),
     .ln2            = _MM_SET1_PD4(0x1.62e42fefa39efp-1),
     .bias           = _MM_SET1_I64(EMAX_DP64), /* 1023 */
     .poly           = {
-        _MM_SET1_PD4(0x1.0p0),
-        _MM_SET1_PD4(0x1.0p-1),
-        _MM_SET1_PD4(0x1.5555555555555p-3),
-        _MM_SET1_PD4(0x1.5555555555555p-5),
-        _MM_SET1_PD4(0x1.1111111111111p-7),
-        _MM_SET1_PD4(0x1.6c16c16c16c17p-10),
-        _MM_SET1_PD4(0x1.a01a01a01a019p-13),
-        _MM_SET1_PD4(0x1.a01a019ffbbebp-16),
-        _MM_SET1_PD4(0x1.71de3a55b4df2p-19),
-        _MM_SET1_PD4(0x1.27e567106f599p-22),
-        _MM_SET1_PD4(0x1.ae63d211b9bc7p-26),
+        _MM_SET1_PD4(0x1p0),
+        _MM_SET1_PD4(0x1.000000000001p-1),
+        _MM_SET1_PD4(0x1.55555555554a2p-3),
+        _MM_SET1_PD4(0x1.555555554f37p-5),
+        _MM_SET1_PD4(0x1.1111111130dd6p-7),
+        _MM_SET1_PD4(0x1.6c16c1878111dp-10),
+        _MM_SET1_PD4(0x1.a01a011057479p-13),
+        _MM_SET1_PD4(0x1.a01992d0fe581p-16),
+        _MM_SET1_PD4(0x1.71df4520705a4p-19),
+        _MM_SET1_PD4(0x1.28b311c80e499p-22),
+        _MM_SET1_PD4(0x1.ad661ce7af3e3p-26),
 
     },
 };
 
 #define ALM_V4_EXP2_64               exp2_data.sixtyfour
 #define ALM_V4_EXP2_BIAS             exp2_data.bias
-#define ALM_V4_EXP2_ONEBY_64         exp2_data.oneby_64
-#define ALM_V4_EXP2_LN2BY_64_HEAD    exp2_data.ln2by_64_head
-#define ALM_V4_EXP2_LN2BY_64_TAIL    exp2_data.ln2by_64_tail
 #define ALM_V4_EXP2_HUGE             exp2_data.huge
 #define ALM_V4_EXP2_LN2              exp2_data.ln2
 #define ALM_V4_EXP2_ARG_MAX          exp2_data.arg_max
 #define ALM_V4_EXP2_MASK             exp2_data.mask
 
 #define C1  exp2_data.poly[0]
-#define C2  C1
 #define C3  exp2_data.poly[1]
 #define C4  exp2_data.poly[2]
 #define C5  exp2_data.poly[3]
@@ -117,22 +108,12 @@ static const struct {
  * 1. Argument Reduction:
  *      2^(x)   =   2^(x*N/N)                --- (1)
  *
- *      Choose 'n' and 'f', such that
- *      x * N = n + f                        --- (2)
- *                | n is integer and |f| <= 0.5
- *
- *     Choose 'm' such that,
- *      n = (N * m)                          --- (3)
- *
- *     From (1), (2) and (3),
- *      2^x = 2^((N*m + f)/N)
- *          = (2^m) * 2^(f/N)
+ *      Let x = n + f                        --- (2)
+ *      | n is integer and |f| <= 0.5
  *
  * 2. Polynomial Evaluation
  *   From (2),
- *     f  = x*N - n
- *   Let,
- *     r  = f/N = (x*N - n)/N == x - n/N
+ *     f  = x - n
  *
  *   Taylor series only exist for e^x
  *        2^x = e^(x*ln2)
@@ -147,56 +128,42 @@ static const struct {
 v_f64x4_t
 ALM_PROTO_OPT(vrd4_exp2)(v_f64x4_t input)
 {
-    v_f64x4_t   r, z, dn;
+    v_f64x4_t   r, dn;
     v_i64x4_t   vx, n, m, cond;
 
     /* Get absolute value */
     vx   = as_v4_i64_f64(input);
     vx   = vx & ALM_V4_EXP2_MASK;
 
-    /* Check if -709 < vx < 709 */
-    cond = (vx > ALM_V4_EXP2_ARG_MAX);
+    /* Check if -1023 < vx < 1023 */
+    cond = ((vx) > ALM_V4_EXP2_ARG_MAX);
 
-    /* x * 64.0 */
-    z    = input * ALM_V4_EXP2_64;
+    dn   = input + ALM_V4_EXP2_HUGE;
 
-    dn   = z + ALM_V4_EXP2_HUGE;
-
-    /* n = int (z) */
-    n    = as_v4_i64_f64(dn) & 0xfff;
+    n    = as_v4_i64_f64(dn);
 
     /* dn = double(n) */
     dn   = dn - ALM_V4_EXP2_HUGE;
 
-#ifdef EXPERIMENTAL
-    /*
-     * r = x - (dn * 1/64))
-     *     where 1/64 is split into Head and Tail values
-     */
-    v_f64x4_t r1, r2;
-    r1  = input - (dn * ALM_V4_EXP2_LN2BY_64_HEAD);
-    r2  = dn * ALM_V4_EXP2_LN2BY_64_TAIL;
-    r   = r1 + r2;
-#else
-    r = input - (dn * ALM_V4_EXP2_ONEBY_64);
-    r *= ALM_V4_EXP2_LN2;
-#endif
+    r = input - dn;
 
-    /* m = n/64, and Calculate m = 2^m  */
-    m = n << (52 - 6);
+    r *= ALM_V4_EXP2_LN2;
+
+    /* Calculate m = 2^m  */
+    m = n << 52;
 
     /* poly = C1 + C2*r + C3*r^2 + C4*r^3 + C5*r^4 + C6*r^5 +
      *          C7*r^6 + C8*r^7 + C9*r^8 + C10*r^9 + C11*r^10 + C12*r^11
      *      = (C1 + C2*r) + r^2(C3 + C4*r) + r^4(C5 + C6*r) +
      *           r^6(C7 + C8*r) + r^8(C9 + C10*r) + r^10(C11 + C12*r)
      */
-    v_f64x4_t poly = POLY_EVAL_11(r,
-                                  C1, C1, C3, C4,  C5,  C6,
+
+    v_f64x4_t poly = POLY_EVAL_11(r, C1, C1, C3, C4, C5, C6,
                                   C7, C8, C9, C10, C11, C12);
+
 
     /* result = poly * 2^m */
     v_f64x4_t ret = as_v4_f64_i64(as_v4_i64_f64(poly) + m);
-    //v_f64x4_t ret = poly * as_v4_f64_i64(m);
 
     if (unlikely(any_v4_u64_loop(cond))) {
         ret = (v_f64x4_t) {

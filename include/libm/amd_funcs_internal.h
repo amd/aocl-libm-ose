31f3c417deec4c9641857d985d8de5bff42198af
Prem Mallappa <Premachandra.Mallappa@amd.com>
2021-04-30 19:08:03 +0530

    libm: funcs_internal: add AVX2 variants
    Change-Id: Ia2277c297cbf7863fc7ec53c009538c9614f5e6a
    Signed-off-by: Prem Mallappa <Premachandra.Mallappa@amd.com>

--- include/libm/amd_funcs_internal.h
+++ include/libm/amd_funcs_internal.h
@@ -46,6 +46,10 @@
 #include "__alm_func_internal.h"
 #undef ALM_PROTO_INTERNAL
 
+#define ALM_PROTO_INTERNAL ALM_PROTO_AVX2
+#include "__alm_func_internal.h"
+#undef ALM_PROTO_INTERNAL
+
 #ifndef ALM_OVERRIDE
 #define ALM_PROTO_INTERNAL ALM_PROTO_OPT
 #include "__alm_func_internal.h"

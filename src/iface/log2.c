83a1c08305853a80f871aa88e4c62d11b7db84e4
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-05-23 15:58:43 +0530

    libm: iface: log2: updated default initializations
    Default initialized to AVX2 variant under /src/isa/avx2/
    
    Change-Id: I41d122cf56634132e20f2c08e466004bafa78dec
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1488]

--- src/iface/log2.c
+++ src/iface/log2.c
@@ -42,9 +42,9 @@ struct alm_arch_funcs __arch_funcs_log2 = {
             &ALM_PROTO_FMA3(log2f),
             &ALM_PROTO_FMA3(log2),
             &ALM_PROTO_FMA3(vrs4_log2f),
-            NULL,                           /* vrs8 ? */
+            &ALM_PROTO_ARCH_AVX2(vrs8_log2f), /* vrs8 ? */
             &ALM_PROTO_FMA3(vrd2_log2),
-            NULL,                           /* vrd4 ? */
+            &ALM_PROTO_ARCH_AVX2(vrd4_log2),  /* vrd4 ? */
             &ALM_PROTO_FMA3(vrsa_log2f),  /*array vector float*/
             &ALM_PROTO_FMA3(vrda_log2),  /*array vector double*/
         },

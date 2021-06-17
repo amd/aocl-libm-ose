dc715b5f99cb2b1fa6fdc3fa0ad99197f577c07b
dnikku <Deepika.Nikku@amd.com>
2021-06-17 09:35:23 +0000

    libm: acosf: added opt/acosf.c to build using iface
    Added optimized acosf file to build using iface acos file
    
    Signed-off-by: dnikku <Deepika.Nikku@amd.com>
    AMD-Internal: [CPUPL-1173]
    Change-Id: I5a0de3385c473bcebd3ac39543192dcc8b3ba7ce

--- src/iface/acos.c
+++ src/iface/acos.c
@@ -39,12 +39,30 @@ struct alm_arch_funcs __arch_funcs_acos = {
     .def_arch = ALM_UARCH_VER_DEFAULT,
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
-            &ALM_PROTO_REF(acosf),
+            &ALM_PROTO_OPT(acosf),
             &ALM_PROTO_REF(acos),
             NULL,                           /* vrs4 ? */
             NULL,                           /* vrs8 ? */
             NULL,                           /* vrd2 ? */
             NULL,                           /* vrd4 ? */
+        },
+
+        [ALM_UARCH_VER_ZEN2] = {
+            &ALM_PROTO_ARCH_ZN2(acosf),
+            &ALM_PROTO_REF(acos),
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+        },
+
+        [ALM_UARCH_VER_ZEN3] = {
+            &ALM_PROTO_ARCH_ZN3(acosf),
+            &ALM_PROTO_REF(acos),
+            NULL,
+            NULL,
+            NULL,
+            NULL,
         },
     },
 };

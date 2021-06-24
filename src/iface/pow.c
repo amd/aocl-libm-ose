7cb481e01a346d42840df59ffd3a91ba2ab3b6b8
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-06-24 12:51:58 +0530

    libm: avx512: iface: Add pow v8d variants
    Change-Id: I6a52158914931efdd5fcd9b8a70b580bbd3b328b
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1666]

--- src/iface/pow.c
+++ src/iface/pow.c
@@ -69,6 +69,7 @@ struct alm_arch_funcs __arch_funcs_pow = {
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_powf),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_pow),
         },
     },
 };
@@ -84,6 +85,7 @@ LIBM_IFACE_PROTO(pow)(void *arg)
         [ALM_FUNC_VECT_SP_8] = &G_ENTRY_PT_PTR(vrs8_powf),
         [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_pow),
         [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_pow),
+        [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_pow),
 	    [ALM_FUNC_VECT_SP_ARR] = &G_ENTRY_PT_PTR(vrsa_powf),
 
         [ALM_FUNC_VECT_SP_16] = &G_ENTRY_PT_PTR(vrs16_powf),

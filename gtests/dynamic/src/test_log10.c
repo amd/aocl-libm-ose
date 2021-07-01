870e206253fcd23f416c628b5780a33321fa7afa
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-07-01 17:37:54 +0530

    libm: dynamic load: log10f vector v4s,v8s updated
    Change-Id: Icf8a73eef1007cbd0fed7f42cc68cd0c7f2e4191
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1618]

--- gtests/dynamic/src/test_log10.c
+++ gtests/dynamic/src/test_log10.c
@@ -12,6 +12,7 @@ int test_log10(void* handle) {
     func      s1d = (func)dlsym(handle, "amd_log10");
     func_v2d  v2d = (func_v2d)dlsym(handle, "amd_vrd2_log10");
     funcf_v4s v4s = (funcf_v4s)dlsym(handle, "amd_vrs4_log10f");
+    funcf_v8s v8s = (funcf_v8s)dlsym(handle, "amd_vrs8_log10f");
 
     funcf_va  vas = (funcf_va)dlsym(handle, "amd_vrsa_log10f");
     func_va   vad = (func_va)dlsym(handle, "amd_vrda_log10");
@@ -22,6 +23,7 @@ int test_log10(void* handle) {
     /*for vector routines*/
     __m128d ip_vrd2, op_vrd2;
     __m128  ip_vrs4, op_vrs4;
+    __m256  ip_vrs8, op_vrs8;
 
     //array vector inputs
     float *input_arrayf   = (float *)  malloc(sizeof(float) * array_size);
@@ -43,10 +45,14 @@ int test_log10(void* handle) {
     double output_array_vrd2[2];
     float input_array_vrs4[4] = {3.5f, 1.2f, 3.4f, 0.5f};
     float output_array_vrs4[4];
+    float input_array_vrs8[8] = {1.2f, 2.3f, 5.6f, 50.3f,
+                                -50.4f, 45.3f, 23.4f, 4.5f};
+    float output_array_vrs8[8];
 
     /*packed inputs*/
     ip_vrd2 = _mm_loadu_pd(input_array_vrd2);
     ip_vrs4 = _mm_loadu_ps(input_array_vrs4);
+    ip_vrs8 = _mm256_loadu_ps(input_array_vrs8);
 
     error = dlerror();
     if (error != NULL) {
@@ -54,7 +60,7 @@ int test_log10(void* handle) {
         return 1;
     }
 
-    printf("Exercising log10 routines\n");
+    printf("Exercising log10 scalar routines\n");
     /*scalar*/
     outputf = s1f(inputf);
     printf("amd_log10f(%f) = %f\n", (double)inputf, (double)outputf);
@@ -62,6 +68,7 @@ int test_log10(void* handle) {
     printf("amd_log10(%lf) = %lf\n", input, output);
 
     /*vrd2*/
+    printf("Running amd_vrd2_log10\n");
     op_vrd2 = v2d(ip_vrd2);
     _mm_storeu_pd(output_array_vrd2, op_vrd2);
     printf("amd_vrd2_log10([%lf, %lf] = [%lf, %lf])\n",
@@ -69,11 +76,23 @@ int test_log10(void* handle) {
             output_array_vrd2[0], output_array_vrd2[1]);
 
     /*vrs4*/
+    printf("Running amd_vrs4_log10f\n");
     op_vrs4 = v4s(ip_vrs4);
     _mm_storeu_ps(output_array_vrs4, op_vrs4);
-    printf("amd_vrs4_log10([%f, %f] = [%f, %f])\n",
+    printf("amd_vrs4_log10f([%f, %f] = [%f, %f])\n",
             (double)input_array_vrs4[0], (double)input_array_vrs4[1],
             (double)output_array_vrs4[0], (double)output_array_vrs4[1]);
+
+    /*vrs8*/
+    printf("Running amd_vrs8_log10f\n");
+    op_vrs8 = v8s(ip_vrs8);
+    _mm256_storeu_ps(output_array_vrs8, op_vrs8);
+    printf("amd_vrs8_log10f\ninput:\n");
+    for(i=0; i<8; i++)
+        printf("%f\t",(double)input_array_vrs8[i]);
+    printf("\nOutput:\n");
+    for(i=0; i<8; i++)
+        printf("%f\t",(double)output_array_vrs8[i]);
 
     /*vector array*/
     printf("amd_vrsa_log10f\nInput:\n");

b4ed02ff62b7fd81f8954f23627c168abec29fc5
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-07-29 14:31:34 +0530

    libm: avx512: entry points: Fix missing alias for v8d log2
    Change-Id: Ia13640fc73bb1d973e3dfb966483f1717c2bf3da
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1708]

--- src/entry_pt_map.c
+++ src/entry_pt_map.c
@@ -236,7 +236,7 @@ LIBM_DECL_FN_MAP(vrd4_cos);
 LIBM_DECL_FN_MAP(vrd4_tan);
 
 LIBM_DECL_FN_MAP(vrd8_exp2);
-//LIBM_DECL_FN_MAP(vrd4_log2);
+LIBM_DECL_FN_MAP(vrd8_log2);
 LIBM_DECL_FN_MAP(vrd8_exp);
 LIBM_DECL_FN_MAP(vrd8_pow);
 //LIBM_DECL_FN_MAP(vrd4_expm1);
@@ -329,7 +329,7 @@ WEAK_LIBM_ALIAS(vrd4_sin, FN_PROTOTYPE(vrd4_sin));
 
 WEAK_LIBM_ALIAS(vrd8_exp, FN_PROTOTYPE(vrd8_exp));
 WEAK_LIBM_ALIAS(vrd8_pow, FN_PROTOTYPE(vrd8_pow));
-//WEAK_LIBM_ALIAS(vrd8_exp2, FN_PROTOTYPE(vrd8_exp2));
+WEAK_LIBM_ALIAS(vrd8_exp2, FN_PROTOTYPE(vrd8_log2));
 WEAK_LIBM_ALIAS(vrd8_exp2, FN_PROTOTYPE(vrd8_exp2));
 WEAK_LIBM_ALIAS(vrd8_log, FN_PROTOTYPE(vrd8_log));
 WEAK_LIBM_ALIAS(vrd8_sin, FN_PROTOTYPE(vrd8_sin));

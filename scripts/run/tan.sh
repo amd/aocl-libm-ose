e6bbffd258275d66076f7811004afa36ff24bcc4
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-07-30 10:18:01 +0530

    libm: avx512: scripts: Updated for tan with v8d,v16s
    Change-Id: I3d704db1b042a0f9db86f6dffb844e78b50f851a
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1708]

--- scripts/run/tan.sh
+++ scripts/run/tan.sh
@@ -25,10 +25,18 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-exe="$1"
+EXE="$1"
+
+arch="$2"
 
 nargs=1
+
 xranges=(-1000,1000)
-input_types=("s1d" "s1f" "v4s" "v2d" "v4d" "v8s")
+
+input_types=("s1d" "s1f" "v4s" "v4d" "v2d" "v8s")
+if [ ${arch} = "avx512" ]; then
+    input_types+=("v8d" "v16s")
+fi
 
 run_exe_nargs $exe $nargs $input_types $xranges
+

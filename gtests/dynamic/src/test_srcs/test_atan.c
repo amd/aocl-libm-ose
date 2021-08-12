6c5067edcde7ad431463cc7f95ca7eedcf4aecff
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-08-12 16:45:26 +0530

    libm: dynamic load: Added vector float tanf variants
    Change-Id: I2ba71e95e97bed8f1bca5f54041f657e156f06a2
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1708]

--- gtests/dynamic/src/test_srcs/test_atan.c
+++ gtests/dynamic/src/test_srcs/test_atan.c
@@ -6,8 +6,11 @@ int test_atan(void* handle) {
     const char* func_name = "atan";
     data.s1f = (funcf)dlsym(handle, "amd_atanf");
     data.s1d = (func)dlsym(handle, "amd_atan");
+    data.v4s = (funcf_v4s)dlsym(handle, "amd_vrs4_atanf");
+    data.v8s = (funcf_v8s)dlsym(handle, "amd_vrs8_atanf");
 
-    if (data.s1f == NULL || data.s1d == NULL) {
+    if (data.s1f == NULL || data.s1d == NULL ||
+        data.v4s == NULL || data.v8s == NULL) {
         ret = 1;
     }
     if (ret == 1) {

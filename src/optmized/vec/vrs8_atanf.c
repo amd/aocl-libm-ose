4f619c25af9a52a28679e2466f5ffbba15dd9ed3
Vijay Tiwari <vijay.tiwari@amd.com>
2021-08-18 14:00:18 +0530

    Libm:vrs8_atanf function optimization for perf gain
    Optimized perf comparison on lib-diesel-01 machine in MOPS
    atanf= 83 vrs4_atanf= 225 vrs8_atanf= 333
    MULP < 2 for all
    AMD-Internal: [CPUPL-1793]
    Signed-off-by: Vijay Tiwari <vijay.tiwari@amd.com>
    Change-Id: Idbbac2775d985794580974cc429d971f1c876769
    
    Change-Id: I213211bc214093d78b3c39734cf76844f1603e56

--- src/optmized/vec/vrs8_atanf.c
+++ src/optmized/vec/vrs8_atanf.c
@@ -29,31 +29,30 @@
  * sign = sign(x)
  * x = abs(x)
  *
- * Reduce the given x, so that every element of x falls into the interval [-(2-sqrt(3)),+(2-sqrt(3))]
- * Use the following identities to evalute atan and reduce the range of every element
- * atan(x) = pi/2 - atan(1/x)				when x > 1
- * 		   = pi/6 + atan(f) 				when f > (2-sqrt(3))
+ * Argument Reduction for every x into the interval [-(2-sqrt(3)),+(2-sqrt(3))]
+ *
+ * Use the following identities
+ * atan(x) = pi/2 - atan(1/x)               when x > 1
+ *         = pi/6 + atan(f)                 when f > (2-sqrt(3))
  * where f = (sqrt(3)*x-1)/(x+sqrt(3))
  *
- * All elements are approximated by using a polynomial
+ * All elements are approximated by using polynomial of degree 7
  */
-#include <stdint.h>
 #include <libm_util_amd.h>
 #include <libm_special.h>
 #include <libm_macros.h>
-#include <libm/types.h>
-#include <libm/typehelper.h>
 #include <libm/typehelper-vec.h>
 #include <libm/amd_funcs_internal.h>
-#include <libm/compiler.h>
 #include <libm/poly.h>
+#include <stdbool.h>
+
 static struct {
-    v_f32x8_t poly_atanf[9];
+    v_f32x8_t poly_atanf[3];
     v_u32x8_t mask_32;
-    float P[4], Range, sqrt3, unit;
+    float P[4], range, sqrt3, unit;
 } v8_atanf_data = {
     .sqrt3 = 0x1.bb67aep0,
-    .Range = 0x1.126146p-2,
+    .range = 0x1.126146p-2,
     .mask_32 = _MM256_SET1_I32(0x7FFFFFFF),
     .unit= 1.0f,
     // Values of factors of pi required to calculate atanf
@@ -65,32 +64,20 @@ static struct {
     },
     // Polynomial coefficients obtained using fpminimax algorithm from Sollya
     .poly_atanf = {
-        _MM_SET1_PS8(-0x1.555556p-2f),
-        _MM_SET1_PS8(0x1.99999ap-3f),
-        _MM_SET1_PS8(-0x1.24924ap-3f),
-        _MM_SET1_PS8(0x1.c71c7p-4f),
-        _MM_SET1_PS8(-0x1.745cd2p-4f),
-        _MM_SET1_PS8(0x1.3b0aeap-4f),
-        _MM_SET1_PS8(-0x1.1061c6p-4f),
-        _MM_SET1_PS8(0x1.d1242ap-5f),
-        _MM_SET1_PS8(-0x1.3a3c92p-5f),
+        _MM_SET1_PS8(-0x1.5552f2p-2f),
+        _MM_SET1_PS8(0x1.9848ap-3f),
+        _MM_SET1_PS8(-0x1.066ac8p-3f),
     },
 };
-#define THEEPS v8_atanf_data.THEEPS
+
 #define SQRT3 v8_atanf_data.sqrt3
-#define Range v8_atanf_data.Range
-#define P v8_atanf_data.P
+#define RANGE v8_atanf_data.range
+#define PI v8_atanf_data.P
 #define ALM_V8_ATANF_MASK_32  v8_atanf_data.mask_32
-#define Unit v8_atanf_data.unit
+#define UNIT v8_atanf_data.unit
 #define C0 v8_atanf_data.poly_atanf[0]
 #define C1 v8_atanf_data.poly_atanf[1]
 #define C2 v8_atanf_data.poly_atanf[2]
-#define C3 v8_atanf_data.poly_atanf[3]
-#define C4 v8_atanf_data.poly_atanf[4]
-#define C5 v8_atanf_data.poly_atanf[5]
-#define C6 v8_atanf_data.poly_atanf[6]
-#define C7 v8_atanf_data.poly_atanf[7]
-#define C8 v8_atanf_data.poly_atanf[8]
 /*
  ********************************************
  * Implementation Notes
@@ -98,8 +85,7 @@ static struct {
  * sign = sign(xi)
  * xi = |xi|
  *
- * Reduce every element(xi) from x into f, so that it falls into the interval [-(2-sqrt(3)),+(2-sqrt(3))]
- * Use the following identities for every xi to finally evaluate vrs8_atanf and reduce the range of xi
+ * Argument reduction: Use the following identities
  *
  * 1. If xi > 1,
  *      atan(xi) = pi/2 - atan(1/xi)
@@ -108,36 +94,58 @@ static struct {
  *      atan(x) = pi/6 + atan(f)
  *      where f = (sqrt(3)*xi-1)/(xi+sqrt(3))
  *
- *      atan(f) is calculated using the polynomial,
- *      f + C0*xi^3 + C1*xi^5 + C2*xi^7 + C3*xi^9 + C4*xi^11+
- *      C5*xi^13 + C6*xi^15 + C7*xi^17 + C8*xi^19
+ *      atan(xi) is calculated using the polynomial,
+ *      xi + C0*xi^3 + C1*xi^5 + C2*xi^7
  *
  */
+static inline bool
+all_v8_u32_loop(v_u32x8_t cond)
+{
+    for (int i = 0; i < 8; i++) {
+        if (!cond[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 v_f32x8_t
 ALM_PROTO_OPT(vrs8_atanf)(v_f32x8_t x)
 {
-    v_f32x8_t poly, aux, result;
-    v_u32x8_t sign;
-    v_u32x8_t n={0};
+    v_f32x8_t aux, result, poly1;
+    v_u32x8_t sign,cond;
     v_u32x8_t  ux = as_v8_u32_f32 (x);
     /* Get sign of the input value */
     sign = ux & ~ALM_V8_ATANF_MASK_32;
     /* Get absolute value of input */
     aux  = as_v8_f32_u32(ux & ALM_V8_ATANF_MASK_32);
-    for(int i=0;i<8;i++){
-        if(aux[i]>Unit){
-            aux[i]=Unit/aux[i];
-            n[i]=2;
+
+    float F = 2+SQRT3;
+    cond = aux>=F;
+
+    if(all_v8_u32_loop(cond)){
+        aux = UNIT/aux;
+        poly1 = POLY_EVAL_ODD_7(aux, C0, C1, C2);
+        result = PI[2]-poly1;
+    }else{
+        for(int i=0;i<8;++i){
+            if(aux[i]>=F){
+                aux[i] = UNIT/aux[i];
+                float poly = POLY_EVAL_ODD_7(aux[i], C0[i], C1[i], C2[i]);
+                result[i] = PI[2]-poly;
+            }else if(aux[i]>UNIT){
+                aux[i] = UNIT/aux[i];
+                aux[i] = (aux[i]*SQRT3-UNIT)/(SQRT3+aux[i]);
+                float poly = POLY_EVAL_ODD_7(aux[i], C0[i], C1[i], C2[i]);
+                result[i] = PI[3]-poly;
+            }else if(aux[i]>RANGE){
+                aux[i] = (aux[i]*SQRT3-UNIT)/(SQRT3+aux[i]);
+                float poly = POLY_EVAL_ODD_7(aux[i], C0[i], C1[i], C2[i]);
+                result[i] = PI[1]+poly;
+            }else{
+                result[i] = POLY_EVAL_ODD_7(aux[i], C0[i], C1[i], C2[i]);
+            }
         }
-        if(aux[i]>Range){
-            aux[i]=(aux[i]*SQRT3-Unit)/(SQRT3+aux[i]);
-            n[i]++;
-        }
-    }
-    poly = POLY_EVAL_ODD_19(aux, C0, C1, C2, C3, C4, C5, C6, C7, C8);
-    for(int i=0;i<8;i++){
-        if (n[i] > 1) poly[i] = -poly[i];
-            result[i] = P[n[i]]+poly[i];
     }
     return as_v8_f32_u32(as_v8_u32_f32(result) ^ sign);
 }

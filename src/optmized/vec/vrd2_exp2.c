2a98177f7c3f8ea078b9da1c3256aa44dc8cc865
Xavier, Arnold joseph <arnoldjoseph.xavier@amd.com>
2021-10-26 15:08:45 +0530

    libm:vrd2_exp:Bug fix
    Inputs higher than 1022.0 to be handled by scalar exp2
    
    Signed-off-by: Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
    AMD-Internal: [CPUPL-1927]
    Change-Id: I452ec702464662ffd7b418654a6a20b3f96eedb1

--- src/optmized/vec/vrd2_exp2.c
+++ src/optmized/vec/vrd2_exp2.c
@@ -54,7 +54,7 @@ static const struct {
     v_f64x2_t   poly[12];
     } exp2_data = {
     .mask           = _MM_SET1_I64x2(0x7FFFFFFFFFFFFFFF),
-    .arg_max       = _MM_SET1_I64x2(0x408FF80000000000),
+    .arg_max       = _MM_SET1_I64x2(0x408FF00000000000),
     .sixtyfour      = _MM_SET1_PD2(0x1.0p+6),
     .ln2by_64_head  = _MM_SET1_PD2(0x1.63p-1),
     .ln2by_64_tail  = _MM_SET1_PD2(-0x1.bd0105c610ca8p-13),

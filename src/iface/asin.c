9461b653fd8cd955b56aca1bcb683baa5a338a84
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-01-07 15:25:11 +0530

    libm: scalar asin double: Updated iface
    Change-Id: I998d0bfe7e0846440ef58397ea7d24a30bfb07dc
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1978]

--- src/iface/asin.c
+++ src/iface/asin.c
@@ -38,7 +38,7 @@ struct alm_arch_funcs __arch_funcs_asin = {
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(asinf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_REF(asin),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(asin),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX2(vrs4_asinf),/* vrs4 ? */
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_AVX2(vrs8_asinf),/* vrs8 ? */
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_asinf),
@@ -47,24 +47,27 @@ struct alm_arch_funcs __arch_funcs_asin = {
 
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(asinf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_REF(asin),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(asin),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN(vrs4_asinf),
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN(vrs8_asinf),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(asinf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(asin),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN2(vrs4_asinf), /* vrs4 ? */
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN2(vrs8_asinf), /* vrs8 ? */
         },
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(asinf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(asin),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN3(vrs4_asinf), /* vrs4 ? */
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN3(vrs8_asinf), /* vrs8 ? */
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(asinf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(asin),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN4(vrs4_asinf), /* vrs4 ? */
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN4(vrs8_asinf), /* vrs8 ? */
 

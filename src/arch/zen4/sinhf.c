f9037d52ad56f3bde60aaa80ec29b3d0ec6170a5
Vijay Tiwari <vijay.tiwari@amd.com>
2022-01-11 12:41:37 +0530

    [sinhf] missing zen4/sinhf file added
    Change-Id: I49bfe9431f09b431f9cbfb888a46328c2cbfcd44

--- src/arch/zen4/sinhf.c
+++ src/arch/zen4/sinhf.c
@@ -25,57 +25,7 @@
  *
  */
 
-#include <libm_macros.h>
-#include <libm/amd_funcs_internal.h>
-#include <libm/iface.h>
-#include <libm/entry_pt.h>
-#include <libm/arch/all.h>
+#define ALM_OVERRIDE 1
 
-
-static const
-struct alm_arch_funcs __arch_funcs_sinh = {
-    .def_arch = ALM_UARCH_VER_DEFAULT,
-    .funcs = {
-        [ALM_UARCH_VER_DEFAULT] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(sinhf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_REF(sinh),
-        },
-
-        [ALM_UARCH_VER_ZEN] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(sinhf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_REF(sinh),
-        },
-
-        [ALM_UARCH_VER_ZEN2] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(sinhf),
-        },
-
-        [ALM_UARCH_VER_ZEN3] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(sinhf),
-        },
-
-        [ALM_UARCH_VER_ZEN4] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(sinhf),
-        },
-    }
-};
-
-void
-LIBM_IFACE_PROTO(sinh)(void *arg)
-{
-    alm_ep_wrapper_t g_entry_sinh = {
-       .g_ep = {
-        [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(sinhf),
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(sinh),
-#if 0
-        [ALM_FUNC_VECT_SP_4] = &G_ENTRY_PT_PTR(vrs4_sinhf),
-        [ALM_FUNC_VECT_SP_8] = &G_ENTRY_PT_PTR(vrs8_sinhf),
-        [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_sinh),
-        [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_sinh),
-#endif
-        },
-    };
-
-    alm_iface_fixup(&g_entry_sinh, &__arch_funcs_sinh);
-}
-
+#include <libm/arch/zen4.h>
+#include "../../optmized/sinhf.c"

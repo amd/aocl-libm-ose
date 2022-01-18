295bde634d5c3ac77a9937b1528bb9b540ea3fa5
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-01-18 18:47:29 +0530

    libm: iface: Updated acosh scalar double precision
    Change-Id: Id7562be75bf9360158c7b693d8cde3b04b5a69dd
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-1988]

--- src/iface/acosh.c
+++ src/iface/acosh.c
@@ -29,8 +29,6 @@
 #include <libm/amd_funcs_internal.h>
 #include <libm/iface.h>
 #include <libm/entry_pt.h>
-
-//
 #include <libm/arch/all.h>
 
 
@@ -39,28 +37,69 @@ struct alm_arch_funcs __arch_funcs_acosh = {
     .def_arch = ALM_UARCH_VER_DEFAULT,
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
-            &ALM_PROTO_REF(acoshf),
-            &ALM_PROTO_REF(acosh),
-            NULL,                           /* vrs4 ? */
-            NULL,                           /* vrs8 ? */
-            NULL,                           /* vrd2 ? */
-            NULL,                           /* vrd4 ? */
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(acoshf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(acosh),
+#if 0
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX2(vrs4_acoshf),/* vrs4 ? */
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_AVX2(vrs8_acoshf),/* vrs8 ? */
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_acoshf),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_acosh),
+#endif
+        },
+
+        [ALM_UARCH_VER_ZEN] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(acoshf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(acosh),
+#if 0
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN(vrs4_acoshf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN(vrs8_acoshf),
+#endif
+        },
+
+        [ALM_UARCH_VER_ZEN2] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(acoshf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(acosh),
+#if 0
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN2(vrs4_acoshf), /* vrs4 ? */
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN2(vrs8_acoshf), /* vrs8 ? */
+#endif
+        },
+        [ALM_UARCH_VER_ZEN3] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(acoshf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(acosh),
+#if 0
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN3(vrs4_acoshf), /* vrs4 ? */
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN3(vrs8_acoshf), /* vrs8 ? */
+#endif
+        },
+
+        [ALM_UARCH_VER_ZEN4] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(acoshf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(acosh),
+#if 0
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN4(vrs4_acoshf), /* vrs4 ? */
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN4(vrs8_acoshf), /* vrs8 ? */
+
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_acoshf),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_acosh),
+#endif
         },
     },
 };
 
 void
-LIBM_IFACE_PROTO(acosh)(void *arg)
-{
+LIBM_IFACE_PROTO(acosh)(void *arg) {
     alm_ep_wrapper_t g_entry_acosh = {
         .g_ep = {
-        [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(acoshf),
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(acosh),
+            [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(acoshf),
+            [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(acosh),
 #if 0
-        [ALM_FUNC_VECT_SP_4] = &G_ENTRY_PT_PTR(vrs4_acoshf),
-        [ALM_FUNC_VECT_SP_8] = &G_ENTRY_PT_PTR(vrs8_acoshf),
-        [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_acosh),
-        [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_acosh),
+            [ALM_FUNC_VECT_SP_4] = &G_ENTRY_PT_PTR(vrs4_acoshf),
+            [ALM_FUNC_VECT_SP_8] = &G_ENTRY_PT_PTR(vrs8_acoshf),
+            [ALM_FUNC_VECT_SP_16] = &G_ENTRY_PT_PTR(vrs16_acoshf),
+            [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_acosh),
+            [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_acosh),
+            [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_acosh),
 #endif
         },
     };

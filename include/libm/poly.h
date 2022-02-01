f79d5225132c27c72f4c3b9816d39f8d9d6ae932
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-02-01 18:48:03 +0530

    libm: Evaluate 7 coeff polynomial with even exponents
    This it to be used in cosh scalar double
    
    Change-Id: Ib19825a2b02f23a532b609a7c907235f00e026d9
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-2010]

--- include/libm/poly.h
+++ include/libm/poly.h
@@ -412,10 +412,28 @@
          q;                                              \
          })
 
+/*
+* p(x) = C0*x^0 + C1*x^2 + C2*x^4 + C3*x^6 +C4*x^8 + C5*x^10 + C6*x^12
+*/
+#define POLY_EVAL_EVEN_7(x, c0, c1, c2, c3, c4, c5, c6) ({\
+         __typeof(x) q, a0, a1, a2, a3;                   \
+         __typeof(x) x2, x4, x6, x8, x10, x12;            \
+         x2 = x*x;                                        \
+         x4 = x2*x2;                                      \
+         x6 = x2*x4;                                      \
+         x8 = x4*x4;                                      \
+         x10 = x4*x6;                                     \
+         x12 = x6*x6;                                     \
+         a0 = c0 + c1*x2;                                 \
+         a1 = c2*x4 + c3*x6;                              \
+         a2 = c4*x8 + c5*x10;                             \
+         a3 = c6*x12;                                     \
+         q = a0 + a1;                                     \
+         q += a2 + a3;                                    \
+         q;                                               \
+         })
 
-         
-         
-/* 
+/*
  * poly = C0 + C1*r^2 + C2*r^4 + C3*r^6+ C4*r^8 \
  *
  *      = C0 + r^2*(C1 + C2*r^2) + r^4*(C3*r^2+C4*r^4)

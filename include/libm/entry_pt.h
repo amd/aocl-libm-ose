f72cef60dbede8b60c284799087804c4fa21f2bd
Xavier, Arnold joseph <arnoldjoseph.xavier@amd.com>
2022-02-07 13:24:31 +0530

    libm:clog: Enabled calls to clogf in iface
    Signed-off-by: Joseph Arnold Xavier <Arnoldjoseph.Xavier@amd.com>
    AMD-Internal: [CPUPL-1993]
    Change-Id: I2845e2a6c75297dc3c15f06e7493de6b42735efc

--- include/libm/entry_pt.h
+++ include/libm/entry_pt.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2021, Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -153,6 +153,7 @@ extern alm_ep_func_t        G_ENTRY_PT_PTR(truncf);
  */
 extern alm_ep_func_t        G_ENTRY_PT_PTR(cexp);
 extern alm_ep_func_t        G_ENTRY_PT_PTR(cexpf);
+extern alm_ep_func_t        G_ENTRY_PT_PTR(clogf);
 extern alm_ep_func_t        G_ENTRY_PT_PTR(cpow);
 extern alm_ep_func_t        G_ENTRY_PT_PTR(cpowf);
 

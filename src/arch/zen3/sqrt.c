e5e4a41585bcb190b408e601e4f78a1745b10308
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-05-13 19:00:56 +0530

    libm: optmized sqrt: Added iface and arch specific sources
    Change-Id: Ie05a06a8f900027aa6c0742dc33761f075f7a8c1
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-2142]

--- src/arch/zen3/sqrt.c
+++ src/arch/zen3/sqrt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2022 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -25,52 +25,7 @@
  *
  */
 
-#include <libm_macros.h>
-#include <libm/amd_funcs_internal.h>
-#include <libm/iface.h>
-#include <libm/entry_pt.h>
+#define ALM_OVERRIDE 1
 
-//
-#include <libm/arch/all.h>
-
-static const
-struct alm_arch_funcs __arch_funcs_sqrt = {
-    .def_arch = ALM_UARCH_VER_DEFAULT,
-    .funcs = {
-        [ALM_UARCH_VER_DEFAULT] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
-        },
-        [ALM_UARCH_VER_ZEN] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
-        },
-
-        [ALM_UARCH_VER_ZEN2] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
-        },
-
-        [ALM_UARCH_VER_ZEN3] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
-        },
-
-        [ALM_UARCH_VER_ZEN4] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
-            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
-        },
-    },
-};
-
-void
-LIBM_IFACE_PROTO(sqrt)(void *arg) {
-    alm_ep_wrapper_t g_entry_sqrt = {
-       .g_ep = {
-            [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(sqrtf),
-            [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(sqrt),
-        },
-    };
-
-    alm_iface_fixup(&g_entry_sqrt, &__arch_funcs_sqrt);
-}
+#include <libm/arch/zen3.h>
+#include "../../optmized/sqrt.c"

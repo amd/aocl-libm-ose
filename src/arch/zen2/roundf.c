af758b90b16fcaa9bd4c712ca5b96d9596a63a2d
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2022-06-01 16:16:34 +0530

    libm: optmized C variant for roundf scalar
    Change-Id: I33ddae6c8861b6e043ec0573b554136a65d59a90
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
    AMD-Internal: [CPUPL-2175]

--- src/arch/zen2/roundf.c
+++ src/arch/zen2/roundf.c
@@ -13,7 +13,7 @@
  *    specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * ANY roundRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
  * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
@@ -25,53 +25,8 @@
  *
  */
 
-#include <libm_macros.h>
-#include <libm/amd_funcs_internal.h>
-#include <libm/iface.h>
-#include <libm/entry_pt.h>
-#include <libm/arch/all.h>
+#define ALM_OVERRIDE 1
 
+#include <libm/arch/zen2.h> 
 
-static const
-struct alm_arch_funcs __arch_funcs_round = {
-    .def_arch = ALM_UARCH_VER_DEFAULT,
-    .funcs = {
-        [ALM_UARCH_VER_DEFAULT] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(roundf),
-            //[ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(round),
-        },
-
-        [ALM_UARCH_VER_ZEN] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(roundf),
-            //[ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(round),
-        },
-
-        [ALM_UARCH_VER_ZEN2] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(roundf),
-            //[ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(round),
-        },
-
-        [ALM_UARCH_VER_ZEN3] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(roundf),
-            //[ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(round),
-        },
-
-        [ALM_UARCH_VER_ZEN4] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(roundf),
-            //[ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(round),
-        },
-    },
-};
-
-void
-LIBM_IFACE_PROTO(round)(void *arg) {
-    alm_ep_wrapper_t g_entry_round = {
-       .g_ep = {
-        [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(roundf),
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(round),
-        },
-    };
-
-    alm_iface_fixup(&g_entry_round, &__arch_funcs_round);
-}
-
+#include "../../optmized/roundf.c"
---
+++ src/optmized/roundf.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2008-2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* Implementation notes
+    float amd_roundf (float x);
+    when x = NAN, return x, raise FE_INVALID
+    when x is QNAN, return x
+    when x is INF, return x.
+*/
+
+#include "fn_macros.h"
+#include "libm_util_amd.h"
+#include <libm/alm_special.h>
+#include <libm/amd_funcs_internal.h>
+#include <libm/typehelper.h>
+
+float ALM_PROTO_OPT(roundf)(float f) {
+    uint32_t ux, ux_temp, sign, exponent, mantissa;
+    int intexp;            /*Needs to be signed */
+
+    ux = asuint32(f);
+    sign = ux & SIGNBIT_SP32;
+
+    /*  if NAN or INF */
+    if (unlikely((ux & EXPBITS_SP32) == EXPBITS_SP32)) {
+    #ifdef WINDOWS
+        return __alm_handle_errorf(ux |= QNAN_MASK_32, 0);
+    #else
+        /* If NaN, raise exception, no exception for Infinity */
+        if (f != f) {
+            return __alm_handle_errorf(ux, AMD_F_INVALID);
+        }
+        return f;
+    #endif
+        return __alm_handle_errorf(ux, 0);
+    }
+
+    /*Get the exponent of the input*/
+    intexp = (ux & EXPBITS_SP32) >> 23;
+    intexp -= 0x7F;
+    /*If exponent > 22 then the number is already rounded*/
+    if (intexp > 22) {
+        return f;
+    }
+
+    if (intexp < 0) {
+        ux_temp = asuint32(f);
+        ux_temp &= 0x7FFFFFFF;
+        /*Add with a large number (2^23 +1) = 8388609.0F
+        to force an overflow*/
+        f = (f + 8388609.0F);
+        /*Subtract back with t he large number*/
+        f -= 8388609;
+        if (sign) {
+            ux_temp |= SIGNBIT_SP32;
+        }
+
+        return asfloat(ux_temp);
+    }
+    else {
+        /*if(intexp == -1)
+            u32exp = 0x3F800000;*/
+        ux &= 0x7FFFFFFF;
+        f += 0.5f;
+        exponent = ux & EXPBITS_SP32;
+        /*right shift then left shift to discard the decimal places*/
+        mantissa = (ux & MANTBITS_SP32) >> (23 - intexp);
+        mantissa = mantissa << (23 - intexp);
+        ux_temp = sign | exponent | mantissa;
+
+        return (asfloat(ux_temp));
+    }
+}

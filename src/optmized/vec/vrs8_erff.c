08c5f98cf24fbfa82ac4ee85bd1c3b9f1eec13f4
Edvin Hopkins <Edvin.Hopkins@amd.com>
2022-07-25 07:32:25 +0000

    libm: Adding faster variant of vrs8 erff using 16 deg polynomial
    Change-Id: I87933d5e70b5d260ecedc13c11a10642e230fa95
    Signed-off-by: Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>

--- src/optmized/vec/vrs8_erff.c
+++ src/optmized/vec/vrs8_erff.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020, Advanced Micro Devices. All rights reserved.
+ * Copyright (C) 2019-2022, Advanced Micro Devices. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -30,27 +30,16 @@
  *
  * Implementation Notes
  * ----------------------
- *  if x > 3.9192059040069580078125f
+ *  if |x| > 3.9192059040069580078125f
  *     return 1.0
  *
- *  if 0.974708974361419677734375f < x <= 3.9192059040069580078125f
- *      return -9.334529419817705e-2 + x * (1.770500458970012 + x * (-1.9873639831178735 + x * (3.272308933339834 +
- *              x * (-4.402751805448494 + x * (3.7608972096338276 + x * (-2.0966214980804354 +
- *              x * (0.7930106628290651 + x * (-0.20671156774709162 + x * (3.677264344574519e-2 +
- *              x * (-4.2806518649629016e-3 + x * (2.94780430932192e-4 + x * (-9.12466047071725e-6))))))))))));
- *
- *
- *  if 5.40909968549385666847229003906E-5f x < 0.974708974361419677734375f
- *      return -1.7016133165416813e-11f + x * (1.1283791722401213f + x * (-2.574859935324604e-7f + x * (-0.3761213392501202f +
- *               x * (-5.1212385080044055e-5f + x * (0.11314475379382989f + x * (-1.1672466078260007e-3f + x * (-2.393903580433729e-2f +
- *               x * (-4.896886935591104e-3f +  x * (1.0606170926673056e-2f + x * (-3.656783296404107e-3f + x * 4.0345739971510983e-4f))))))
- *
- *  if 6.60290488951886800350621342659E-9f < x < 5.40909968549385666847229003906E-5f
- *       return -1.860188690104156e-15f + x * (1.1283791677145343f + x * (-3.0517569546146566e-5f));
- *
- *  if x <= 6.60290488951886800350621342659E-9f
- *       return  x + 0x1.06eba8p-3f * x;
- *
+ *  else
+ *      return x * (0x1.20dd7890d27e1cec99fce48c29cp0 + x * (-0x1.ab4bed70f238422edeeba9c558p-16 + x * (-0x1.80a1bd5878e0b0689c5ff4fcdd4p-2 +
+ *             x * (-0x1.07cb4cde6a7d9528c8a732990e4p-8 + x * (0x1.092cba598f96f00ddc5854cf7cp-3 + x * (-0x1.51f0ce4ac87c55f11f685864714p-5 + 
+ *             x * (0x1.4101f320bf8bc4d41c228faaa6cp-5 + x * (-0x1.2300882a7d1b712726997de80ep-4 + x * (0x1.d45745fff0e4b6d0604a9ab6284p-5 +
+ *             x * (-0x1.9eb1491956e31ded96176d7c8acp-6 + x * (0x1.b9183fc75d326b9044bc63c9694p-8 + x * (-0x1.10e8f8c89ad8645e7d769cd596cp-10 +
+ *             x * (0x1.224ffc80cc19957a48ecedad6c8p-14 + x * (0x1.12a30f42c71308321e7e7cb0174p-18 + x * (-0x1.155445e2e006723066d72d22ddcp-20 +
+ *             x * 0x1.c6a4181da4ef76f22bd39bb5dcp-25)))))))))))))))
  *
  */
 
@@ -69,145 +58,63 @@
 
 
 static const struct {
-    v_f32x8_t   tblsz_byln2;
-    v_f32x8_t   ln2_tbl_head, ln2_tbl_tail;
-    v_f32x8_t   huge;
-    v_u32x8_t   all_true, bound1, bound2, bound3, bound4;
+    v_u32x8_t   bound;
     v_u32x8_t   mask;
     v_u32x8_t   one;
-    v_f32x8_t   poly_1;
-    v_f32x8_t   poly_2[3];
-    v_f32x8_t   poly_3[12];
-    v_f64x4_t   poly_4[13];
+    v_f64x4_t   poly[16];
 } v_erff_data = {
-              .tblsz_byln2 =  _MM256_SET1_PS8(0x1.71547652b82fep+0f),
-              .ln2_tbl_head = _MM256_SET1_PS8(0x1.63p-1),
-              .ln2_tbl_tail = _MM256_SET1_PS8(-0x1.bd0104p-13),
-              .huge        =  _MM256_SET1_PS8(0x1.8p+23) ,
-              .bound1     =   _MM256_SET1_I32(0x31E2DFC4),
-              .bound2      =  _MM256_SET1_I32(0x3862DFC4),
-              .bound3      =  _MM256_SET1_I32(0x3F798687),
-              .bound4      =  _MM256_SET1_I32(0x407AD447),
-              .all_true    =  _MM256_SET1_I32(0xffffffff),
+              .bound       =  _MM256_SET1_I32(0x407AD447),
               .mask        =  _MM256_SET1_I32(0x7FFFFFFF),
               .one         =  _MM256_SET1_I32(0x3f800000),
 
              // Polynomial coefficients obtained using Remez algorithm
-              .poly_1   =     _MM256_SET1_PS8(0x1.06eba8p-3f),
+              .poly = {
 
-
-              .poly_2 = {
-                              _MM256_SET1_PS8(-1.860188690104156e-15f),
-                              _MM256_SET1_PS8(1.1283791677145343f),
-                              _MM256_SET1_PS8(-3.0517569546146566e-5f),
-              },
-
-              .poly_3 = {
-                              _MM256_SET1_PS8(-1.7016133165416813e-11f),
-                              _MM256_SET1_PS8(1.1283791722401213f),
-                              _MM256_SET1_PS8(-2.574859935324604e-7f),
-                              _MM256_SET1_PS8(-0.3761213392501202f),
-                              _MM256_SET1_PS8(-5.1212385080044055e-5f),
-                              _MM256_SET1_PS8(0.11314475379382989f),
-                              _MM256_SET1_PS8(-1.1672466078260007e-3f),
-                              _MM256_SET1_PS8(-2.393903580433729e-2f),
-                              _MM256_SET1_PS8(-4.896886935591104e-3f),
-                              _MM256_SET1_PS8(1.0606170926673056e-2f),
-                              _MM256_SET1_PS8(-3.656783296404107e-3f),
-                              _MM256_SET1_PS8(4.0345739971510983e-4f),
-
+                            _MM_SET1_PD4(0x1.20dd7890d27e1cec99fce48c29cp0),
+                            _MM_SET1_PD4(-0x1.ab4bed70f238422edeeba9c558p-16),
+                            _MM_SET1_PD4(-0x1.80a1bd5878e0b0689c5ff4fcdd4p-2),
+                            _MM_SET1_PD4(-0x1.07cb4cde6a7d9528c8a732990e4p-8),
+                            _MM_SET1_PD4(0x1.092cba598f96f00ddc5854cf7cp-3),
+                            _MM_SET1_PD4(-0x1.51f0ce4ac87c55f11f685864714p-5),
+                            _MM_SET1_PD4(0x1.4101f320bf8bc4d41c228faaa6cp-5),
+                            _MM_SET1_PD4(-0x1.2300882a7d1b712726997de80ep-4),
+                            _MM_SET1_PD4(0x1.d45745fff0e4b6d0604a9ab6284p-5),
+                            _MM_SET1_PD4(-0x1.9eb1491956e31ded96176d7c8acp-6),
+                            _MM_SET1_PD4(0x1.b9183fc75d326b9044bc63c9694p-8),
+                            _MM_SET1_PD4(-0x1.10e8f8c89ad8645e7d769cd596cp-10),
+                            _MM_SET1_PD4(0x1.224ffc80cc19957a48ecedad6c8p-14),
+                            _MM_SET1_PD4(0x1.12a30f42c71308321e7e7cb0174p-18),
+                            _MM_SET1_PD4(-0x1.155445e2e006723066d72d22ddcp-20),
+                            _MM_SET1_PD4(0x1.c6a4181da4ef76f22bd39bb5dcp-25),
 
               },
-
-              .poly_4 = {
-                              _MM_SET1_PD4(-9.334529419817705e-2),
-                              _MM_SET1_PD4(1.770500458970012),
-                              _MM_SET1_PD4(-1.9873639831178735),
-                              _MM_SET1_PD4(3.272308933339834),
-                              _MM_SET1_PD4(-4.402751805448494),
-                              _MM_SET1_PD4(3.7608972096338276),
-                              _MM_SET1_PD4(-2.0966214980804354),
-                              _MM_SET1_PD4(0.7930106628290651),
-                              _MM_SET1_PD4(-0.20671156774709162),
-                              _MM_SET1_PD4(3.677264344574519e-2),
-                              _MM_SET1_PD4(-4.2806518649629016e-3),
-                              _MM_SET1_PD4(2.94780430932192e-4),
-                              _MM_SET1_PD4(-9.12466047071725e-6),
-
-
-              },
-
 
 };
 
-#define TBL_LN2   v_erff_data.tblsz_byln2
-#define LN2_TBL_H v_erff_data.ln2_tbl_head
-#define LN2_TBL_T v_erff_data.ln2_tbl_tail
-#define EXPF_BIAS v_erff_data.expf_bias
-#define EXP_HUGE  v_erff_data.huge
-#define BOUND1    v_erff_data.bound1
-#define BOUND2    v_erff_data.bound2
-#define BOUND3    v_erff_data.bound3
-#define BOUND4    v_erff_data.bound4
-#define ALL_TRUE  v_erff_data.all_true
+#define BOUND     v_erff_data.bound
 #define MASK      v_erff_data.mask
 #define ONE       v_erff_data.one
 
-#define A1 v_erff_data.poly_1
-
-/* Coefficients for 3-degree polynomial */
-#define B1 v_erff_data.poly_2[0]
-#define B2 v_erff_data.poly_2[1]
-#define B3 v_erff_data.poly_2[2]
-
-/* Coefficients for 11-degree polynomial */
-#define C1  v_erff_data.poly_3[0]
-#define C2  v_erff_data.poly_3[1]
-#define C3  v_erff_data.poly_3[2]
-#define C4  v_erff_data.poly_3[3]
-#define C5  v_erff_data.poly_3[4]
-#define C6  v_erff_data.poly_3[5]
-#define C7  v_erff_data.poly_3[6]
-#define C8  v_erff_data.poly_3[7]
-#define C9  v_erff_data.poly_3[8]
-#define C10 v_erff_data.poly_3[9]
-#define C11 v_erff_data.poly_3[10]
-#define C12 v_erff_data.poly_3[11]
-
-/* Coefficients for 12-degree polynomial */
-#define D1  v_erff_data.poly_4[0]
-#define D2  v_erff_data.poly_4[1]
-#define D3  v_erff_data.poly_4[2]
-#define D4  v_erff_data.poly_4[3]
-#define D5  v_erff_data.poly_4[4]
-#define D6  v_erff_data.poly_4[5]
-#define D7  v_erff_data.poly_4[6]
-#define D8  v_erff_data.poly_4[7]
-#define D9  v_erff_data.poly_4[8]
-#define D10 v_erff_data.poly_4[9]
-#define D11 v_erff_data.poly_4[10]
-#define D12 v_erff_data.poly_4[11]
-#define D13 v_erff_data.poly_4[12]
-
+/* Coefficients for 16-degree polynomial */
+#define A1  v_erff_data.poly[0]
+#define A2  v_erff_data.poly[1]
+#define A3  v_erff_data.poly[2]
+#define A4  v_erff_data.poly[3]
+#define A5  v_erff_data.poly[4]
+#define A6  v_erff_data.poly[5]
+#define A7  v_erff_data.poly[6]
+#define A8  v_erff_data.poly[7]
+#define A9  v_erff_data.poly[8]
+#define A10 v_erff_data.poly[9]
+#define A11 v_erff_data.poly[10]
+#define A12 v_erff_data.poly[11]
+#define A13 v_erff_data.poly[12]
+#define A14  v_erff_data.poly[13]
+#define A15  v_erff_data.poly[14]
+#define A16  v_erff_data.poly[15]
 
 
 #define SCALAR_ERFF ALM_PROTO_OPT(erff)
-
-
-/*
-*/
-
-static inline int test_condition_for_all(v_u32x8_t cond) {
-
-    for(int i =0; i < 8; i++) {
-
-        if(cond[i] != 0xffffffff)
-            return 0;
-
-    }
-
-    return 1;
-}
 
 v_f32x8_t
 ALM_PROTO_OPT(vrs8_erff)(v_f32x8_t _x) {
@@ -220,79 +127,31 @@ ALM_PROTO_OPT(vrs8_erff)(v_f32x8_t _x) {
 
     v_f32x8_t x = as_v8_f32_u32(uvx);
 
-    v_u32x8_t cond1 = uvx <= BOUND1;
+    v_f32x4_t _y1 = _mm256_extractf128_ps(x, 0);
 
-    if(test_condition_for_all(cond1)) {
+    v_f32x4_t _y2 = _mm256_extractf128_ps(x, 1);
 
-       result = x + A1 * x;
+    v_f64x4_t _y1d = _mm256_cvtps_pd(_y1);
 
-       result = as_v8_f32_u32(sign | as_v8_u32_f32(result));
+    v_f64x4_t _y2d = _mm256_cvtps_pd(_y2);
 
-       return result;
+    /* Special implementation of Horner's method for polynomial through the origin*/
+    v_f64x4_t result1 = POLY_EVAL_HORNER_16_0(_y1d, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16);
 
-    }
+    v_f64x4_t result2 = POLY_EVAL_HORNER_16_0(_y2d, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16);
 
-    v_u32x8_t cond2 = uvx <= BOUND2;
+    v_f32x8_t result3 =  _mm256_setr_m128(_mm256_cvtpd_ps(result1), _mm256_cvtpd_ps(result2));
 
-    if(test_condition_for_all(cond2)) {
+    // Vectorized ternary operator to check for values that should be rounded to 1.0
+    v_f32x8_t bound_float = as_v8_f32_u32(BOUND);
 
-       result  = B1 + x * (B2 + x * (B3));
+    v_f32x8_t mask1 = _mm256_cmp_ps(bound_float,x, _CMP_LT_OQ);
 
-       result = as_v8_f32_u32(sign | as_v8_u32_f32(result));
+    v_f32x8_t fONE = as_v8_f32_u32(ONE);
 
-       return result;
+    result = _mm256_blendv_ps(result3,fONE,mask1);
 
-    }
-
-    v_u32x8_t cond3 = uvx <= BOUND3;
-
-    if(test_condition_for_all(cond3)) {
-
-        result = POLY_EVAL_HORNER_11(x, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12);
-
-        result = as_v8_f32_u32(sign | as_v8_u32_f32(result));
-
-        return result;
-
-    }
-
-    v_u32x8_t cond4 = uvx <= BOUND4;
-
-    if(test_condition_for_all(cond4)) {
-
-        v_f32x4_t _y1 = _mm256_extractf128_ps(x, 0);
-
-        v_f32x4_t _y2 = _mm256_extractf128_ps(x, 1);
-
-        v_f64x4_t _y1d = _mm256_cvtps_pd(_y1);
-
-        v_f64x4_t _y2d = _mm256_cvtps_pd(_y2);
-
-        v_f64x4_t result1 = POLY_EVAL_HORNER_12(_y1d, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13);
-
-        v_f64x4_t result2 = POLY_EVAL_HORNER_12(_y2d, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13);
-
-        result =  _mm256_setr_m128(_mm256_cvtpd_ps(result1), _mm256_cvtpd_ps(result2));
-
-        result = as_v8_f32_u32(sign | as_v8_u32_f32(result));
-
-        return result;
-
-    }
-
-    cond4 = uvx > BOUND4;
-
-    if(test_condition_for_all(cond4)) {
-
-        return as_v8_f32_u32(sign | ONE);
-
-    }
-
-    for(uint32_t i = 0; i < 8; i++) {
-
-        result[i] = SCALAR_ERFF(_x[i]);
-
-    }
+    result = as_v8_f32_u32(sign | as_v8_u32_f32(result));
 
     return result;
 

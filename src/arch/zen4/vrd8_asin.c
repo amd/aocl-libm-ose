43f97375b68458bddbc5a76bbc8ee8c3ce9d4bec
Kiran MVijay <kiran.mvijay@amd.com>
2022-10-06 07:37:07 +0000

    libm: Added vector 8 variant asin() for zen4
    Modified it to return correct values.
    [MAX ULP ERR = 2]
    
    Change-Id: I42b62b58e4a087afb08eb18f486eff7ed672fc2f
    Signed-off-by: Kiran M Vijay <kiran.mvijay@amd.com>
    AMD-Internal: [CPUPL-2377]

--- src/arch/zen4/vrd8_asin.c
+++ src/arch/zen4/vrd8_asin.c
@@ -51,47 +51,60 @@
 #include <libm/arch/zen4.h>
 
 static struct {
-    v_f64x8_t THEEPS, HALF, ONE, poly_asin[5], PI_BY_TWO;
+    v_f64x8_t HALF, ONE, poly_asin[13];
     v_u64x8_t mask_64;
     double pi_by_two, two, one, half;
 } v8_asin_data = {
     .mask_64 = _MM512_SET1_U64x8(0x7FFFFFFFFFFFFFFFUL),
-    .THEEPS = _MM512_SET1_PD8(0x1.6a09e6p-12),
     .HALF = _MM512_SET1_PD8(0.5),
     .ONE = _MM512_SET1_PD8(0x1p0),
-    .PI_BY_TWO = _MM512_SET1_PD8(0x1.921fb6p-1),
     // Polynomial coefficients obtained using fpminimax algorithm from Sollya
     .poly_asin = {
         // polynomial coeffs are obtained from vrd8_asin.sollya
-        _MM512_SET1_PD8(0x1.5555fcp-3),
-        _MM512_SET1_PD8(0x1.32f8d8p-4),
-        _MM512_SET1_PD8(0x1.7525aap-5),
-        _MM512_SET1_PD8(0x1.86e46ap-6),
-        _MM512_SET1_PD8(0x1.5d456cp-5),
+        _MM512_SET1_PD8(0x1.ffffffffdecd3p-1),
+        _MM512_SET1_PD8(0x1.55555555552aap-3),
+        _MM512_SET1_PD8(0x1.333333337cbaep-4),
+        _MM512_SET1_PD8(0x1.6db6db3c0984p-5),
+        _MM512_SET1_PD8(0x1.f1c72dd86cbafp-6),
+        _MM512_SET1_PD8(0x1.6e89d3ff33aa4p-6),
+        _MM512_SET1_PD8(0x1.1c6d83ae664b6p-6),
+        _MM512_SET1_PD8(0x1.c6e1568b90518p-7),
+        _MM512_SET1_PD8(0x1.8f6a58977fe49p-7),
+        _MM512_SET1_PD8(0x1.a6ab10b3321bp-8),
+        _MM512_SET1_PD8(0x1.43305ebb2428fp-6),
+        _MM512_SET1_PD8(-0x1.0e874ec5e3157p-6),
+        _MM512_SET1_PD8(0x1.06eec35b3b142p-5),
     },
     // Values of factors of pi required to calculate asin
-    .pi_by_two = 0x1.921fb6p-1,
+    .pi_by_two = 0x1.921fb54442d18p-1,
     .two = 0x1p1,
     .one = 0x1p0,
     .half = 0x1p-1,
 };
-#define V8_ASIN_THEEPS     v8_asin_data.THEEPS
+
 #define V8_ASIN_HALF       v8_asin_data.HALF
 #define V8_ASIN_ONE        v8_asin_data.ONE
-#define V8_PI_BY_TWO        v8_asin_data.PI_BY_TWO
 
 #define V8_ASIN_MASK_64    v8_asin_data.mask_64
 
-#define F_PI_BY_TWO    v8_asin_data.pi_by_two
+#define V8_PI_BY_TWO    v8_asin_data.pi_by_two
 #define TWO          v8_asin_data.two
 #define F_ONE        v8_asin_data.one
 #define F_HALF       v8_asin_data.half
 
-#define C1 v8_asin_data.poly_asin[0]
-#define C2 v8_asin_data.poly_asin[1]
-#define C3 v8_asin_data.poly_asin[2]
-#define C4 v8_asin_data.poly_asin[3]
-#define C5 v8_asin_data.poly_asin[4]
+#define C0 v8_asin_data.poly_asin[0]
+#define C1 v8_asin_data.poly_asin[1]
+#define C2 v8_asin_data.poly_asin[2]
+#define C3 v8_asin_data.poly_asin[3]
+#define C4 v8_asin_data.poly_asin[4]
+#define C5 v8_asin_data.poly_asin[5]
+#define C6 v8_asin_data.poly_asin[6]
+#define C7 v8_asin_data.poly_asin[7]
+#define C8 v8_asin_data.poly_asin[8]
+#define C9 v8_asin_data.poly_asin[9]
+#define C10 v8_asin_data.poly_asin[10]
+#define C11 v8_asin_data.poly_asin[11]
+#define C12 v8_asin_data.poly_asin[12]
 
 #define ZERO                0
 #define VECTOR_LENGTH       8
@@ -99,7 +112,7 @@ static struct {
 v_f64x8_t
 ALM_PROTO_ARCH_ZN4(vrd8_asin)(v_f64x8_t x)
 {
-    v_f64x8_t r, poly, result, G = _MM512_SET1_PD8(0.0), n =  _MM512_SET1_PD8(F_PI_BY_TWO);
+    v_f64x8_t r, poly, result, G = _MM512_SET1_PD8(0.0), n =  _MM512_SET1_PD8(V8_PI_BY_TWO);
     v_u64x8_t ux = as_v8_u64_f64(x);
     v_u64x8_t sign = ux & ~V8_ASIN_MASK_64;
     r  = as_v8_f64_u64(ux & V8_ASIN_MASK_64);
@@ -110,13 +123,14 @@ ALM_PROTO_ARCH_ZN4(vrd8_asin)(v_f64x8_t x)
             G[i] = F_HALF * (F_ONE - r[i]);
             r[i] = -TWO * ALM_PROTO_KERN(sqrt)(G[i]);
         } else {
-            n[i] = 0.0f;
+            n[i] = 0.0;
             G[i] = r[i] * r[i];
         }
     }
 
-    poly = r + r * G * POLY_EVAL_5(G,C1,C2,C3,C4,C5);
+    poly = r + r * G * POLY_EVAL_12(G,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12); 
     poly =  (n + poly) + n;
+
     result = as_v8_f64_u64(as_v8_u64_f64(poly) ^ sign);
 
     return result;

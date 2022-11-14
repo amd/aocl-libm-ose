22059ee37de6f990628eb44aa3953111620e7328
Kiran Vijay <Kiran.MVijay@amd.com>
2022-11-14 20:38:41 +0530

    Minor change in nearbyintf()
    Change-Id: I615a9d29b11a56964a2cc98e5478b9353ba81b6c
    Signed-off-by: Kiran M Vijay <kiran.mvijay@amd.com>
    AMD-Internal: [CPUPL-2736]

--- src/optmized/nearbyintf.c
+++ src/optmized/nearbyintf.c
@@ -29,41 +29,40 @@
 #include "libm_errno_amd.h"
 #include <libm/alm_special.h>
 #include <libm/amd_funcs_internal.h>
-
+#include <libm/typehelper.h>
 
 float ALM_PROTO_OPT(nearbyintf)(float x)
 {
-    /* Check for input range */
-    //unsigned int mxscr_stat;
-    UT32 checkbits,val_2p23;
+    uint32_t checkbits_u32;
     uint32_t sign;
-    checkbits.f32=x;
+    float val_2p23_f32;
+    checkbits_u32 = asuint32(x);
 
     // Clear the sign bit and check if the value can be rounded(i.e check if exponent less than 23)
-    if( (checkbits.u32 & POS_BITSET_F32) > EXP_VAL_23_F32)   // -> EXP value is 150 (-127 bias equals 23)
+    if((checkbits_u32 & POS_BITSET_F32) > EXP_VAL_23_F32)
     {
         // Check nan or inf
-        if(    (checkbits.u32 & EXPBITS_SP32) == EXPBITS_SP32)
+        if((checkbits_u32 & EXPBITS_SP32) == EXPBITS_SP32)
         {
-            if((checkbits.u32 & MANTBITS_SP32) == POS_ZERO_F32)
+            if((checkbits_u32 & MANTBITS_SP32) == POS_ZERO_F32)
             {
                 // x is Inf
 #ifdef WINDOWS
-                return  __alm_handle_errorf(checkbits.u32, AMD_F_INVALID);
+                return  __alm_handle_errorf(checkbits_u32, AMD_F_INVALID);
 #else
-                return  __alm_handle_errorf(checkbits.u32, AMD_F_NONE);
+                return  __alm_handle_errorf(checkbits_u32, AMD_F_NONE);
 #endif
             }
             else
             {
                 // x is NaN
 #ifdef WINDOWS
-                return  __alm_handle_errorf(checkbits.u32 | QNAN_MASK_32, AMD_F_NONE);
+                return  __alm_handle_errorf(checkbits_u32 | QNAN_MASK_32, AMD_F_NONE);
 #else
-                if (checkbits.u32 & QNAN_MASK_32)
-                    return  __alm_handle_errorf(checkbits.u32 | QNAN_MASK_32, AMD_F_NONE);
+                if(checkbits_u32 & QNAN_MASK_32)
+                    return  __alm_handle_errorf(checkbits_u32 | QNAN_MASK_32, AMD_F_NONE);
                 else
-                    return  __alm_handle_errorf(checkbits.u32 | QNAN_MASK_32, AMD_F_INVALID);
+                    return  __alm_handle_errorf(checkbits_u32 | QNAN_MASK_32, AMD_F_INVALID);
 #endif
             }
         }
@@ -71,24 +70,11 @@ float ALM_PROTO_OPT(nearbyintf)(float x)
             return x;
     }
 
-    sign =  checkbits.u32 & SIGNBIT_SP32;
-    val_2p23.u32 = sign | EXP_VAL_23_F32;
+    sign =  checkbits_u32 & SIGNBIT_SP32;
 
-    // By default MASKING flags are all set to 1, so all exceptions are masked.
-    // When an an exception is masked, the processor simply sets the exception flag and continues the operation.
-    // NOTE: If the exception is unmasked, the flag is also set but additionally an exception handler is invoked.
-    /*
-    mxscr_stat = _mm_getcsr();
-    if(unlikely( (mxscr_stat & _MM_MASK_UNDERFLOW) != _MM_MASK_UNDERFLOW))
-    {
-        _mm_setcsr(mxscr_stat | _MM_MASK_UNDERFLOW); // _MM_MASK_UNDERFLOW = 0x00000800
-        val_2p23.f32 = (x + val_2p23.f32) - val_2p23.f32;
-        mxscr_stat = mxscr_stat & 0xffffffc0; // clear the exceptions
-        _mm_setcsr(mxscr_stat);
-    }
-    */
-    // This extra line is to take care of denormals and various rounding modes
-    val_2p23.u32 = ((val_2p23.u32 << 1) >> 1) | sign;
+    val_2p23_f32 = asfloat(sign | EXP_VAL_23_F32);
+    val_2p23_f32 = (x + val_2p23_f32) - val_2p23_f32;
+    val_2p23_f32 = asfloat(asuint32(val_2p23_f32) | sign);
 
-    return (val_2p23.f32);
+    return val_2p23_f32;
 }

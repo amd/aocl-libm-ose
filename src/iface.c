52bd28409a9b1c5257a8b71615f328d6954ed1a8
Kiran Vijay <Kiran.MVijay@amd.com>
2023-01-09 11:43:10 +0530

    Changing libcpu API calls. Linking libstdc++
    API calls to check CPU arch is changed.
    libstdc++ is explicitly linked from libm side,
    when using static libcpu library.
    
    Change-Id: Icc36d1cd051d001b9d5960a038b9212f971913a2

--- src/iface.c
+++ src/iface.c
@@ -28,7 +28,7 @@
 #include <stddef.h>                     /* for NULL */
 
 #ifdef USE_AOCL_CPUID
-#include "alci/cpu_features.h"
+#include "alci/arch.h"
 #endif
 #include <libm/entry_pt.h>
 #include <libm/iface.h>
@@ -160,13 +160,13 @@ alm_get_uach(void)
 {
     alm_uarch_ver_t arch_ver;
 
-    if (alc_cpu_arch_is_zen4())
+    if (alcpu_arch_is_zen4())
         arch_ver = ALM_UARCH_VER_ZEN4;
-    else if (alc_cpu_arch_is_zen3())
+    else if (alcpu_arch_is_zen3())
         arch_ver = ALM_UARCH_VER_ZEN3;
-    else if (alc_cpu_arch_is_zen2())
+    else if (alcpu_arch_is_zen2())
         arch_ver = ALM_UARCH_VER_ZEN2;
-    else if (alc_cpu_arch_is_zen())
+    else if (alcpu_arch_is_zen())
         arch_ver = ALM_UARCH_VER_ZEN;
     else
         arch_ver = ALM_UARCH_VER_DEFAULT;

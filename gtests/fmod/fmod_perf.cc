45db7c2eb6c29ca1039b35c9f84a70b46fc93024
Kiran Vijay <Kiran.MVijay@amd.com>
2023-03-06 18:10:54 +0530

    libm:gtests: Optimizing fmod() function
    Optimizing fmod() function while converting from Assembly code to C language
    Also, updated the test data for conformance tests of fmod and fmodf
    
    Change-Id: I32def3906b07be1cfefe3822334761338cddd28b
    Signed-off-by: Kiran M Vijay <kiran.mvijay@amd.com>
    AMD-Internal: [CPUPL-3057]

---
+++ gtests/fmod/fmod_perf.cc
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2008-2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <chrono>
+#include "benchmark.h"
+#include "almtestperf.h"
+#include "callback.h"
+
+using namespace std;
+using namespace ALM;
+
+int AlmTestPerfFramework::AlmTestPerformance(InputParams *params) {
+  string funcnam = "AoclLibm";
+  string libm;
+
+  if((params->fwidth == ALM::FloatWidth::E_ALL) ||
+    (params->fwidth == ALM::FloatWidth::E_F32)) {
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Scalar)) {
+      string varnam = "_s1s(fmodf)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTestf, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_4)) {
+      string varnam = "_v4s(fmodf)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest4f, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_8)) {
+      string varnam = "_v8s(fmodf)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest8f, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    #if defined(__AVX512__)
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_16)) {
+      string varnam = "_v16s(fmodf)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest16f, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    #endif
+  }
+
+  if((params->fwidth == ALM::FloatWidth::E_ALL) ||
+    (params->fwidth == ALM::FloatWidth::E_F64)) {
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Scalar)) {
+      string varnam = "_s1d(fmod)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTestd, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_2)) {
+      string varnam = "_v2d(fmod)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest2d, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_4)) {
+      string varnam = "_v4d(fmod)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest4d, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    #if defined(__AVX512__)
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_8)) {
+      string varnam = "_v8d(fmod)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest8d, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    #endif
+  }
+
+  size_t retval = benchmark::RunSpecifiedBenchmarks();
+
+  return (int)retval;
+}
+
+AlmTestPerfFramework::~AlmTestPerfFramework() {
+#if defined(DEBUG_PRINTS)
+  cout << "AlmTestPerfFramework destructor completed" << endl;
+#endif
+}

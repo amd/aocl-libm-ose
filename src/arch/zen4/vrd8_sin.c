8b5b924676128f46501b798318726d5e8642c8b8
Jonathan Gibson <Jonathan.Gibson@amd.com>
2023-03-22 15:31:18 +0000

    Corrected the function signature in the header.
    Change-Id: I3dccceb9d846e11eed962dc1199542091f928c24

--- src/arch/zen4/vrd8_sin.c
+++ src/arch/zen4/vrd8_sin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2022 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2023 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -38,7 +38,7 @@
 
 /*
  * Signature:
- *    v_f64x8_t amd_vrd4_sin(v_f64x8_t x)
+ *    v_f64x8_t amd_vrd8_sin(v_f64x8_t x)
  *
  *
  *

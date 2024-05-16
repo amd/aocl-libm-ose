420caba2df58a8ec39d4e45061ecd905f5edb37a
Nimmy Krishnan <nimmy.krishnan@amd.com>
2024-05-16 17:08:46 +0530

    libm:iface: Adding v2d_sqrt to iface
    Signed-off-by: Nimmy Krishnan <nimmy.krishnan@amd.com>
    Change-Id: I568ebddb410ade49e8281e4ff19b6c40aaaedd54
    AMD-Internal: [CPUPL-5107]

--- src/iface/sqrt.c
+++ src/iface/sqrt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2022 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -40,25 +40,30 @@ struct alm_arch_funcs __arch_funcs_sqrt = {
         [ALM_UARCH_VER_DEFAULT] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_sqrt),
         },
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_sqrt),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_sqrt),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_sqrt),
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_OPT(sqrtf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_OPT(sqrt),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_sqrt),
         },
     },
 };
@@ -69,6 +74,7 @@ LIBM_IFACE_PROTO(sqrt)(void *arg) {
        .g_ep = {
             [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(sqrtf),
             [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(sqrt),
+            [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_sqrt),
         },
     };
 

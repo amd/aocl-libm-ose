1d1e420e7bccb0c57029e083241302cd0349520d
kimvijay <kiran.mvijay@amd.com>
2024-06-25 10:03:51 +0000

    libm: Added vrsa & vrda exp10 variants in C
    Converted & optimized vrsa & vrda variants of exp10 function from
    assembly code to C.
    
    Improved performance:
    vrsa_exp10f: 156%
    vrda_exp10:  95%
    
    Change-Id: I757998d4dc546304bafd8dfad63417e52683a54a
    Signed-off-by: Kiran M Vijay <kiran.mvijay@amd.com>
    AMD-Internal: [CPUPL-5170]

--- src/iface/exp10.c
+++ src/iface/exp10.c
@@ -43,8 +43,8 @@ struct alm_arch_funcs __arch_funcs_exp10 = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(exp10),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_FMA3(vrsa_exp10f),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_FMA3(vrda_exp10), 
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX2(vrsa_exp10f),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_exp10), 
         },
 
             [ALM_UARCH_VER_ZEN] = {
@@ -52,8 +52,8 @@ struct alm_arch_funcs __arch_funcs_exp10 = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(exp10),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_FMA3(vrsa_exp10f),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_FMA3(vrda_exp10),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN(vrsa_exp10f),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN(vrda_exp10),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
@@ -61,8 +61,8 @@ struct alm_arch_funcs __arch_funcs_exp10 = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(exp10),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_FMA3(vrsa_exp10f),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_FMA3(vrda_exp10),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN2(vrsa_exp10f),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN2(vrda_exp10),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
@@ -70,8 +70,8 @@ struct alm_arch_funcs __arch_funcs_exp10 = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(exp10),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_FMA3(vrsa_exp10f),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_FMA3(vrda_exp10),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN3(vrsa_exp10f),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN3(vrda_exp10),
         },
 
             [ALM_UARCH_VER_ZEN4] = {
@@ -79,9 +79,8 @@ struct alm_arch_funcs __arch_funcs_exp10 = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(exp10),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
-
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_FMA3(vrsa_exp10f),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_FMA3(vrda_exp10),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN4(vrsa_exp10f),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_exp10),
         },
 
     },

6b6193f9b7a9bb733f70421b6824d2ef3a8b9505
Nimmy Krishnan <nimmy.krishnan@amd.com>
2024-07-30 07:34:08 +0000

    libm:iface: Added single precision vector versions of powx to iface
    Added vrs4, vrs8 and vrs16 versions of powx to iface
    
    Signed-off-by: Nimmy Krishnan <nimmy.krishnan@amd.com>
    Change-Id: I85805390a24c2ead7df32fd6c9206b59d8879189
    AMD-Internal: [CPUPL-5497]

--- src/iface/powx.c
+++ src/iface/powx.c
@@ -40,27 +40,41 @@ struct alm_arch_funcs __arch_funcs_powx = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX2(vrd4_powx),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_powx),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX2(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_AVX2(vrs8_powxf),
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_powxf),
+
         },
 
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN(vrd4_powx),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN(vrs8_powxf),
+
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN2(vrd4_powx),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN2(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN2(vrs8_powxf),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN3(vrd4_powx),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN3(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN3(vrs8_powxf),
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN4(vrd4_powx),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_powx),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN4(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN4(vrs8_powxf),
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_powxf),
         },
 
         [ALM_UARCH_VER_ZEN5] = {
@@ -78,6 +92,9 @@ LIBM_IFACE_PROTO(powx)(void *arg) {
             [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_powx),
             [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_powx),
+            [ALM_FUNC_VECT_SP_4] = &G_ENTRY_PT_PTR(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &G_ENTRY_PT_PTR(vrs8_powxf),
+            [ALM_FUNC_VECT_SP_16] = &G_ENTRY_PT_PTR(vrs16_powxf),
         },
     };
 

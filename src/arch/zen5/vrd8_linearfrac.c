bcd75602d488460eefe38b04c9d07300eab594b7
Nimmy Krishnan <nimmy.krishnan@amd.com>
2024-08-04 19:37:06 +0000

    libm:linearfrac: Adding Zen5 versions of double precision vector versions
    Added source files for Zen5 versions of vrd2, vrd4 and vrd8 versions of linearfrac
    Added Zen5 double precision vector versions to iface
    
    Signed-off-by: Nimmy Krishnan <nimmy.krishnan@amd.com>
    Change-Id: I57075e6732d804e33a6c6f80b0ea7e23353962c8
    AMD-Internal: [CPUPL-5489]

---
+++ src/arch/zen5/vrd8_linearfrac.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2024, Advanced Micro Devices. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <libm_util_amd.h>
+#include <libm/alm_special.h>
+
+#include <libm_macros.h>
+#include <libm/amd_funcs_internal.h>
+#include <libm/types.h>
+#include <libm/typehelper.h>
+#include <libm/typehelper-vec.h>
+#include <libm/compiler.h>
+
+#include <libm/arch/zen5.h>
+
+/*
+C implementation of Linearfrac
+
+Signature:
+    v_f64x8_t linearfrac(v_f64x8_t a, v_f64x8_t b, double scalea, double shifta, double scaleb, double shiftb)
+
+Implementation notes:
+
+    Linearfrac function performs a linear fraction transformation of vector a by vector b
+    with scalar parameters
+    y[i] = (scalea.a[i]+shifta)/(scaleb.b[i]+shiftb)
+
+*/
+
+#define VECTORLENGTH 8
+
+v_f64x8_t
+ALM_PROTO_ARCH_ZN5(vrd8_linearfrac)(v_f64x8_t a, v_f64x8_t b, double scalea, double shifta, double scaleb, double shiftb)
+{
+    v_f64x8_t result;
+    v_f64x8_t transa, transb;
+
+    for(int i = 0; i < VECTORLENGTH; i++)
+    {
+         transa[i] = (scalea * a[i]) + shifta;
+         transb[i] = (scaleb * b[i]) + shiftb;
+
+         result[i] = transa[i] / transb[i];
+    }    
+
+    return result;
+}
--- src/iface/linearfrac.c
+++ src/iface/linearfrac.c
@@ -79,6 +79,10 @@ struct alm_arch_funcs __arch_funcs_linearfrac = {
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN5(vrs4_linearfracf),
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN5(vrs8_linearfracf),
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN5(vrs16_linearfracf),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_linearfrac),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_linearfrac),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN5(vrd8_linearfrac),
+            
         },
 
     },

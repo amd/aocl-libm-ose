a17b78375b1766736d6a57a330541a9dd3ae8386
Mangala V <mangala.v@amd.com>
2025-01-08 16:21:23 +0000

    libm:iface:powx: Added single and double precision entry points for powx
    Signed-off-by: Mangala V <mangala.v@amd.com>
    Change-Id: I068f8a6facc69a25f00e9f181b33d4bd16dea6dc
    AMD-Internal: [CPUPL-6172]

--- src/iface/powx.c
+++ src/iface/powx.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -88,6 +88,10 @@ struct alm_arch_funcs __arch_funcs_powx = {
         [ALM_UARCH_VER_ZEN5] = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_powx),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_powx),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_powx),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN4(vrs4_powxf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN4(vrs8_powxf),
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_powxf),
             [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN5(vrsa_powxf),
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_powx),
         },

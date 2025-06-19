82c2a58e803045b5a1bc10bf4f377d13fd52b605
Managala V <mangala.v@amd.com>
2025-06-19 15:47:50 +0000

    gtests:tanf: Added vector array versions of tanf Added accuracy and performance tests for vector array versions of tanf
    Signed-off-by: Mangala V <mangala.v@amd.com>
    AMD-Internal: [CPUPL-6859]

--- gtests/include/func_var_existence.h
+++ gtests/include/func_var_existence.h
@@ -83,7 +83,7 @@ static map<string, vector<string>> libm_funcs = {
     {"sin",       {"s1d", "s1f", "vrd2", "vrd4", "vrd8", "vrs4", "vrs8", "vrs16", "vrda", "vrsa"}},
     {"sinh",      {"s1d", "s1f"}},
     {"sqrt",      {"s1d", "s1f", "vrd2", "vrd4", "vrd8", "vrs4", "vrs8", "vrs16", "vrda", "vrsa"}},
-    {"tan",       {"s1d", "s1f", "vrd2", "vrd4", "vrd8", "vrs4", "vrs8", "vrs16"}},
+    {"tan",       {"s1d", "s1f", "vrd2", "vrd4", "vrd8", "vrs4", "vrs8", "vrs16", "vrsa"}},
     {"tanh",      {"s1d", "s1f", "vrs4", "vrs8", "vrs16", "vrsa"}},
     {"trunc",     {"s1d", "s1f"}},
     {"linearfrac",{"vrd2", "vrd4", "vrd8", "vrs4", "vrs8", "vrs16", "vrda", "vrsa"}},
--- gtests/tan/tan_callback.cc
+++ gtests/tan/tan_callback.cc
@@ -214,6 +214,34 @@ int test_v16s(test_data *data, int idx)  {
   return 0;
 }
 
+int test_vas(test_data *data, int count)  {
+#if (LIBM_PROTOTYPE != PROTOTYPE_GLIBC)
+  float *ip  = (float*)data->ip;
+  float *op  = (float*)data->op;
+#if (LIBM_PROTOTYPE == PROTOTYPE_AOCL)
+  amd_vrsa_tanf(count, ip, op);
+#elif (LIBM_PROTOTYPE == PROTOTYPE_SVML)
+  vsTan(count, ip, op);
+#endif
+#endif
+  return 0;
+}
+
+int test_vad(test_data *data, int count)  {
+#if 0 //Enable when support is added for vrda
+#if (LIBM_PROTOTYPE != PROTOTYPE_GLIBC)
+  double *ip  = (double*)data->ip;
+  double *op  = (double*)data->op;
+#if (LIBM_PROTOTYPE == PROTOTYPE_AOCL)
+  amd_vrda_tan(count, ip, op);
+#elif (LIBM_PROTOTYPE == PROTOTYPE_SVML)
+  vdTan(count, ip, op);
+#endif
+#endif
+#endif
+  return 0;
+}
+
 #ifdef __cplusplus
 }
 #endif

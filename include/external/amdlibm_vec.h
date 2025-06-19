f3064d3602e278c05c9a9676f88bf7fcb6a7799f
Managala V <mangala.v@amd.com>
2025-06-19 10:53:30 +0000

    libm:include:external: Updated vrsa erff declaration Updated erff api declarations for vrsa in amdlibm_vec.h Enabled testcase to run erff vrsa variants in examples.
    Signed-off-by: Mangala V <mangala.v@amd.com>
    AMD-Internal: [CPUPL-6856]

--- include/external/amdlibm_vec.h
+++ include/external/amdlibm_vec.h
@@ -375,6 +375,7 @@ extern "C" {
     void amd_vrsa_asinf           (int len, float  *src, float  *dst);
     void amd_vrsa_atanf           (int len, float  *src, float  *dst);
     void amd_vrsa_coshf           (int len, float  *src, float  *dst);
+    void amd_vrsa_erff            (int len, float  *src, float  *dst);
 
 /* AVX512 variants */
 #if defined(__AVX512F__)

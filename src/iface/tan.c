50e4cccdd4d6b7ec8969327af0c949acd6278eb8
Managala V <mangala.v@amd.com>
2025-06-21 16:21:39 +0000

    libm:iface:tan: Added vrda of tan to iface Added vrda version of tan to iface
    Signed-off-by: Mangala V <mangala.v@amd.com>
    AMD-Internal: [CPUPL-6865]

--- src/iface/tan.c
+++ src/iface/tan.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -47,7 +47,8 @@ struct alm_arch_funcs __arch_funcs_tan = {
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX2(vrd4_tan),   /* v4d ? */
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_tanf),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_tan),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX2(vrsa_tanf)
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX2(vrsa_tanf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_tan),
         },
 
          [ALM_UARCH_VER_ZEN] = {
@@ -57,7 +58,8 @@ struct alm_arch_funcs __arch_funcs_tan = {
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN(vrs8_tanf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_tan),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN(vrd4_tan),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN(vrsa_tanf)
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN(vrsa_tanf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN(vrda_tan),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
@@ -67,7 +69,8 @@ struct alm_arch_funcs __arch_funcs_tan = {
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN2(vrs8_tanf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_tan),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN2(vrd4_tan),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN2(vrsa_tanf)
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN2(vrsa_tanf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN2(vrda_tan),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
@@ -77,7 +80,8 @@ struct alm_arch_funcs __arch_funcs_tan = {
             [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_ZN3(vrs8_tanf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_tan),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN3(vrd4_tan),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN3(vrsa_tanf)
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN3(vrsa_tanf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN3(vrda_tan),
         },
 
         [ALM_UARCH_VER_ZEN4] = {
@@ -90,7 +94,8 @@ struct alm_arch_funcs __arch_funcs_tan = {
 
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_tanf),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_tan),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN4(vrsa_tanf)
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN4(vrsa_tanf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_tan),
         },
 
         [ALM_UARCH_VER_ZEN5] = {
@@ -103,7 +108,8 @@ struct alm_arch_funcs __arch_funcs_tan = {
 
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN5(vrs16_tanf),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN5(vrd8_tan),
-            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN5(vrsa_tanf)
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN5(vrsa_tanf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_tan),
         },
     },
 };
@@ -121,8 +127,8 @@ LIBM_IFACE_PROTO(tan)(void *arg)
         [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_tan),
         [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_tan),
         [ALM_FUNC_VECT_SP_16] = &G_ENTRY_PT_PTR(vrs16_tanf),
-        [ALM_FUNC_VECT_SP_ARR] = &G_ENTRY_PT_PTR(vrsa_tanf)
-
+        [ALM_FUNC_VECT_SP_ARR] = &G_ENTRY_PT_PTR(vrsa_tanf),
+        [ALM_FUNC_VECT_DP_ARR] = &G_ENTRY_PT_PTR(vrda_tan),
         },
     };
 

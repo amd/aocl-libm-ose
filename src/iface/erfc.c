405ce5e031bca8d9d8295fdb030a2520dd613b42
Nimmy Krishnan <nimmy.krishnan@amd.com>
2025-08-20 06:27:59 +0000

    libm:iface:erfc: Adding vrd2_erfc to iface
    Signed-off-by: Nimmy Krishnan <nimmy.krishnan@amd.com>
    AMD-Internal:[CPUPL-6968]

--- src/iface/erfc.c
+++ src/iface/erfc.c
@@ -38,26 +38,32 @@ struct alm_arch_funcs __arch_funcs_erfc = {
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(erfc),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_erfc),
         },
 
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(erfc),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_erfc),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(erfc),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_erfc),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(erfc),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_erfc),
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(erfc),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_erfc),
         },
 
         [ALM_UARCH_VER_ZEN5] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(erfc),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_erfc),
         },
     },
 };
@@ -68,6 +74,7 @@ LIBM_IFACE_PROTO(erfc)(void *arg)
     alm_ep_wrapper_t g_entry_erfc = {
        .g_ep = {
         [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(erfc),
+        [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_erfc),
         },
     };
 

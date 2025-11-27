21bd2dc1f35fab840d95fd9a93071d9de60ccc8e
jagar <jagarish1.r@amd.com>
2025-11-27 15:06:02 +0530

    libm:iface:round:Add vrs4_roundf with Zen and AVX2 variants
    Registered vrs4_roundf in the interface layer with support
    for Zen and AVX2 implementations for dynamic dispatch.
    
    AMD-Internal: [SWLCSG-3910]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/round.c
+++ src/iface/round.c
@@ -37,6 +37,7 @@ static const struct alm_arch_funcs __arch_funcs_round = {
         [ALM_UARCH_VER_DEFAULT] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(round),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX2(vrs4_roundf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_round),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX2(vrd4_round),
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_round)
@@ -45,43 +46,49 @@ static const struct alm_arch_funcs __arch_funcs_round = {
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(round),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN(vrs4_roundf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_round),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN(vrd4_round),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN(vrda_round),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN(vrda_round)
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(round),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN2(vrs4_roundf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_round),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN2(vrd4_round),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN2(vrda_round),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN2(vrda_round)
         },
 
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(round),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN3(vrs4_roundf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_round),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN3(vrd4_round),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN3(vrda_round),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN3(vrda_round)
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(round),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN4(vrs4_roundf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_round),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN4(vrd4_round),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_round),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_round),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_round)
+
         },
 
         [ALM_UARCH_VER_ZEN5] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN5(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(round),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_ZN5(vrs4_roundf),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_round),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_round),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN5(vrd8_round),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_round),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_round)
         },
     },
 };
@@ -90,12 +97,13 @@ void
 LIBM_IFACE_PROTO(round)(void *arg) {
     alm_ep_wrapper_t g_entry_round = {
        .g_ep = {
-        [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(roundf),
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(round),
-        [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_round),
-        [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_round),
-        [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_round),
-        [ALM_FUNC_VECT_DP_ARR] = &G_ENTRY_PT_PTR(vrda_round),
+            [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(roundf),
+            [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(round),
+            [ALM_FUNC_VECT_SP_4] = &G_ENTRY_PT_PTR(vrs4_roundf),
+            [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_round),
+            [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_round),
+            [ALM_FUNC_VECT_DP_ARR] = &G_ENTRY_PT_PTR(vrda_round)
         },
     };
 

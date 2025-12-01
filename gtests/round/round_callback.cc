633913f671651b6a4523e6ad9caac7804947b921
jagar <jagarish1.r@amd.com>
2025-12-01 18:46:06 +0530

    gtests: Enable gtests for vrs4_round
    * Enable gtests for vrs4_round to validate accuracy and performance.
    
    AMD-Internal: [SWLCSG-3910]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- gtests/round/round_callback.cc
+++ gtests/round/round_callback.cc
@@ -149,14 +149,12 @@ int test_v2d(test_data *data, int idx)  {
 }
 
 int test_v4s(test_data *data, int idx)  {
-#if 0
 #if ((LIBM_PROTOTYPE != PROTOTYPE_MSVC) && (LIBM_PROTOTYPE != PROTOTYPE_GLIBC))
   float *ip  = (float*)data->ip;
   float *op  = (float*)data->op;
   __m128 ip4 = _mm_set_ps(ip[idx+3], ip[idx+2], ip[idx+1], ip[idx]);
   __m128 op4 = LIBM_FUNC_VEC(s, 4, roundf)(ip4);
   _mm_store_ps(&op[0], op4);
-#endif
 #endif
   return 0;
 }

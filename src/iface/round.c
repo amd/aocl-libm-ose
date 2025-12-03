9770918431c2b2585a5472739f39862bc5f22840
jagar <jagarish1.r@amd.com>
2025-12-03 13:27:12 +0530

    libm:iface:round:Add vrd4_round with Zen and AVX2 variants
    Registered vrd4_round in the interface layer with support
    for Zen and AVX2 implementations for dynamic dispatch.
    
    AMD-Internal: [SWLCSG-3910]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/round.c
+++ src/iface/round.c
@@ -38,36 +38,42 @@ static const struct alm_arch_funcs __arch_funcs_round = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(round),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX2(vrd4_round),
         },
 
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(round),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN(vrd4_round),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(round),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN2(vrd4_round),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(round),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN3(vrd4_round),
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(round),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN4(vrd4_round),
         },
 
         [ALM_UARCH_VER_ZEN5] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN5(roundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(round),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_round),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_round),
         },
     },
 };
@@ -79,6 +85,7 @@ LIBM_IFACE_PROTO(round)(void *arg) {
         [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(roundf),
         [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(round),
         [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_round),
+        [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_round),
         },
     };
 

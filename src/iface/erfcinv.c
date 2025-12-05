cd242c899f97dc0324264a90199cfb240b1a06cb
Ashish Sriram <Ashish.Sriram@amd.com>
2025-12-05 08:29:34 +0000

    libm:iface:vrd_erfcinv: Adding double precision vrd*_erfcinv to iface
    Signed-off-by: Ashish Sriram <ashish.sriram@amd.com>
    AMD-Internal:[CPUPL-7624]

--- src/iface/erfcinv.c
+++ src/iface/erfcinv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2025-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -38,26 +38,50 @@ struct alm_arch_funcs __arch_funcs_erfcinv = {
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(erfcinv),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_erfcinv),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX2(vrd4_erfcinv),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_erfcinv),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_erfcinv),
         },
 
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(erfcinv),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_erfcinv),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN(vrd4_erfcinv),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN(vrda_erfcinv),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_erfcinv),
         },
 
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(erfcinv),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_erfcinv),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN2(vrd4_erfcinv),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN2(vrda_erfcinv),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_erfcinv),
         },
 
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(erfcinv),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_erfcinv),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN3(vrd4_erfcinv),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN3(vrda_erfcinv),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_erfcinv),
         },
 
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(erfcinv),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_erfcinv),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN4(vrd4_erfcinv),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_erfcinv),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_erfcinv),
         },
 
         [ALM_UARCH_VER_ZEN5] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(erfcinv),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_erfcinv),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_erfcinv),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_erfcinv),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN5(vrd8_erfcinv),
         },
     },
 };
@@ -67,7 +91,11 @@ LIBM_IFACE_PROTO(erfcinv)(void *arg)
 {
     alm_ep_wrapper_t g_entry_erfcinv = {
        .g_ep = {
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(erfcinv),
+        [ALM_FUNC_SCAL_DP]     = &G_ENTRY_PT_PTR(erfcinv),
+        [ALM_FUNC_VECT_DP_2]   = &G_ENTRY_PT_PTR(vrd2_erfcinv),
+        [ALM_FUNC_VECT_DP_4]   = &G_ENTRY_PT_PTR(vrd4_erfcinv),
+        [ALM_FUNC_VECT_DP_8]   = &G_ENTRY_PT_PTR(vrd8_erfcinv),
+        [ALM_FUNC_VECT_DP_ARR] = &G_ENTRY_PT_PTR(vrda_erfcinv),
         },
     };
 

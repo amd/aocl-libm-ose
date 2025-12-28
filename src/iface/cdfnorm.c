a5b08b106a7f82277b0fa77630ca50fbcaaf960b
Nimmy Krishnan <nimmy.krishnan@amd.com>
2025-12-28 18:52:16 +0000

    libm:src:iface:vrda_cdfnorm: Adding vrda_cdfnorm to iface
    Signed-off-by: Nimmy Krishnan <nimmy.krishnan@amd.com>
    AMD-Internal:[CPUPL-7805]

--- src/iface/cdfnorm.c
+++ src/iface/cdfnorm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -40,36 +40,42 @@ struct alm_arch_funcs __arch_funcs_cdfnorm = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX2(vrd2_cdfnorm),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX2(vrd4_cdfnorm),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cdfnorm),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_cdfnorm),
         },
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(cdfnorm),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN(vrd2_cdfnorm),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN(vrd4_cdfnorm),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cdfnorm),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN(vrda_cdfnorm),
         },
         [ALM_UARCH_VER_ZEN2] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(cdfnorm),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN2(vrd2_cdfnorm),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN2(vrd4_cdfnorm),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cdfnorm),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN2(vrda_cdfnorm),
         },
         [ALM_UARCH_VER_ZEN3] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(cdfnorm),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN3(vrd2_cdfnorm),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN3(vrd4_cdfnorm),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cdfnorm),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN3(vrda_cdfnorm),
         },
         [ALM_UARCH_VER_ZEN4] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(cdfnorm),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN4(vrd2_cdfnorm),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN4(vrd4_cdfnorm),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cdfnorm),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_cdfnorm),
         },
         [ALM_UARCH_VER_ZEN5] = {
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(cdfnorm),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_ZN5(vrd2_cdfnorm),
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_cdfnorm),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN5(vrd8_cdfnorm),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_cdfnorm),
         },
     }
 };
@@ -81,6 +87,7 @@ LIBM_IFACE_PROTO(cdfnorm)(void *arg) {
         [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_cdfnorm),
         [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_cdfnorm),
         [ALM_FUNC_VECT_DP_8] = &G_ENTRY_PT_PTR(vrd8_cdfnorm),
+        [ALM_FUNC_VECT_DP_ARR] = &G_ENTRY_PT_PTR(vrda_cdfnorm),
         },
     };
 

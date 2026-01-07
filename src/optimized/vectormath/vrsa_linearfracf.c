db2998ccb869eb158563ab089e26a0df297dec0f
Nimmy Krishnan <nimmy.krishnan@amd.com>
2026-01-07 07:14:09 +0000

    libm:src:vrsa_linearfrac: Fix mask generation in remainder case
    In the remainder handling section, GET_MASK_DOUBLE_256_BIT(length) was used instead of GET_MASK_FLOAT_256_BIT(remainder).
    This was causing failures when the operation is in-place. Fixed by using proper mask generation instruction.
    
    Signed-off-by: Nimmy Krishnan <nimmy.krishnan@amd.com>
    AMD-Internal:[CPUPL-7843]

--- src/optimized/vectormath/vrsa_linearfracf.c
+++ src/optimized/vectormath/vrsa_linearfracf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2024-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -118,7 +118,7 @@ void ALM_PROTO_OPT(vrsa_linearfracf)(int length, const float *a, const float *b,
         remainder = length - j;
         if (remainder)
         {
-            __m256i mask = GET_MASK_DOUBLE_256_BIT(length);
+            __m256i mask = GET_MASK_FLOAT_256_BIT(remainder);
             a_v = _mm256_maskload_ps(&a[j], mask);
             b_v = _mm256_maskload_ps(&b[j], mask);
             /* transa = (a * scalea) + shifta */

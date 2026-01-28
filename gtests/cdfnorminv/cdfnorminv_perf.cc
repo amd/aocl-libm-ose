4ce6f326b1fb7991b3cc24bc036a5b14a93b938e
assriram <assriram@amd.com>
2026-01-28 01:47:44 +0000

    libm:gtests:vrd_cdfnorminv: Added tests for vector double precision cdfnorminv
    gtests added for cdfnorminv vrd variants
    
    Signed-off-by: Ashish Sriram <ashish.sriram@amd.com>
    AMD-Internal:[CPUPL-7883]

--- gtests/cdfnorminv/cdfnorminv_perf.cc
+++ gtests/cdfnorminv/cdfnorminv_perf.cc
@@ -44,13 +44,45 @@ int AlmTestPerfFramework::AlmTestPerformance(InputParams *params) {
   string funcnam = "AoclLibm";
   string libm;
 
- if((params->fwidth == ALM::FloatWidth::E_ALL) ||
+  if((params->fwidth == ALM::FloatWidth::E_ALL) ||
     (params->fwidth == ALM::FloatWidth::E_F64)) {
     if((params->fqty == ALM::FloatQuantity::E_All) ||
      (params->fqty == ALM::FloatQuantity::E_Scalar)) {
       string varnam = "_s1d(cdfnorminv)";
       libm = funcnam + varnam;
       benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTestd, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_2)) {
+      string varnam = "_v2d(cdfnorminv)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest2d, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_4)) {
+      string varnam = "_v4d(cdfnorminv)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest4d, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+
+    #if defined(__AVX512__)
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_8)) {
+      string varnam = "_v8d(cdfnorminv)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTest8d, params)
+                 ->Args({(int)params->count})->Iterations(params->niter);
+    }
+    #endif
+    
+    if((params->fqty == ALM::FloatQuantity::E_All) ||
+     (params->fqty == ALM::FloatQuantity::E_Vector_Array)) {
+      string varnam = "_vad(cdfnorminv)";
+      libm = funcnam + varnam;
+      benchmark::RegisterBenchmark(libm.c_str(), &LibmPerfTestad, params)
                  ->Args({(int)params->count})->Iterations(params->niter);
     }
   }

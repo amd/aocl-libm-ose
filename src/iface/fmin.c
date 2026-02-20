a319a52bdbe8dd2d5f41a1732b59ff7acbcd9d82
Dhiraj Kumar <dhiraj.kumar@amd.com>
2026-02-20 18:35:20 +0530

    libm:src:fmin: Add AVX512 support for fmin/fminf
    - Add AVX512 dispatch and wrapper
    
    Signed-off-by: Dhiraj Kumar <dhiraj.kumar@amd.com>

--- src/iface/fmin.c
+++ src/iface/fmin.c
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2008-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -40,6 +40,12 @@ struct alm_arch_funcs __arch_funcs_fmin = {
             [ALM_FUNC_SCAL_DP]     = &ALM_PROTO_ARCH_AVX2(fmin),
             [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX2(vrsa_fminf),
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_fmin),
+        },
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP]     = &ALM_PROTO_ARCH_AVX512(fminf),
+            [ALM_FUNC_SCAL_DP]     = &ALM_PROTO_ARCH_AVX512(fmin),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX512(vrsa_fminf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX512(vrda_fmin),
         },
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP]     = &ALM_PROTO_ARCH_ZN(fminf),

95ce27ec55ee8a95c53aeef645e1912cf8c8b8cb
jagar <jagarish1.r@amd.com>
2026-02-23 14:28:29 +0530

    libm:src:acos: Add AVX512 arch wrappers for acos variants
    * Implement portable AVX512 acos kernels (vrs16_cosf)
      under src/optimized/vec/avx512.
    * Add acos/acosf dispatch tables to register AVX512
      variants and enable AVX512 runtime dispatch.
    
    New AVX512 acos/acosf dispatch entries:
      amd_acosf_avx512,amd_acos_avx512,
      amd_vrd{2,4}_acos_avx512,
      amd_vrs{4,8,16}_acosf_avx512,
      amd_vrda_acos_avx512, amd_vrsa_acosf_avx512
    
    AMD-Internal : [CPUPL-8027]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/acos.c
+++ src/iface/acos.c
@@ -50,6 +50,18 @@ struct alm_arch_funcs __arch_funcs_acos = {
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_acos),
         },
 
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP]      = &ALM_PROTO_ARCH_AVX512(acosf),
+            [ALM_FUNC_SCAL_DP]      = &ALM_PROTO_ARCH_AVX512(acos),
+            [ALM_FUNC_VECT_SP_4]    = &ALM_PROTO_ARCH_AVX512(vrs4_acosf),
+            [ALM_FUNC_VECT_SP_8]    = &ALM_PROTO_ARCH_AVX512(vrs8_acosf),
+            [ALM_FUNC_VECT_DP_2]    = &ALM_PROTO_ARCH_AVX512(vrd2_acos),
+            [ALM_FUNC_VECT_DP_4]    = &ALM_PROTO_ARCH_AVX512(vrd4_acos),
+            [ALM_FUNC_VECT_SP_ARR]  = &ALM_PROTO_ARCH_AVX512(vrsa_acosf),
+            [ALM_FUNC_VECT_DP_ARR]  = &ALM_PROTO_ARCH_AVX512(vrda_acos),
+            [ALM_FUNC_VECT_SP_16]   = &ALM_PROTO_ARCH_AVX512(vrs16_acosf),
+        },
+
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(acosf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(acos),

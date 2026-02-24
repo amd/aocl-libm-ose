2c7e8f7268e4da5c24e1b434c6d4421d5a477e29
jagar <jagarish1.r@amd.com>
2026-02-24 12:27:44 +0530

    libm:src:log2: Add AVX512 arch wrappers for log2 variants
    * Add log2/log2f dispatch tables to register AVX512
      variants and enable AVX512 runtime dispatch.
    
    New AVX512 log2/log2f dispatch entries:
      amd_log2f_avx512,amd_log2_avx512,
      amd_vrs4_log2f_avx512,amd_vrs8_log2f_avx512,
      amd_vrd2_log2_avx512,amd_vrd4_log2_avx512,
      amd_vrsa_log2f_avx512,amd_vrda_log2_avx512,
      amd_vrs16_log2f_avx512,amd_vrd8_log2_avx512
    
    AMD-Internal : [CPUPL-8027]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/log2.c
+++ src/iface/log2.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -30,7 +30,6 @@
 #include <libm/iface.h>
 #include <libm/entry_pt.h>
 
-//
 #include <libm/arch/all.h>
 
 
@@ -49,6 +48,19 @@ struct alm_arch_funcs __arch_funcs_log2 = {
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_log2),
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_log2f),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_log2)
+        },
+
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(log2f),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(log2),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX512(vrs4_log2f),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_AVX512(vrs8_log2f), /* v8s ? */
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX512(vrd2_log2),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX512(vrd4_log2),  /* v4d ? */
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX512(vrsa_log2f),  /*vector array variants*/
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX512(vrda_log2),
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_AVX512(vrs16_log2f),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_AVX512(vrd8_log2)
         },
 
         [ALM_UARCH_VER_ZEN] = {

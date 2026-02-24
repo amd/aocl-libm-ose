0e338e50cf5f8025253b13972217566477047f65
jagar <jagarish1.r@amd.com>
2026-02-24 09:49:55 +0530

    libm:src:exp10: Add AVX512 arch wrappers for exp10 variants
    * Implement portable AVX512 exp10 kernels under
      src/optimized/vec/avx512.
    * Add exp10/exp10f dispatch tables to register AVX512
      variants and enable AVX512 runtime dispatch.
    
    New AVX512 exp10/exp10f dispatch entries:
      amd_exp10f_avx512, amd_exp10_avx512,
      amd_vrda_exp10_avx512, amd_vrsa_exp10f_avx512
    
    AMD-Internal : [CPUPL-8027]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/exp10.c
+++ src/iface/exp10.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -44,10 +44,19 @@ struct alm_arch_funcs __arch_funcs_exp10 = {
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
             [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX2(vrsa_exp10f),
-            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_exp10), 
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_exp10),
         },
 
-            [ALM_UARCH_VER_ZEN] = {
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(exp10f),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(exp10),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_FMA3(vrd2_exp10),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX512(vrsa_exp10f),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX512(vrda_exp10),
+        },
+
+        [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(exp10f),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(exp10),
             [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_FMA3(vrs4_exp10f),

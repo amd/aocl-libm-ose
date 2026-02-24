71f265ff0ba6263a8b1ecb396868e01e84130630
jagar <jagarish1.r@amd.com>
2026-02-24 12:33:54 +0530

    libm:src:logb: Add AVX512 arch wrappers for logb variants
    * Add logb/logbf dispatch tables to register AVX512
      variants and enable AVX512 runtime dispatch.
    
    New AVX512 logb/logbf dispatch entries:
      amd_logbf_avx512,amd_logb_avx512
    
    AMD-Internal : [CPUPL-8027]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/logb.c
+++ src/iface/logb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -29,6 +29,7 @@
 #include <libm/amd_funcs_internal.h>
 #include <libm/iface.h>
 #include <libm/entry_pt.h>
+
 #include <libm/arch/all.h>
 
 
@@ -43,6 +44,11 @@ struct alm_arch_funcs __arch_funcs_logb = {
             NULL,                           /* vrs8 ? */
             NULL,                           /* vrd2 ? */
             NULL,                           /* vrd4 ? */
+        },
+
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(logbf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(logb),
         },
 
         [ALM_UARCH_VER_ZEN] = {

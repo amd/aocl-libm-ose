a53f216ce827b744d9aea1015a2f2a89115302d2
jagar <jagarish1.r@amd.com>
2026-02-24 09:25:42 +0530

    libm:src:cosh: Add AVX512 arch wrappers for cosh variants
    * Add cosh/coshf dispatch tables to register AVX512
      variants and enable AVX512 runtime dispatch.
    
    New AVX512 cosh/coshf dispatch entries:
      amd_coshf_avx512, amd_cosh_avx512,
      amd_vrs{4,8}_coshf_avx512,
      amd_vrsa_coshf_avx512, amd_vrda_cosh_avx512
    
    AMD-Internal : [CPUPL-8027]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/cosh.c
+++ src/iface/cosh.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -44,6 +44,15 @@ struct alm_arch_funcs __arch_funcs_cosh = {
             [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_REF(vrd2_cosh),
             [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX2(vrsa_coshf),
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX2(vrda_cosh),
+        },
+
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(coshf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(cosh),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX512(vrs4_coshf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_AVX512(vrs8_coshf),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX512(vrsa_coshf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX512(vrda_cosh),
         },
 
         [ALM_UARCH_VER_ZEN] = {

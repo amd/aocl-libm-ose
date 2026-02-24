ef151acbbd21ad2f12828beda7ed8f5c0f6d1793
jagar <jagarish1.r@amd.com>
2026-02-24 06:29:10 +0530

    libm:src:cos: Add AVX512 arch wrappers for cos variants
    * Implement portable AVX512 cos kernels under
      src/optimized/vec/avx512.
    * Add cos/cosf dispatch tables to register AVX512
      variants and enable AVX512 runtime dispatch.
    
    New AVX512 cos/cosf dispatch entries:
      amd_cosf_avx512,amd_cos_avx512,
      amd_vrd{2,4,8}_cos_avx512,
      amd_vrs{4,8,16}_cosf_avx512,
      amd_vrda_cos_avx512, amd_vrsa_cosf_avx512
    
    AMD-Internal : [CPUPL-8027]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/cos.c
+++ src/iface/cos.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -51,6 +51,19 @@ struct alm_arch_funcs __arch_funcs_cos = {
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cos),
         },
 
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(cosf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(cos),
+            [ALM_FUNC_VECT_SP_4] = &ALM_PROTO_ARCH_AVX512(vrs4_cosf),
+            [ALM_FUNC_VECT_SP_8] = &ALM_PROTO_ARCH_AVX512(vrs8_cosf),
+            [ALM_FUNC_VECT_DP_2] = &ALM_PROTO_ARCH_AVX512(vrd2_cos),
+            [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_AVX512(vrd4_cos),
+            [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_AVX512(vrsa_cosf),
+            [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_AVX512(vrda_cos),
+            [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_AVX512(vrs16_cosf),
+            [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_AVX512(vrd8_cos),
+        },
+
         [ALM_UARCH_VER_ZEN] = {
             [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(cosf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(cos),
@@ -99,7 +112,6 @@ struct alm_arch_funcs __arch_funcs_cos = {
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN4(vrd4_cos),
             [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN4(vrsa_cosf),
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN4(vrda_cos),
-
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN4(vrs16_cosf),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN4(vrd8_cos),
         },
@@ -113,7 +125,6 @@ struct alm_arch_funcs __arch_funcs_cos = {
             [ALM_FUNC_VECT_DP_4] = &ALM_PROTO_ARCH_ZN5(vrd4_cos),
             [ALM_FUNC_VECT_SP_ARR] = &ALM_PROTO_ARCH_ZN5(vrsa_cosf),
             [ALM_FUNC_VECT_DP_ARR] = &ALM_PROTO_ARCH_ZN5(vrda_cos),
-
             [ALM_FUNC_VECT_SP_16] = &ALM_PROTO_ARCH_ZN5(vrs16_cosf),
             [ALM_FUNC_VECT_DP_8] = &ALM_PROTO_ARCH_ZN5(vrd8_cos),
         },

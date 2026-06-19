f95828093ef1f97c48f7b637da44a200f80203c3
jagar <jagarish1.r@amd.com>
2026-06-19 14:51:09 +0530

    libm:iface:lroundf: update lroundf for arch dispatch
    Update iface/lround.c to dispatch the lroundf entry point to
    per-microarchitecture optimized implementations (AVX2, AVX512,
    ZN through ZN6) instead of falling back to the REF path.
    
    Align lroundf dispatch behavior with the existing double-precision
    lround implementation.
    
    AMD-Internal: [CPUPL-8641]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/lround.c
+++ src/iface/lround.c
@@ -31,41 +31,48 @@
 #include <libm/entry_pt.h>
 #include <libm/arch/all.h>
 
+
 static const
 struct alm_arch_funcs __arch_funcs_lround = {
     .def_arch = ALM_UARCH_VER_DEFAULT,
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(lround),
         },
+
         [ALM_UARCH_VER_AVX512] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(lround),
         },
+
         [ALM_UARCH_VER_ZEN] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(lround),
         },
+
         [ALM_UARCH_VER_ZEN2] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(lround),
         },
+
         [ALM_UARCH_VER_ZEN3] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(lround),
         },
+
         [ALM_UARCH_VER_ZEN4] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(lround),
         },
+
         [ALM_UARCH_VER_ZEN5] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN5(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(lround),
         },
 
         [ALM_UARCH_VER_ZEN6] = {
-            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_REF(lroundf),
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN6(lroundf),
             [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN6(lround),
         },
     },
@@ -75,8 +82,8 @@ void
 LIBM_IFACE_PROTO(lround)(void *arg) {
     alm_ep_wrapper_t g_entry_lround = {
        .g_ep = {
-        [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(lroundf),
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(lround),
+            [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(lroundf),
+            [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(lround),
         },
     };
 

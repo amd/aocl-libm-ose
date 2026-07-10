f702a9e81718bae992353b4c852c83cf943f3a59
R, Jagadish <jagadish1.r@amd.com>
2026-07-10 15:17:38 +0530

    libm:iface : update ldexp iface and entry_pt
    Update iface/ldexp.c to dispatch amd_ldexpf and amd_ldexp
    entry points to per-microarch optimized implementations
    (AVX2, AVX512, ZN through ZN6) instead of falling back
    to the REF path.
    
    Initialize ldexp in entry_pt to AVX2 arch
    
    AMD-Internal: [SWLCSG-4468]
    Signed-off-by: Jagadish R <jagadish1.r@amd.com>

--- src/iface/ldexp.c
+++ src/iface/ldexp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -29,33 +29,51 @@
 #include <libm/amd_funcs_internal.h>
 #include <libm/iface.h>
 #include <libm/entry_pt.h>
-
-//
 #include <libm/arch/all.h>
-
 
 static const
 struct alm_arch_funcs __arch_funcs_ldexp = {
     .def_arch = ALM_UARCH_VER_DEFAULT,
     .funcs = {
         [ALM_UARCH_VER_DEFAULT] = {
-            &ALM_PROTO_REF(ldexpf),
-            &ALM_PROTO_REF(ldexp),
-            NULL,                           /* vrs4 ? */
-            NULL,                           /* vrs8 ? */
-            NULL,                           /* vrd2 ? */
-            NULL,                           /* vrd4 ? */
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX2(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX2(ldexp),
         },
-#if 0
+
+        [ALM_UARCH_VER_AVX512] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_AVX512(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_AVX512(ldexp),
+        },
+
         [ALM_UARCH_VER_ZEN] = {
-            &ALM_PROTO_FMA3(ldexpf),
-            &ALM_PROTO_FMA3(ldexp),
-            NULL,                           /* vrs4 ? */
-            NULL,                           /* vrs8 ? */
-            NULL,                           /* vrd2 ? */
-            NULL,                           /* vrd4 ? */
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN(ldexp),
         },
-#endif
+
+        [ALM_UARCH_VER_ZEN2] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN2(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN2(ldexp),
+        },
+
+        [ALM_UARCH_VER_ZEN3] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN3(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN3(ldexp),
+        },
+
+        [ALM_UARCH_VER_ZEN4] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN4(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN4(ldexp),
+        },
+
+        [ALM_UARCH_VER_ZEN5] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN5(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN5(ldexp),
+        },
+
+        [ALM_UARCH_VER_ZEN6] = {
+            [ALM_FUNC_SCAL_SP] = &ALM_PROTO_ARCH_ZN6(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &ALM_PROTO_ARCH_ZN6(ldexp),
+        },
     },
 };
 
@@ -63,18 +81,11 @@ void
 LIBM_IFACE_PROTO(ldexp)(void *arg)
 {
     alm_ep_wrapper_t g_entry_ldexp = {
-       .g_ep = {
-        [ALM_FUNC_SCAL_SP]   = &G_ENTRY_PT_PTR(ldexpf),
-        [ALM_FUNC_SCAL_DP]   = &G_ENTRY_PT_PTR(ldexp),
-#if 0
-        [ALM_FUNC_VECT_SP_4] = &G_ENTRY_PT_PTR(vrs4_ldexpf),
-        [ALM_FUNC_VECT_SP_8] = &G_ENTRY_PT_PTR(vrs8_ldexpf),
-        [ALM_FUNC_VECT_DP_2] = &G_ENTRY_PT_PTR(vrd2_ldexp),
-        [ALM_FUNC_VECT_DP_4] = &G_ENTRY_PT_PTR(vrd4_ldexp),
-#endif
+        .g_ep = {
+            [ALM_FUNC_SCAL_SP] = &G_ENTRY_PT_PTR(ldexpf),
+            [ALM_FUNC_SCAL_DP] = &G_ENTRY_PT_PTR(ldexp),
         },
     };
 
     alm_iface_fixup(&g_entry_ldexp, &__arch_funcs_ldexp);
 }
-
